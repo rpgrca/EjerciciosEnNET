0464a237f36459b37c8de8e2a08e47778e027ce5
Roberto Alfonso <roberto.alfonso@praxisemr.com>
2020-10-24 15:34:37 -0300

    add test, total returns one after adding item

--- ABM.Core.UnitTests/Inventario.vb
+++ ABM.Core.UnitTests/Inventario.vb
@@ -1,9 +1,16 @@
 ﻿Public Class Inventario
 
+    Private _productoAgregado As Boolean
+
     Public ReadOnly Property Total As Integer
         Get
+            If _productoAgregado Then Return 1
             Return 0
         End Get
     End Property
+
+    Public Sub Agregar(producto As String)
+        _productoAgregado = True
+    End Sub
 
 End Class
--- ABM.Core.UnitTests/TotalEnInventarioDebe.vb
+++ ABM.Core.UnitTests/TotalEnInventarioDebe.vb
@@ -2,11 +2,22 @@
 
 Public Class TotalEnInventarioDebe
 
+    Public Const LATA_DE_ARVEJAS As String = "Lata de arvejas Arcor"
+
     <Fact> Public Sub DevolverCero_CuandoSePideElTotalDeProductosDeUnInventarioVacio()
         Dim sut As Inventario
 
         sut = New Inventario()
         Assert.Equal(0, sut.Total)
+    End Sub
+
+    <Fact> Public Sub DevolverUno_CuandoSePideElTotalDeContactosDeUnaAgendaConUnContacto()
+        Dim sut As Inventario
+
+        sut = New Inventario()
+        sut.Agregar(LATA_DE_ARVEJAS)
+
+        Assert.Equal(1, sut.Total)
     End Sub
 
 End Class
