65c0f2a30476f28cbb7ef80d1248f4f1077004ac
Roberto Alfonso <roberto.alfonso@praxisemr.com>
2020-10-23 23:22:21 -0300

    move agenda to own file

--- ABM.Core.UnitTests/AgendaDebe.vb
+++ ABM.Core.UnitTests/AgendaDebe.vb
@@ -28,23 +28,3 @@ Public Class AgendaDebe
         Assert.Equal(2, sut.Total)
     End Sub
 End Class
-
-Public Class Agenda
-
-    Private ReadOnly _contactos As List(Of Object)
-
-    Public Sub New()
-        _contactos = New List(Of Object)
-    End Sub
-
-    Public ReadOnly Property Total As Integer
-        Get
-            Return _contactos.Count
-        End Get
-    End Property
-
-    Public Sub Agregar(nombre As String)
-        _contactos.Add(nombre)
-    End Sub
-
-End Class
---
+++ ABM.Core/Agenda.vb
@@ -0,0 +1,19 @@
+﻿Public Class Agenda
+
+    Private ReadOnly _contactos As List(Of Object)
+
+    Public Sub New()
+        _contactos = New List(Of Object)
+    End Sub
+
+    Public ReadOnly Property Total As Integer
+        Get
+            Return _contactos.Count
+        End Get
+    End Property
+
+    Public Sub Agregar(nombre As String)
+        _contactos.Add(nombre)
+    End Sub
+
+End Class
