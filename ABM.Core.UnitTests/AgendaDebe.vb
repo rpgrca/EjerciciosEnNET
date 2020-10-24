0182c59d52ed5ed609a1fe4de452676698c5ee55
Roberto Alfonso <roberto.alfonso@praxisemr.com>
2020-10-23 23:16:43 -0300

    add second test, total returns one with a client

--- ABM.Core.UnitTests/AgendaDebe.vb
+++ ABM.Core.UnitTests/AgendaDebe.vb
@@ -9,14 +9,33 @@ Public Class AgendaDebe
         Assert.Equal(0, sut.Total)
     End Sub
 
+    <Fact> Public Sub DevolverUno_CuandoSePideElTotalDeContactosDeUnaAgendaConUnContacto()
+        Dim sut As Agenda
+
+        sut = New Agenda()
+        sut.Agregar("Juan Perez")
+
+        Assert.Equal(1, sut.Total)
+    End Sub
+
 End Class
 
 Public Class Agenda
 
+    Private _agregado As Boolean
+
     Public ReadOnly Property Total As Integer
         Get
+            If _agregado Then
+                Return 1
+            End If
+
             Return 0
         End Get
     End Property
+
+    Public Sub Agregar(nombre As String)
+        _agregado = True
+    End Sub
 
 End Class
