ef2f9be6a80b7b0d801c5dc7e738d0868f55fdec
Roberto Alfonso <roberto.alfonso@praxisemr.com>
2020-10-25 14:15:34 -0300

    add first test for Factura

---
+++ ABM/ABM.Core.UnitTests/FacturaDebe.vb
@@ -0,0 +1,19 @@
+﻿Imports Xunit
+Imports ABM.Core.UnitTests.Constantes
+
+Public Class FacturaDebe
+
+    <Fact> Public Sub RetornarFacturaConDatos_CuandoSeCreaUnaFacturaAlClienteYEnUnaFecha()
+        Dim fechaVenta = #2020/12/13#
+        Dim clientes As New Agenda()
+        Dim cliente As Cliente = clientes.Agregar(CLIENTE_JUAN_PEREZ)
+        Dim sut As Factura
+
+        sut = New Factura(cliente, fechaVenta)
+
+        Assert.True(sut.HechaA(cliente))
+        Assert.True(sut.HechaEl(fechaVenta))
+        Assert.Equal(0, sut.Total)
+    End Sub
+
+End Class
---
+++ ABM/ABM.Core/Factura.vb
@@ -0,0 +1,19 @@
+﻿Public Class Factura
+
+    Public Sub New(comprador As Cliente, fecha As Date)
+
+    End Sub
+
+    Public Function HechaA(comprador As Cliente) As Boolean
+        Return True
+    End Function
+
+    Public Function HechaEl(fecha As Date) As Boolean
+        Return True
+    End Function
+
+    Public Function Total() As Integer
+        Return 0
+    End Function
+
+End Class
