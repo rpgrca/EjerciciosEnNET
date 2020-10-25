e164b75b3667fefeaf3eb9d39da8faba28c9b6f2
Roberto Alfonso <roberto.alfonso@praxisemr.com>
2020-10-25 00:01:30 -0300

    gitignore rebase

---
+++ ABM.Core.UnitTests/AgendaDebe.vb
@@ -0,0 +1,22 @@
+﻿Imports Xunit
+
+Public Class AgendaDebe
+
+    <Fact> Public Sub DevolverCero_CuandoSePideElTotalDeContactosDeUnaAgendaVacia()
+        Dim sut As Agenda
+
+        sut = New Agenda()
+        Assert.Equal(0, sut.Total)
+    End Sub
+
+End Class
+
+Public Class Agenda
+
+    Public ReadOnly Property Total As Integer
+        Get
+            Return 0
+        End Get
+    End Property
+
+End Class
--- ABM.Core.UnitTests/UnitTest1.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports System.Text
-Imports Xunit
-
-Public Class UnitTest1
-
-    <Fact()> Public Sub TestMethod1()
-    End Sub
-
-End Class
