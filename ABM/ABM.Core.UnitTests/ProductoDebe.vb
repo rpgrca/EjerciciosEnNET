ad7dcdd4c02a000d78aed27b1418abaf72a0ebc0
Roberto Alfonso <roberto.alfonso@praxisemr.com>
2020-10-25 21:18:28 -0300

    refactor tests

--- ABM/ABM.Core.UnitTests/ProductoDebe.vb
+++ ABM/ABM.Core.UnitTests/ProductoDebe.vb
@@ -33,25 +33,5 @@ Public Class ProductoDebe
         Assert.Equal(Inventario.NAME_IS_INVALID_EXCEPTION, exception.Message)
     End Sub
 
-    <Theory>
-    <InlineData(Nothing)>
-    <InlineData("")>
-    Public Sub LanzarExcepcion_CuandoSeCambiaElCodigoAUnCodigoInvalido(codigoInvalido As String)
-        Dim sut = New Inventario()
-        Dim producto = sut.Agregar(LATA_DE_ARVEJAS, PRECIO_UNITARIO_LATA_DE_ARVEJAS, CODIGO_DE_LATA_DE_ARVEJAS)
-
-        Dim exception = Assert.Throws(GetType(ArgumentException), Sub() sut.CambiarCodigoDe(producto, codigoInvalido))
-        Assert.Equal(Inventario.CODE_IS_INVALID_EXCEPTION, exception.Message)
-    End Sub
-
-    <Fact>
-    Public Sub LanzarExcepcion_CuandoSeCambiaElCodigoAUnoExistente()
-        Dim inventario = New Inventario()
-        Dim producto = inventario.Agregar(LATA_DE_ARVEJAS, PRECIO_UNITARIO_LATA_DE_ARVEJAS, CODIGO_DE_LATA_DE_ARVEJAS)
-        inventario.Agregar(LATA_DE_CERVEZA, PRECIO_UNITARIO_LATA_DE_CERVEZA, CODIGO_DE_LATA_DE_CERVEZA)
-
-        Dim exception = Assert.Throws(GetType(ArgumentException), Sub() inventario.CambiarCodigoDe(producto, CODIGO_DE_LATA_DE_CERVEZA))
-        Assert.Equal(Inventario.CODE_IS_REPEATED_EXCEPTION, exception.Message)
-    End Sub
 
 End Class
