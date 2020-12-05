e01201ab90cd2786bf4c4b39cb1d9ed0bd85d34e
Roberto <rpgrca@gmail.com>
2020-12-05 20:54:28 -0300

    refactor

---
+++ AdventOfCode2020/Day5/SeatLocator.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace AdventOfCode2020.Day5
+{
+
+    public class SeatLocator
+    {
+        private readonly string[] _seatCodes;
+        private List<int> _orderedIds;
+        private IEnumerable<int> _possibleValues;
+        private IEnumerable<int> _missingIds;
+
+        public int SeatId { get; private set; }
+
+        public SeatLocator(string[] seatCodes)
+        {
+            _seatCodes = seatCodes;
+            FindSeatId();
+        }
+
+        private void FindSeatId()
+        {
+            ObtainIdsInAscendingOrder();
+            ObtainPossibleIds();
+            CalculateSeatId();
+        }
+
+        private void ObtainIdsInAscendingOrder() =>
+            _orderedIds = _seatCodes
+                .Select(p => new Seat(p).Id)
+                .OrderBy(p => p)
+                .ToList();
+
+        private void ObtainPossibleIds() =>
+            _possibleValues = Enumerable.Range(_orderedIds[0], _orderedIds[^1]);
+
+        private void CalculateSeatId()
+        {
+            FindMissingIds();
+            FindMissingIdWithPresentPreviousAndNextIds();
+        }
+
+        private void FindMissingIdWithPresentPreviousAndNextIds()
+        {
+            var possibleSolutions = new List<int>();
+
+            foreach (var missingId in _missingIds)
+            {
+                if (_orderedIds.Contains(missingId - 1) && _orderedIds.Contains(missingId + 1))
+                {
+                    possibleSolutions.Add(missingId);
+                }
+            }
+
+            SeatId = possibleSolutions.Single();
+        }
+
+        private void FindMissingIds() =>
+            _missingIds = _possibleValues.Except(_orderedIds);
+    }
+}
---
+++ AdventOfCode2020/Day5/SeatTopLocator.cs
@@ -0,0 +1,21 @@
+using System.Linq;
+
+namespace AdventOfCode2020.Day5
+{
+
+    public class SeatTopLocator
+    {
+        private readonly string[] _seatCodes;
+
+        public int MaximumSeatId { get; private set; }
+
+        public SeatTopLocator(string[] seatCodes)
+        {
+            _seatCodes = seatCodes;
+            CalculateMaximumSeatId();
+        }
+
+        private void CalculateMaximumSeatId() =>
+            MaximumSeatId = _seatCodes.Max(p => new Seat(p).Id);
+    }
+}
