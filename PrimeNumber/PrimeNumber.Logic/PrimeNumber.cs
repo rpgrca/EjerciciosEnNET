ef8f477916f132bdff955fd84bf11b71ee4e75af
Roberto <rpgrca@gmail.com>
2021-03-30 01:49:40 -0300

    more object oriented

--- PrimeNumber/PrimeNumber.Logic/PrimeNumber.cs
+++ PrimeNumber/PrimeNumber.Logic/PrimeNumber.cs
@@ -6,30 +6,73 @@ namespace PrimeNumber.Logic
 {
     public class PrimeNumber : IClassifier
     {
+        private int _value;
+        private List<int> _possibleDivisors;
+        private List<int> _previousDivisors;
+        private int _possibleDivisor;
+        private bool ValueHasADivisor { get; set; }
+
         public bool IsClassificationOf(int value)
         {
             if (value <= 0) return false;
             if (value <= 2) return true;
 
-            var possibleDivisors = Enumerable.Range(2, (int)Math.Sqrt(value)).ToList();
-            var previousDivisors = new List<int>();
-            while (possibleDivisors.Count > 0)
+            _value = value;
+            CalculatePossibleDivisorsUntilSquareRootOfValue();
+            CreatePreviousDivisorsList();
+            while (PossibleDivisorsExist())
             {
-                if (previousDivisors.Any(p => possibleDivisors[0] % p == 0))
-                {
-                    possibleDivisors.RemoveAt(0);
-                    continue;
-                }
+                TakeFirstPossibleDivisor();
+                AnyPreviousDivisorCanDividePossibleDivisor(
+                    yes: () => DiscardPossibleDivisor(),
+                    no: () => {
+                        IsValueDivisibleByPossibleDivisor(
+                            yes: () => ValueHasADivisor = true,
+                            no: () => {
+                                AddPossibleDivisorToListOfPreviousDivisors();
+                                DiscardPossibleDivisor();
+                            });
+                    }
+                );
 
-                if (value % possibleDivisors[0] == 0)
-                {
-                    return false;
-                }
-
-                previousDivisors.Add(possibleDivisors[0]);
+                if (ValueHasADivisor) return false;
             }
 
             return true;
         }
+
+        private void CalculatePossibleDivisorsUntilSquareRootOfValue() =>
+            _possibleDivisors = Enumerable.Range(2, (int)Math.Sqrt(_value)).ToList();
+
+        private void CreatePreviousDivisorsList() =>
+            _previousDivisors = new List<int>();
+
+        private bool PossibleDivisorsExist() =>
+            _possibleDivisors.Count > 0;
+
+        private void TakeFirstPossibleDivisor() =>
+            _possibleDivisor = _possibleDivisors[0];
+
+        private void AnyPreviousDivisorCanDividePossibleDivisor(Action yes, Action no)
+        {
+            if (_previousDivisors.Any(p => _possibleDivisor % p == 0))
+                yes();
+            else
+                no();
+        }
+
+        private void DiscardPossibleDivisor() =>
+            _possibleDivisors.RemoveAt(0);
+
+        private void IsValueDivisibleByPossibleDivisor(Action yes, Action no)
+        {
+            if (_value % _possibleDivisor == 0)
+                yes();
+            else
+                no();
+        }
+
+        private void AddPossibleDivisorToListOfPreviousDivisors() =>
+            _previousDivisors.Add(_possibleDivisor);
     }
 }
