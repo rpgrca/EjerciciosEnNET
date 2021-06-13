82d5c35c6de0f7580d35456a3d45e28ed72405e0
Roberto <rpgrca@gmail.com>
2021-06-13 10:12:20 -0300

    first test, identity test

---
+++ Codewars/GravityFlip/GravityFlip.Logic/GravityFlip.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GravityFlip.Logic
+{
+    public class GravityFlip
+    {
+        public int[] State { get; set; }
+
+        public GravityFlip() => State = Array.Empty<int>();
+
+        public void Flip(char dir, int[] arr)
+        {
+        }
+    }
+}
