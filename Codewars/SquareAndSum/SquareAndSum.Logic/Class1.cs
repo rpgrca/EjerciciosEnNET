4ec636addb544a8d4e72c78434336af97ca072dd
Roberto <rpgrca@gmail.com>
2021-07-20 23:10:28 -0300

    initial commit

---
+++ Codewars/SquareAndSum/SquareAndSum.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SquareAndSum.Logic
+{
+    public class Class1
+    {
+    }
+}
---
+++ Codewars/SquareAndSum/SquareAndSum.UnitTests/SquareAndSumMust.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace SquareAndSum.UnitTests
+{
+    public class SquareAndSumMust
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 2, 2 }, 9)]
+        public void Test1(int[] values, int expectedValue)
+        {
+            Assert.Equal(expectedValue, SquareSum(values));
+        }
+
+        private int SquareSum(int[] values)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
