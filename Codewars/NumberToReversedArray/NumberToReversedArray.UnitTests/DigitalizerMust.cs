85b41bbe91f511985fd6db74a3782be8dd56529b
Roberto <rpgrca@gmail.com>
2021-07-22 23:42:54 -0300

    rename test method

--- Codewars/NumberToReversedArray/NumberToReversedArray.UnitTests/DigitalizerMust.cs
+++ Codewars/NumberToReversedArray/NumberToReversedArray.UnitTests/DigitalizerMust.cs
@@ -8,7 +8,7 @@ namespace NumberToReversedArray.UnitTests
     {
         [Theory]
         [InlineData(35231, new long[] { 1, 3, 2, 5, 3 })]
-        public void Test1(long originalNumber, long[] expectedOutput)
+        public void ReturnReversedArrayOfDigits_WhenValueIsGiven(long originalNumber, long[] expectedOutput)
         {
             Assert.Equal(expectedOutput, Digitalizer.Digitalize(originalNumber));
         }
