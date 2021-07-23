cecaa1660fe211d32a03291d6dcd1f1c8f228271
Roberto <rpgrca@gmail.com>
2021-07-22 23:43:55 -0300

    add iteration, single digit number

--- Codewars/NumberToReversedArray/NumberToReversedArray.Logic/Digitalizer.cs
+++ Codewars/NumberToReversedArray/NumberToReversedArray.Logic/Digitalizer.cs
@@ -6,6 +6,9 @@ namespace NumberToReversedArray.Logic
     {
         public static long[] Digitalize(long n)
         {
+            if (n == 0)
+                return new long[] { 0 };
+
             return new long[] { 1, 3, 2, 5, 3 };
         }
     }
--- Codewars/NumberToReversedArray/NumberToReversedArray.UnitTests/DigitalizerMust.cs
+++ Codewars/NumberToReversedArray/NumberToReversedArray.UnitTests/DigitalizerMust.cs
@@ -8,6 +8,7 @@ namespace NumberToReversedArray.UnitTests
     {
         [Theory]
         [InlineData(35231, new long[] { 1, 3, 2, 5, 3 })]
+        [InlineData(0, new long[] { 0 })]
         public void ReturnReversedArrayOfDigits_WhenValueIsGiven(long originalNumber, long[] expectedOutput)
         {
             Assert.Equal(expectedOutput, Digitalizer.Digitalize(originalNumber));
