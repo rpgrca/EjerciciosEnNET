903c1fe53257e1fbe4397867473686e00c82c121
Roberto <rpgrca@gmail.com>
2021-07-22 23:57:37 -0300

    pass all tests

--- Codewars/NumberToReversedArray/NumberToReversedArray.Logic/Digitalizer.cs
+++ Codewars/NumberToReversedArray/NumberToReversedArray.Logic/Digitalizer.cs
@@ -1,15 +1,11 @@
 ﻿using System;
+using System.Linq;
 
 namespace NumberToReversedArray.Logic
 {
     public class Digitalizer
     {
-        public static long[] Digitalize(long n)
-        {
-            if (n == 0)
-                return new long[] { 0 };
-
-            return new long[] { 1, 3, 2, 5, 3 };
-        }
+        public static long[] Digitalize(long n) =>
+            n.ToString().Select(p => long.Parse($"{p}")).Reverse().ToArray();
     }
 }
--- Codewars/NumberToReversedArray/NumberToReversedArray.UnitTests/DigitalizerMust.cs
+++ Codewars/NumberToReversedArray/NumberToReversedArray.UnitTests/DigitalizerMust.cs
@@ -9,6 +9,7 @@ namespace NumberToReversedArray.UnitTests
         [Theory]
         [InlineData(35231, new long[] { 1, 3, 2, 5, 3 })]
         [InlineData(0, new long[] { 0 })]
+        [InlineData(100, new long[] { 0, 0, 1 })]
         public void ReturnReversedArrayOfDigits_WhenValueIsGiven(long originalNumber, long[] expectedOutput)
         {
             Assert.Equal(expectedOutput, Digitalizer.Digitalize(originalNumber));
