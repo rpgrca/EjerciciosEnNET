2e75ebe39f1d6f617b84684c2531c5501e11eb9f
Roberto <rpgrca@gmail.com>
2021-07-29 23:50:31 -0300

    initial setup

---
+++ Codewars/BreakCamelCase/BreakCamelCase.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BreakCamelCase.Logic
+{
+    public class Class1
+    {
+    }
+}
---
+++ Codewars/BreakCamelCase/BreakCamelCase.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace BreakCamelCase.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
