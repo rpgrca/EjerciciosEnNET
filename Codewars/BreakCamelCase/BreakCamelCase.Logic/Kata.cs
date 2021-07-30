d8e3eab5b59aa88277e434a29134cf41d2a3d5d3
Roberto <rpgrca@gmail.com>
2021-07-30 00:58:18 -0300

    final solution using regular expressions

--- Codewars/BreakCamelCase/BreakCamelCase.Logic/Kata.cs
+++ Codewars/BreakCamelCase/BreakCamelCase.Logic/Kata.cs
@@ -1,34 +1,10 @@
-﻿using System.Collections.Generic;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 namespace BreakCamelCase.Logic
 {
     public class Kata
     {
-        public static string BreakCamelCase(string str)
-        {
-            var words = new List<string>();
-            var word = string.Empty;
-
-            foreach (var character in str)
-            {
-                if (char.IsUpper(character))
-                {
-                    if (! string.IsNullOrEmpty(word))
-                    {
-                        words.Add(word);
-                    }
-
-                    word = character.ToString();
-                }
-                else
-                {
-                    word += character;
-                }
-            }
-
-            words.Add(word);
-            return string.Join(" ", words);
-        }
+        public static string BreakCamelCase(string str) =>
+            Regex.Replace(str, "([A-Z])", " $1").Trim();
     }
 }
