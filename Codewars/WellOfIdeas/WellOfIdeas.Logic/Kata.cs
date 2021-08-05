cd1be489146c39a6e79819d4d60c823fecd3683c
Roberto <rpgrca@gmail.com>
2021-08-04 23:39:04 -0300

    pass all asserts

--- Codewars/WellOfIdeas/WellOfIdeas.Logic/Kata.cs
+++ Codewars/WellOfIdeas/WellOfIdeas.Logic/Kata.cs
@@ -7,7 +7,8 @@ namespace WellOfIdeas.Logic
     {
         public static string Well(string[] x)
         {
-            if (x.Count(p => p == "good") >= 1) return "Publish!";
+            if (x.Count(p => p == "good") >= 1 && x.Count(p => p == "good") <= 2) return "Publish!";
+            if (x.Count(p => p == "good") > 2) return "I smell a series!";
             if (x.Count(p => p == "bad") >= 2) return "Fail!";
             return "";
         }
