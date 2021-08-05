3cd18ae0236c1a2b12d9f18c06f971efdba49efc
Roberto <rpgrca@gmail.com>
2021-08-04 23:40:56 -0300

    split tests

--- Codewars/WellOfIdeas/WellOfIdeas.UnitTests/WellOfIdeasMust.cs
+++ Codewars/WellOfIdeas/WellOfIdeas.UnitTests/WellOfIdeasMust.cs
@@ -7,10 +7,20 @@ namespace WellOfIdeas.UnitTests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void ReturnFail_WhenThereAreNoGoodAndAtLeastTwoBads()
         {
             Assert.Equal("Fail!", Kata.Well(new string[] {"bad", "bad", "bad"}));
+        }
+
+        [Fact]
+        public void ReturnPublish_WhenThereAreOneOrTwoGoods()
+        {
             Assert.Equal("Publish!", Kata.Well(new string[] {"good", "bad", "bad", "bad", "bad"}));
+        }
+
+        [Fact]
+        public void ReturnISmellASeries_WhenThereAreMoreThanTwoGoods()
+        {
             Assert.Equal("I smell a series!", Kata.Well(new string[] {"good", "bad", "bad", "bad", "bad", "good", "bad", "bad", "good"}));
         }
     }
