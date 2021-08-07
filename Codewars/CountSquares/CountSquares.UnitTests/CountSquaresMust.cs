8aeef32cb30b1492e13c1b09bc58f64cb4d20008
Roberto <rpgrca@gmail.com>
2021-08-06 22:59:59 -0300

    refactor tests

--- Codewars/CountSquares/CountSquares.UnitTests/CountSquaresMust.cs
+++ Codewars/CountSquares/CountSquares.UnitTests/CountSquaresMust.cs
@@ -1,4 +1,3 @@
-using System;
 using Xunit;
 using CountSquares.Logic;
 
@@ -6,46 +5,15 @@ namespace CountSquares.UnitTests
 {
     public class CountSquaresMust
     {
-        [Fact]
-        public void Test1()
-        {
-            Assert.Equal(1, Kata.CountSquares(0));
-        }
-
-        [Fact]
-        public void Test10()
-        {
-            Assert.Equal(8, Kata.CountSquares(1));
-        }
-
-        [Fact]
-        public void Test11()
-        {
-            Assert.Equal(26, Kata.CountSquares(2));
-        }
-
-        [Fact]
-        public void Test12()
-        {
-            Assert.Equal(98, Kata.CountSquares(4));
-        }
-
-        [Fact]
-        public void Test2()
-        {
-            Assert.Equal(152,Kata.CountSquares(5));
-        }
-
-        [Fact]
-        public void Test3()
-        {
-            Assert.Equal(1538, Kata.CountSquares(16));
-        }
-
-        [Fact]
-        public void Test4()
-        {
-            Assert.Equal(3176, Kata.CountSquares(23));
-        }
+        [Theory]
+        [InlineData(0, 1)]
+        [InlineData(1, 8)]
+        [InlineData(2, 26)]
+        [InlineData(4, 98)]
+        [InlineData(5, 152)]
+        [InlineData(16, 1538)]
+        [InlineData(23, 3176)]
+        public void ReturnCorrectCountOfSquaresWithPaint(int cuts, int expectedCount) =>
+            Assert.Equal(expectedCount, Kata.CountSquares(cuts));
     }
 }
