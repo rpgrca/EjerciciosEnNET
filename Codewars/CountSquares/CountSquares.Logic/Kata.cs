9a699b753f4dd72f0963f4a8b51ace2d7b687ad6
Roberto <rpgrca@gmail.com>
2021-08-06 22:39:15 -0300

    initial kata upload

---
+++ Codewars/CountSquares/CountSquares.Logic/Kata.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CountSquares.Logic
+{
+    public class Kata
+    {
+        public static int CountSquares(int cuts)
+        {
+            return 0;
+        }
+    }
+}
