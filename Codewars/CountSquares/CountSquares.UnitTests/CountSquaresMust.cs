840ae80f47615678443a006416ff54c2efbfd24b
Roberto <rpgrca@gmail.com>
2021-08-06 22:40:25 -0300

    split asserts into individual tests

--- Codewars/CountSquares/CountSquares.UnitTests/CountSquaresMust.cs
+++ Codewars/CountSquares/CountSquares.UnitTests/CountSquaresMust.cs
@@ -10,8 +10,23 @@ namespace CountSquares.UnitTests
         public void Test1()
         {
             Assert.Equal(1, Kata.CountSquares(0));
+        }
+
+        [Fact]
+        public void Test2()
+        {
             Assert.Equal(152,Kata.CountSquares(5));
+        }
+
+        [Fact]
+        public void Test3()
+        {
             Assert.Equal(1538, Kata.CountSquares(16));
+        }
+
+        [Fact]
+        public void Test4()
+        {
             Assert.Equal(3176, Kata.CountSquares(23));
         }
     }
