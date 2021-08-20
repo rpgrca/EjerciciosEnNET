164b4a44ec7d8e77e13e942f5400dde973f87604
Roberto <rpgrca@gmail.com>
2021-08-20 01:15:39 -0300

    split test asserts into single ones

--- Codewars/HumanReadableTime/HumanReadableTime.UnitTests/TimeFormatMust.cs
+++ Codewars/HumanReadableTime/HumanReadableTime.UnitTests/TimeFormatMust.cs
@@ -1,4 +1,3 @@
-using System;
 using Xunit;
 using HumanReadableTime.Logic;
 
@@ -10,9 +9,29 @@ namespace HumanReadableTime.UnitTests
         public void HumanReadableTest()
         {
             Assert.Equal("00:00:00", TimeFormat.GetReadableTime(0));
+        }
+
+        [Fact]
+        public void HumanReadableTest2()
+        {
             Assert.Equal("00:00:05", TimeFormat.GetReadableTime(5));
+        }
+
+        [Fact]
+        public void HumanReadableTest3()
+        {
             Assert.Equal("00:01:00", TimeFormat.GetReadableTime(60));
+        }
+
+        [Fact]
+        public void HumanReadableTest4()
+        {
             Assert.Equal("23:59:59", TimeFormat.GetReadableTime(86399));
+        }
+
+        [Fact]
+        public void HumanReadableTest5()
+        {
             Assert.Equal("99:59:59", TimeFormat.GetReadableTime(359999));
         }
     }
