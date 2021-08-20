66c8e6540dd97801b0d4fd412ca3c7e4a962cc71
Roberto <rpgrca@gmail.com>
2021-08-20 01:46:11 -0300

    extract constants from function

--- Codewars/HumanReadableTime/HumanReadableTime.Logic/TimeFormat.cs
+++ Codewars/HumanReadableTime/HumanReadableTime.Logic/TimeFormat.cs
@@ -1,13 +1,14 @@
-﻿using System;
-
-namespace HumanReadableTime.Logic
+﻿namespace HumanReadableTime.Logic
 {
     public static class TimeFormat
     {
+        private const int SECONDS_PER_HOUR = 3600;
+        private const int SECONDS_PER_MINUTE = 60;
+
         public static string GetReadableTime(int seconds)
         {
-            var hours = seconds / 3600;
-            var minutes = seconds % 3600 / 60;
+            var hours = seconds / SECONDS_PER_HOUR;
+            var minutes = seconds % 3600 / SECONDS_PER_MINUTE;
             var leftovers = seconds % 60;
 
             return $"{hours:00}:{minutes:00}:{leftovers:00}";
