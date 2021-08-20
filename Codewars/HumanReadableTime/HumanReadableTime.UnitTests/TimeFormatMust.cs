4831dd0ff2b8fc0cb3430c689dc1975d807013aa
Roberto <rpgrca@gmail.com>
2021-08-20 01:43:57 -0300

    refactor tests

--- Codewars/HumanReadableTime/HumanReadableTime.UnitTests/TimeFormatMust.cs
+++ Codewars/HumanReadableTime/HumanReadableTime.UnitTests/TimeFormatMust.cs
@@ -5,34 +5,13 @@ namespace HumanReadableTime.UnitTests
 {
     public class TimeFormatMust
     {
-        [Fact]
-        public void HumanReadableTest()
-        {
-            Assert.Equal("00:00:00", TimeFormat.GetReadableTime(0));
-        }
-
-        [Fact]
-        public void HumanReadableTest2()
-        {
-            Assert.Equal("00:00:05", TimeFormat.GetReadableTime(5));
-        }
-
-        [Fact]
-        public void HumanReadableTest3()
-        {
-            Assert.Equal("00:01:00", TimeFormat.GetReadableTime(60));
-        }
-
-        [Fact]
-        public void HumanReadableTest4()
-        {
-            Assert.Equal("23:59:59", TimeFormat.GetReadableTime(86399));
-        }
-
-        [Fact]
-        public void HumanReadableTest5()
-        {
-            Assert.Equal("99:59:59", TimeFormat.GetReadableTime(359999));
-        }
+        [Theory]
+        [InlineData(0, "00:00:00")]
+        [InlineData(5, "00:00:05")]
+        [InlineData(60, "00:01:00")]
+        [InlineData(86399, "23:59:59")]
+        [InlineData(359999, "99:59:59")]
+        public void HumanReadableTest(int seconds, string expectedResult) =>
+            Assert.Equal(expectedResult, TimeFormat.GetReadableTime(seconds));
     }
 }
