8533c61f7267a87d53803d8d8653b2abf954f6ee
Roberto <rpgrca@gmail.com>
2021-08-27 01:52:57 -0300

    refactor to make it more oop

---
+++ DivisorsAndSums/DivisorsAndSum.Logic/DivisorSum.cs
@@ -0,0 +1,47 @@
+using System.Linq;
+using System.Collections.Generic;
+
+namespace DivisorsAndSum.Logic
+{
+    public class DivisorSum
+    {
+        private readonly int _amount;
+        private readonly List<string> _equations;
+        private IEnumerable<int> _divisors;
+
+        public string Result { get; private set; }
+
+        public DivisorSum(int amount)
+        {
+            _amount = amount;
+            _equations = new List<string>();
+            Calculate();
+        }
+
+        private void Calculate()
+        {
+            for (var possibleValue = 6; _equations.Count < _amount; possibleValue++)
+            {
+                CalculateDivisorsFor(possibleValue);
+                AddEquationIfPossibleValueFulfillsCondition(possibleValue);
+            }
+
+            GenerateResult();
+        }
+
+        private void AddEquationIfPossibleValueFulfillsCondition(int possibleValue)
+        {
+            if (possibleValue == _divisors.Sum())
+            {
+                _equations.Add($"{string.Join(" + ", _divisors)} = {possibleValue}");
+            }
+        }
+
+        private void CalculateDivisorsFor(int possibleValue) =>
+            _divisors = Enumerable
+                .Range(1, possibleValue - 1)
+                .Where(p => possibleValue % p == 0);
+
+        private void GenerateResult() => Result = string.Join("\n", _equations);
+    }
+}
--- DivisorsAndSums/DivisorsAndSum.UnitTests/DivisorSumMust.cs
+++ DivisorsAndSums/DivisorsAndSum.UnitTests/DivisorSumMust.cs
@@ -1,22 +1,21 @@
-using System;
 using Xunit;
 using DivisorsAndSum.Logic;
 
 namespace DivisorsAndSum.UnitTests
 {
-    public class UnitTest1
+    public class DivisorSumMust
     {
         [Fact]
-        public void Test1()
+        public void ReturnEquationFor6_WhenAskedForFirstValueFulfillingCondition()
         {
-            var sut = new XYZ(1);
+            var sut = new DivisorSum(1);
             Assert.Equal("1 + 2 + 3 = 6", sut.Result);
         }
 
         [Fact]
-        public void Test2()
+        public void ReturnEquationsFor6And28_WhenAskedForFirstTwoValuesFulfillingCondition()
         {
-            var sut = new XYZ(2);
+            var sut = new DivisorSum(2);
             Assert.Equal("1 + 2 + 3 = 6\n1 + 2 + 4 + 7 + 14 = 28", sut.Result);
         }
     }
