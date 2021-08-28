614bb701d27698047a443256fcbe209b7eced259
Roberto <rpgrca@gmail.com>
2021-08-28 01:45:34 -0300

    parametrize values to try

--- DivisorsAndSums/DivisorsAndSum.Logic/DivisorSum.cs
+++ DivisorsAndSums/DivisorsAndSum.Logic/DivisorSum.cs
@@ -6,10 +6,16 @@ namespace DivisorsAndSum.Logic
 {
     public class DivisorSum
     {
+        private static IEnumerable<int> ValuesGenerator()
+        {
+            for (var index = 6; ; index++) yield return index;
+        }
+
         public class Builder
         {
             private int _amount;
             private Func<int, IEnumerable<int>> _divisorCalculator;
+            private Func<IEnumerable<int>> _valuesGenerator;
 
             public Builder UsingDivisorAlgorithm(Func<int, IEnumerable<int>> algorithm)
             {
@@ -23,38 +29,51 @@ namespace DivisorsAndSum.Logic
                 return this;
             }
 
+            public Builder UsingGenerator(Func<IEnumerable<int>> generator)
+            {
+                _valuesGenerator = generator;
+                return this;
+            }
+
             public DivisorSum Build()
             {
                 _divisorCalculator ??= v => Enumerable.Range(1, v / 2).Where(p => v % p == 0);
-                return new DivisorSum(_amount, _divisorCalculator);
+                _valuesGenerator ??= ValuesGenerator;
+                return new DivisorSum(_amount, _valuesGenerator, _divisorCalculator);
             }
         }
 
         private readonly int _amount;
         private readonly List<string> _equations;
+        private readonly IEnumerable<int> _valuesToTry;
+        private readonly Func<int, IEnumerable<int>> _divisorCalculator;
         private IEnumerable<int> _divisors;
-        private readonly Func<int, IEnumerable<int>> _calculateDivisorsFor;
 
         public string Result { get; private set; }
 
-        private DivisorSum(int amount, Func<int, IEnumerable<int>> divisorCalculator)
+        private DivisorSum(int amount, Func<IEnumerable<int>> valuesGenerator, Func<int, IEnumerable<int>> divisorCalculator)
         {
             _amount = amount;
-            _calculateDivisorsFor = divisorCalculator;
+            _divisorCalculator = divisorCalculator;
+            _valuesToTry = valuesGenerator();
             _equations = new List<string>();
             Calculate();
         }
 
         private void Calculate()
         {
-            for (var possibleValue = 6; _equations.Count < _amount; possibleValue++)
+            foreach (var possibleValue in _valuesToTry)
             {
                 CalculateDivisorsFor(possibleValue);
                 AddEquationIfPossibleValueFulfillsCondition(possibleValue);
+
+                if (FinishedObtainingValues()) break;
             }
 
             GenerateResult();
         }
+
+        private bool FinishedObtainingValues() => _equations.Count >= _amount;
 
         private void AddEquationIfPossibleValueFulfillsCondition(int possibleValue)
         {
@@ -65,7 +84,7 @@ namespace DivisorsAndSum.Logic
         }
 
         private void CalculateDivisorsFor(int possibleValue) =>
-            _divisors = _calculateDivisorsFor(possibleValue);
+            _divisors = _divisorCalculator(possibleValue);
 
         private void GenerateResult() => Result = string.Join("\n", _equations);
     }
