c1218a8d1a23a8416b37a6de76a2b9b6d34479a7
Roberto <rpgrca@gmail.com>
2021-10-22 12:50:39 -0300

    add testing project

---
+++ LoggerExercise/ConsoleApp.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ConsoleApp.IntegrationTests
+{
+    public class Database
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ LoggerExercise/ConsoleApp.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ConsoleApp.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
