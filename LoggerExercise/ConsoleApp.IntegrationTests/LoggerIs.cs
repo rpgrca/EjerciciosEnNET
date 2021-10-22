2c588e5abd6a6af0f88be9b7bb09a656eaa7fbaa
Roberto <rpgrca@gmail.com>
2021-10-22 19:11:09 -0300

    add test, throws exception when configuration is invalid

--- LoggerExercise/ConsoleApp.IntegrationTests/LoggerIs.cs
+++ LoggerExercise/ConsoleApp.IntegrationTests/LoggerIs.cs
@@ -11,5 +11,13 @@ namespace ConsoleApp.IntegrationTests
             var sut = new Logger(true, true, true, false, false, false, null);
             var exception = Assert.Throws<NullReferenceException>(() => sut.LogMessage(null, true, true, true));
         }
+
+        [Fact]
+        public void ThrowingException_WhenConfigurationIsInvalid()
+        {
+            var sut = new Logger(false, false, false, true, true, true, null);
+            var exception = Assert.Throws<Exception>(() => sut.LogMessage("test", true, true, true));
+            Assert.Equal("Invalid configuration", exception.Message);
+        }
     }
 }
