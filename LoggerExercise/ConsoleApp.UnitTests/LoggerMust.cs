8e27d807ca9a6ff513d341faea1e1dd097b7d789
Roberto <rpgrca@gmail.com>
2021-10-23 01:08:44 -0300

    add test cases for invalid null dictionary

--- LoggerExercise/ConsoleApp.UnitTests/LoggerMust.cs
+++ LoggerExercise/ConsoleApp.UnitTests/LoggerMust.cs
@@ -12,10 +12,16 @@ namespace ConsoleApp.UnitTests
             Assert.Equal(Logger.INVALID_CONFIGURATION, exception.Message);
         }
 
-        [Fact]
-        public void ThrowException_WhenNullDictionaryIsSupplied()
+        [Theory]
+        [InlineData(true, false, false)]
+        [InlineData(false, false, true)]
+        [InlineData(true, true, false)]
+        [InlineData(true, false, true)]
+        [InlineData(false, true, true)]
+        [InlineData(true, true, true)]
+        public void ThrowException_WhenNullDictionaryIsSuppliedAndLoggerNeedsIt(bool logToFile, bool logToConsole, bool logToDatabase)
         {
-            var exception = Assert.Throws<Exception>(() => new Logger(true, false, false, true, true, true, null));
+            var exception = Assert.Throws<Exception>(() => new Logger(logToFile, logToConsole, logToDatabase, true, true, true, null));
             Assert.Equal(Logger.INVALID_CONFIGURATION_VARIABLES, exception.Message);
         }
     }
