5df439836b97bf77d3c5a67c99ebfafc15ae96aa
Roberto <rpgrca@gmail.com>
2021-10-23 15:12:27 -0300

    split log to database to own class

--- LoggerExercise/ConsoleApp.Logic/Logger.cs
+++ LoggerExercise/ConsoleApp.Logic/Logger.cs
@@ -4,6 +4,53 @@ using System.Data.SqlClient;
 
 namespace ConsoleApp
 {
+    public class DatabaseLogging
+    {
+        private readonly bool _logMessage;
+        private readonly bool _logWarning;
+        private readonly bool _logError;
+        private readonly IDictionary _dbParams;
+
+        public DatabaseLogging(bool logMessage, bool logWarning, bool logError, IDictionary dbParams)
+        {
+            _logMessage = logMessage;
+            _logWarning = logWarning;
+            _logError = logError;
+            _dbParams = dbParams;
+        }
+
+        public void Log(string messageText, bool message, bool warning, bool error)
+        {
+            int t = 0;
+            if (message && _logMessage)
+            {
+                t = 1;
+            }
+
+            if (error && _logError)
+            {
+                t = 2;
+            }
+
+            if (warning && _logWarning)
+            {
+                t = 3;
+            }
+
+            if (t != 0)
+            {
+                string connectionString = "Server=" + _dbParams["serverName"] + ";Initial Catalog=" + _dbParams["DataBaseName"] + ";User ID=" + _dbParams["userName"] + ";Password=" + _dbParams["password"] + ";";
+                SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+                string insertStatement = "insert into Log_Values values('" + messageText + "', " + t.ToString() + ")";
+                SqlCommand sqlCommand = new SqlCommand(insertStatement, sqlConnection);
+                sqlConnection.Open();
+                sqlCommand.ExecuteNonQuery();
+            }
+        }
+
+    }
+
     public class Logger
     {
         public const string INVALID_CONFIGURATION = "Invalid configuration";
@@ -68,7 +115,7 @@ namespace ConsoleApp
 
             if (logToDatabase)
             {
-                LogToDatabase(messageText, message, warning, error);
+                new DatabaseLogging(logMessage, logWarning, logError, dbParams).Log(messageText, message, warning, error);
             }
         }
 
@@ -96,34 +143,5 @@ namespace ConsoleApp
             }
         }
 
-        private void LogToDatabase(string messageText, bool message, bool warning, bool error)
-        {
-            int t = 0;
-            if (message && logMessage)
-            {
-                t = 1;
-            }
-
-            if (error && logError)
-            {
-                t = 2;
-            }
-
-            if (warning && logWarning)
-            {
-                t = 3;
-            }
-
-            if (t != 0)
-            {
-                string connectionString = "Server=" + dbParams["serverName"] + ";Initial Catalog=" + dbParams["DataBaseName"] + ";User ID=" + dbParams["userName"] + ";Password=" + dbParams["password"] + ";";
-                SqlConnection sqlConnection = new SqlConnection(connectionString);
-
-                string insertStatement = "insert into Log_Values values('" + messageText + "', " + t.ToString() + ")";
-                SqlCommand sqlCommand = new SqlCommand(insertStatement, sqlConnection);
-                sqlConnection.Open();
-                sqlCommand.ExecuteNonQuery();
-            }
-        }
     }
 }
