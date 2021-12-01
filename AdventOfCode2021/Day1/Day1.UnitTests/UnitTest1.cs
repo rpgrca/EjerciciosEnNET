069a08e76a25916ecce755e7c4c614470d6922a5
Roberto <rpgrca@gmail.com>
2021-12-01 08:06:14 -0300

    initial upload

---
+++ AdventOfCode2021/Day1/Day1.UnitTests/UnitTest1.cs
@@ -0,0 +1,23 @@
+using System;
+using Xunit;
+
+namespace Day1.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            const string DEPTHS = @"199
+200
+208
+210
+200
+207
+240
+269
+260
+263";
+        }
+    }
+}
