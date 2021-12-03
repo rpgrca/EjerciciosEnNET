b8ddb0b5292fb48c9576db7263c686e00020c392
Roberto <rpgrca@gmail.com>
2021-12-03 09:21:07 -0300

    filter correctly 3 positions

--- AdventOfCode2021/Day3/Day3.Logic/DiagnosticReport.cs
+++ AdventOfCode2021/Day3/Day3.Logic/DiagnosticReport.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Linq;
+using System;
 using System.Collections.Generic;
 
 namespace Day3.Logic
@@ -6,11 +7,12 @@ namespace Day3.Logic
     public class DiagnosticReport
     {
         private readonly string _reportInput;
-        private readonly string[] _values;
+        private readonly List<string> _values;
 
         public int GammaRate { get; }
         public int EpsilonRate { get; }
         public int PowerConsumption { get; }
+        public List<List<string>> FilteredValues { get; }
 
         public DiagnosticReport(string reportInput)
         {
@@ -20,7 +22,7 @@ namespace Day3.Logic
             }
 
             _reportInput = reportInput;
-            _values = _reportInput.Split("\n");
+            _values = _reportInput.Split("\n").ToList();
 
             var gammaValue = string.Empty;
             var epsilonValue = string.Empty;
@@ -56,6 +58,48 @@ namespace Day3.Logic
             GammaRate = Convert.ToInt32(gammaValue, 2);
             EpsilonRate = Convert.ToInt32(epsilonValue, 2);
             PowerConsumption = GammaRate * EpsilonRate;
+        }
+
+        public DiagnosticReport(string reportInput, bool useSecond)
+        {
+            _reportInput = reportInput;
+            _values = _reportInput.Split("\n").ToList();
+            FilteredValues = new List<List<string>>();
+
+            List<string> currentSet = _values;
+
+            for (var index = 0; index < 3; index++)
+            {
+                var zeroes = 0;
+                var ones = 0;
+                var teamZero = new List<string>();
+                var teamOne = new List<string>();
+
+                foreach (var value in currentSet)
+                {
+                    if (value[index] == '0')
+                    {
+                        zeroes++;
+                        teamZero.Add(value);
+                    }
+                    else
+                    {
+                        ones++;
+                        teamOne.Add(value);
+                    }
+                }
+
+                if (zeroes > ones)
+                {
+                    currentSet = teamZero;
+                    FilteredValues.Add(teamZero);
+                }
+                else if (zeroes < ones)
+                {
+                    currentSet = teamOne;
+                    FilteredValues.Add(teamOne);
+                }
+            }
         }
     }
 }
