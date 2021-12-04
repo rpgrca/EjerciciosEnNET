9c0bea7b4763116b97cf2c6fa8644753e5484004
Roberto <rpgrca@gmail.com>
2021-12-04 08:09:57 -0300

    add test, do not mark any number when inexistant

--- AdventOfCode2021/Day4/Day4.UnitTests/BingoMust.cs
+++ AdventOfCode2021/Day4/Day4.UnitTests/BingoMust.cs
@@ -141,7 +141,7 @@ namespace Day4.UnitTests
         }
 
         [Fact]
-        public void Test1()
+        public void MarkDrawnNumberInBoardCorrectly()
         {
             const string BOARD = @"22 13 17 11  0
  8  2 23  4 24
@@ -154,5 +154,52 @@ namespace Day4.UnitTests
 
             Assert.Equal(-22, sut.Boards[0][0][0]);
         }
+
+        [Fact]
+        public void NotMarkAnyNumberInBoard_WhenNumberDidNotExist()
+        {
+            const string BOARD = @"22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19";
+
+            var sut = new Bingo(BOARD);
+            sut.Play("90");
+
+            Assert.Collection(sut.Boards[0],
+                p1 => Assert.Collection(p1,
+                    p11 => Assert.Equal(22, p11),
+                    p12 => Assert.Equal(13, p12),
+                    p13 => Assert.Equal(17, p13),
+                    p14 => Assert.Equal(11, p14),
+                    p15 => Assert.Equal(0, p15)),
+                 p2 => Assert.Collection(p2,
+                    p11 => Assert.Equal(8, p11),
+                    p12 => Assert.Equal(2, p12),
+                    p13 => Assert.Equal(23, p13),
+                    p14 => Assert.Equal(4, p14),
+                    p15 => Assert.Equal(24, p15)),
+                 p3 => Assert.Collection(p3,
+                    p11 => Assert.Equal(21, p11),
+                    p12 => Assert.Equal(9, p12),
+                    p13 => Assert.Equal(14, p13),
+                    p14 => Assert.Equal(16, p14),
+                    p15 => Assert.Equal(7, p15)),
+                 p4 => Assert.Collection(p4,
+                    p11 => Assert.Equal(6, p11),
+                    p12 => Assert.Equal(10, p12),
+                    p13 => Assert.Equal(3, p13),
+                    p14 => Assert.Equal(18, p14),
+                    p15 => Assert.Equal(5, p15)),
+                 p5 => Assert.Collection(p5,
+                    p11 => Assert.Equal(1, p11),
+                    p12 => Assert.Equal(12, p12),
+                    p13 => Assert.Equal(20, p13),
+                    p14 => Assert.Equal(15, p14),
+                    p15 => Assert.Equal(19, p15)));
+        }
+
+
     }
 }
