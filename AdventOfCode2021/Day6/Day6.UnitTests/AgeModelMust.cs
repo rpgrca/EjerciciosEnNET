43eb0cf1b4cbf2ea05c1f2a90c2320d5255b30fb
Roberto <rpgrca@gmail.com>
2021-12-06 07:48:54 -0300

    solve sample puzzle

--- AdventOfCode2021/Day6/Day6.UnitTests/AgeModelMust.cs
+++ AdventOfCode2021/Day6/Day6.UnitTests/AgeModelMust.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System;
 using Xunit;
 using Day6.Logic;
@@ -45,13 +46,23 @@ namespace Day6.UnitTests
         }
 
         [Fact]
-        public void SpawnNewLanternfish_WhenTimerPassesZero()
+        public void SpawnNewLanternfish_WhenFourDaysPassAfterInitialAgeOf3()
         {
             var sut = new AgeModel("3");
             sut.Advance(4);
             Assert.Collection(sut.Ages,
                 p1 => Assert.Equal(6, p1),
                 p2 => Assert.Equal(8, p2));
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            var sut = new AgeModel("3");
+            sut.Advance(5);
+            Assert.Collection(sut.Ages,
+                p1 => Assert.Equal(5, p1),
+                p2 => Assert.Equal(7, p2));
         }
 
         [Fact]
@@ -65,6 +76,64 @@ namespace Day6.UnitTests
                 p3 => Assert.Equal(2, p3),
                 p4 => Assert.Equal(0, p4),
                 p5 => Assert.Equal(1, p5));
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var sut = new AgeModel(SAMPLE_AGES);
+            sut.Advance(2);
+            Assert.Collection(sut.Ages,
+                p1 => Assert.Equal(1, p1),
+                p2 => Assert.Equal(2, p2),
+                p3 => Assert.Equal(1, p3),
+                p4 => Assert.Equal(6, p4),
+                p5 => Assert.Equal(0, p5),
+                p6 => Assert.Equal(8, p6));
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var sut = new AgeModel(SAMPLE_AGES);
+            sut.Advance(18);
+            Assert.Collection(sut.Ages,
+                p1 => Assert.Equal(6, p1),
+                p2 => Assert.Equal(0, p2),
+                p3 => Assert.Equal(6, p3),
+                p4 => Assert.Equal(4, p4),
+                p5 => Assert.Equal(5, p5),
+                p6 => Assert.Equal(6, p6),
+                p7 => Assert.Equal(0, p7),
+                p8 => Assert.Equal(1, p8),
+                p9 => Assert.Equal(1, p9),
+                p10 => Assert.Equal(2, p10),
+                p11 => Assert.Equal(6, p11),
+                p12 => Assert.Equal(0, p12),
+                p13 => Assert.Equal(1, p13),
+                p14 => Assert.Equal(1, p14),
+                p15 => Assert.Equal(1, p15),
+                p16 => Assert.Equal(2, p16),
+                p17 => Assert.Equal(2, p17),
+                p18 => Assert.Equal(3, p18),
+                p19 => Assert.Equal(3, p19),
+                p20 => Assert.Equal(4, p20),
+                p21 => Assert.Equal(6, p21),
+                p22 => Assert.Equal(7, p22),
+                p23 => Assert.Equal(8, p23),
+                p24 => Assert.Equal(8, p24),
+                p25 => Assert.Equal(8, p25),
+                p26 => Assert.Equal(8, p26));
+        }
+
+        [Theory]
+        [InlineData(18, 26)]
+        [InlineData(80, 5934)]
+        public void Test4(int days, int expectedFishes)
+        {
+            var sut = new AgeModel(SAMPLE_AGES);
+            sut.Advance(days);
+            Assert.Equal(expectedFishes, sut.Ages.Count);
         }
     }
 }
