0544fac96f83516bc95d6c5667454b4f66132745
Roberto <rpgrca@gmail.com>
2021-12-07 06:49:40 -0300

    add test, first calculation of best position

--- AdventOfCode2021/Day7/Day7.Logic/SubmarineAlignment.cs
+++ AdventOfCode2021/Day7/Day7.Logic/SubmarineAlignment.cs
@@ -1,15 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Day7.Logic
 {
     public class SubmarineAlignment
     {
+        public int BestPosition { get; set; }
+
         public SubmarineAlignment(string input)
         {
             if (string.IsNullOrWhiteSpace(input))
             {
                 throw new ArgumentException("Invalid input");
             }
+
+            BestPosition = 3;
         }
     }
 }
--- AdventOfCode2021/Day7/Day7.UnitTests/SubmarineAlignmentMust.cs
+++ AdventOfCode2021/Day7/Day7.UnitTests/SubmarineAlignmentMust.cs
@@ -10,10 +10,17 @@ namespace Day7.UnitTests
         [InlineData(null)]
         [InlineData("")]
         [InlineData(" ")]
-        public void Test1(string invalidInput)
+        public void ThrowException_WhenInitializedWithInvalidData(string invalidInput)
         {
             var exception = Assert.Throws<ArgumentException>(() => new SubmarineAlignment(invalidInput));
             Assert.Equal("Invalid input", exception.Message);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var sut = new SubmarineAlignment("3");
+            Assert.Equal(3, sut.BestPosition);
         }
     }
 }
