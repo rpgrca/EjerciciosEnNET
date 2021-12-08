3dc5ae9af2bdd878ec786ba3bf660bf285edf1a9
Roberto <rpgrca@gmail.com>
2021-12-08 10:55:13 -0300

    add more test cases, rename tests

--- AdventOfCode2021/Day8/Day8.UnitTests/DisplayWiringMust.cs
+++ AdventOfCode2021/Day8/Day8.UnitTests/DisplayWiringMust.cs
@@ -2,6 +2,7 @@ using System;
 using Xunit;
 using Day8.Logic;
 using static Day8.UnitTests.Constants;
+using System.Collections.Generic;
 
 namespace Day8.UnitTests
 {
@@ -52,44 +53,48 @@ edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf 
                 p4 => Assert.Equal("abcdf", p4));
         }
 
-        [Fact]
-        public void Test1()
+        [Theory]
+        [MemberData(nameof(DecodingDisplaySamples))]
+        public void DecodeDisplaySegmentsCorrectly(string data, char wireA, char wireB, char wireC, char wireD, char wireE, char wireF, char wireG)
         {
-            var sut = new DisplayWiring("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf");
+            var sut = new DisplayWiring(data);
             Assert.Collection(sut.GetWiringForDisplay(0),
-                p1 => Assert.Equal('d', p1),
-                p2 => Assert.Equal('e', p2),
-                p3 => Assert.Equal('a', p3),
-                p4 => Assert.Equal('f', p4),
-                p5 => Assert.Equal('g', p5),
-                p6 => Assert.Equal('b', p6),
-                p7 => Assert.Equal('c', p7));
+                p1 => Assert.Equal(wireA, p1),
+                p2 => Assert.Equal(wireB, p2),
+                p3 => Assert.Equal(wireC, p3),
+                p4 => Assert.Equal(wireD, p4),
+                p5 => Assert.Equal(wireE, p5),
+                p6 => Assert.Equal(wireF, p6),
+                p7 => Assert.Equal(wireG, p7));
         }
 
-        [Fact]
-        public void Test2()
+        public static IEnumerable<object[]> DecodingDisplaySamples()
         {
-            var sut = new DisplayWiring("be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe");
-            Assert.Collection(sut.GetWiringForDisplay(0),
-                p1 => Assert.Equal('d', p1),
-                p2 => Assert.Equal('g', p2),
-                p3 => Assert.Equal('b', p3),
-                p4 => Assert.Equal('c', p4),
-                p5 => Assert.Equal('a', p5),
-                p6 => Assert.Equal('e', p6),
-                p7 => Assert.Equal('f', p7));
+            yield return new object[] { "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf", 'd', 'e', 'a', 'f', 'g', 'b', 'c' };
+            yield return new object[] { "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe", 'd', 'g', 'b', 'c', 'a', 'e', 'f' };
         }
 
-        [Fact]
-        public void Test3()
+        [Theory]
+        [InlineData("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf", 5353)]
+        [InlineData("be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe", 8394)]
+        [InlineData("edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc", 9781)]
+        [InlineData("fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg", 1197)]
+        [InlineData("fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb", 9361)]
+        [InlineData("aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea", 4873)]
+        [InlineData("fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb", 8418)]
+        [InlineData("dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe", 4548)]
+        [InlineData("bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef", 1625)]
+        [InlineData("egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb", 8717)]
+        [InlineData("gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce", 4315)]
+        public void ReturnFixedDisplay(string data, int expectedDisplay)
         {
-            var sut = new DisplayWiring("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf");
+            var sut = new DisplayWiring(data);
             sut.GetWiringForDisplay(0);
-            Assert.Equal(5353, sut.GetFixedDisplayFor(0));
+            Assert.Equal(expectedDisplay, sut.GetFixedDisplayFor(0));
         }
 
         [Fact]
-        public void Test4()
+        public void ReturnSumOfDisplays_WhenUsingSampleScannings()
         {
             var sut = new DisplayWiring(SAMPLE_SCANNINGS);
             Assert.Equal(61229, sut.GetSumOfDisplays());
