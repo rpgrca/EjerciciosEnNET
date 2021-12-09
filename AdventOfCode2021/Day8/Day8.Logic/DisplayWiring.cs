86cf4f84830f3997027be891ed635dd48a29cb9c
Roberto <rpgrca@gmail.com>
2021-12-08 23:25:48 -0300

    use bit operations instead of strings

--- AdventOfCode2021/Day8/Day8.Logic/DisplayWiring.cs
+++ AdventOfCode2021/Day8/Day8.Logic/DisplayWiring.cs
@@ -7,27 +7,26 @@ namespace Day8.Logic
     {
         private readonly List<string> _signals;
         private readonly List<string> _display;
-        private readonly List<string> _numbers;
-        private readonly Dictionary<char, char> _segments;
+        private readonly List<uint> _numbers;
+        private readonly Dictionary<char, uint> _segments;
 
         public int Value { get; private set; }
-        public List<char> Wiring => _segments.Values.ToList();
 
         public DisplayWiring(List<string> signals, List<string> display)
         {
             _signals = signals;
             _display = display;
 
-            _numbers = new List<string>();
-            _segments = new Dictionary<char, char>
+            _numbers = new List<uint>();
+            _segments = new Dictionary<char, uint>
             {
-                { 'a', '\0' },
-                { 'b', '\0' },
-                { 'c', '\0' },
-                { 'd', '\0' },
-                { 'e', '\0' },
-                { 'f', '\0' },
-                { 'g', '\0' }
+                { 'a', 0 },
+                { 'b', 0 },
+                { 'c', 0 },
+                { 'd', 0 },
+                { 'e', 0 },
+                { 'f', 0 },
+                { 'g', 0 }
             };
 
             ClassifyNumbers();
@@ -35,19 +34,21 @@ namespace Day8.Logic
         }
 
         private void ClassifyNumbers() =>
-            _numbers.AddRange(new List<string>
+            _numbers.AddRange(new List<uint>
             {
-                { null },
-                { string.Join("", _signals.Single(p => p.Length == 2).OrderBy(p => p)) },
-                { null },
-                { null },
-                { string.Join("", _signals.Single(p => p.Length == 4).OrderBy(p => p)) },
-                { null },
-                { null },
-                { string.Join("", _signals.Single(p => p.Length == 3).OrderBy(p => p)) },
-                { string.Join("", _signals.Single(p => p.Length == 7).OrderBy(p => p)) },
-                { null }
+                { 0 },
+                { StringToByte(_signals.Single(p => p.Length == 2)) },
+                { 0 },
+                { 0 },
+                { StringToByte(_signals.Single(p => p.Length == 4)) },
+                { 0 },
+                { 0 },
+                { StringToByte(_signals.Single(p => p.Length == 3)) },
+                { StringToByte(_signals.Single(p => p.Length == 7)) },
+                { 0 }
             });
+
+        private uint StringToByte(string value) => value.Aggregate(0U, (t, i) => t |= 1U << (i - 'a'));
 
         private void CalculateWirings()
         {
@@ -63,7 +64,7 @@ namespace Day8.Logic
             DeduceNumber6();
             DeduceNumber9();
 
-            var fixedNumbers = new Dictionary<string, string>
+            var fixedNumbers = new Dictionary<uint, string>
             {
                 { _numbers[0], "0" },
                 { _numbers[1], "1" },
@@ -77,62 +78,62 @@ namespace Day8.Logic
                 { _numbers[9], "9" }
             };
 
-            Value = int.Parse(string.Concat(_display.Select(p => fixedNumbers[p])));
+            Value = int.Parse(string.Concat(_display.Select(p => fixedNumbers[StringToByte(p)])));
         }
 
         private void DeduceSegmentA() =>
             // 2) Si comparo el 7 con el 1, el segmento en el 7 que no está en el 1 es el segmento AA
-            _segments['a'] = _numbers[7].Except(_numbers[1]).Single();
+            _segments['a'] = _numbers[7] ^ _numbers[1];
 
         private void DeduceNumbersTwoThreeAndFive()
         {
             // 4) Tomo los numeros con 5 segmentos (2, 3, 5), a cada uno le quito los segmentos existentes en los
             // otros y el que queda vacío es el 3
-            var possibleTwoThreeFive = _signals.Where(p => p.Length == 5).Select(p => p).ToList();
-            var a1 = possibleTwoThreeFive[0].Except(possibleTwoThreeFive[1]).Except(possibleTwoThreeFive[2]);
-            if (!a1.Any())
+            var possibleTwoThreeFive = _signals.Where(p => p.Length == 5).Select(p => StringToByte(p)).ToList();
+            var a1 = possibleTwoThreeFive[0] & ~possibleTwoThreeFive[1] & ~possibleTwoThreeFive[2];
+            if (a1 == 0)
             {
-                _numbers[3] = string.Concat(possibleTwoThreeFive[0].OrderBy(p => p));
-                if (! possibleTwoThreeFive[1].Except(_numbers[3]).Except(_numbers[4]).Any())
+                _numbers[3] = possibleTwoThreeFive[0];
+                if ((possibleTwoThreeFive[1] & ~_numbers[3] & ~_numbers[4]) == 0)
                 {
-                    _numbers[5] = string.Concat(possibleTwoThreeFive[1].OrderBy(p => p));
-                    _numbers[2] = string.Concat(possibleTwoThreeFive[2].OrderBy(p => p));
+                    _numbers[5] = possibleTwoThreeFive[1];
+                    _numbers[2] = possibleTwoThreeFive[2];
                 }
                 else
                 {
-                    _numbers[5] = string.Concat(possibleTwoThreeFive[2].OrderBy(p => p));
-                    _numbers[2] = string.Concat(possibleTwoThreeFive[1].OrderBy(p => p));
+                    _numbers[5] = possibleTwoThreeFive[2];
+                    _numbers[2] = possibleTwoThreeFive[1];
                 }
             }
             else
             {
-                var a2 = possibleTwoThreeFive[1].Except(possibleTwoThreeFive[0]).Except(possibleTwoThreeFive[2]);
-                if (!a2.Any())
+                var a2 = possibleTwoThreeFive[1] & ~possibleTwoThreeFive[0] & ~possibleTwoThreeFive[2];
+                if (a2 == 0)
                 {
                     _numbers[3] = possibleTwoThreeFive[1];
-                    if (! possibleTwoThreeFive[0].Except(_numbers[3]).Except(_numbers[4]).Any())
+                    if ((possibleTwoThreeFive[0] & ~_numbers[3] & ~_numbers[4]) == 0)
                     {
-                        _numbers[5] = string.Concat(possibleTwoThreeFive[0].OrderBy(p => p));
-                        _numbers[2] = string.Concat(possibleTwoThreeFive[2].OrderBy(p => p));
+                        _numbers[5] = possibleTwoThreeFive[0];
+                        _numbers[2] = possibleTwoThreeFive[2];
                     }
                     else
                     {
-                        _numbers[5] = string.Concat(possibleTwoThreeFive[2].OrderBy(p => p));
-                        _numbers[2] = string.Concat(possibleTwoThreeFive[0].OrderBy(p => p));
+                        _numbers[5] = possibleTwoThreeFive[2];
+                        _numbers[2] = possibleTwoThreeFive[0];
                     }
                 }
                 else
                 {
-                    _numbers[3] = string.Concat(possibleTwoThreeFive[2].OrderBy(p => p));
-                    if (! possibleTwoThreeFive[0].Except(_numbers[3]).Except(_numbers[4]).Any())
+                    _numbers[3] = possibleTwoThreeFive[2];
+                    if ((possibleTwoThreeFive[0] & ~_numbers[3] & ~_numbers[4]) == 0)
                     {
-                        _numbers[5] = string.Concat(possibleTwoThreeFive[0].OrderBy(p => p));
-                        _numbers[2] = string.Concat(possibleTwoThreeFive[1].OrderBy(p => p));
+                        _numbers[5] = possibleTwoThreeFive[0];
+                        _numbers[2] = possibleTwoThreeFive[1];
                     }
                     else
                     {
-                        _numbers[5] = string.Concat(possibleTwoThreeFive[1].OrderBy(p => p));
-                        _numbers[2] = string.Concat(possibleTwoThreeFive[0].OrderBy(p => p));
+                        _numbers[5] = possibleTwoThreeFive[1];
+                        _numbers[2] = possibleTwoThreeFive[0];
                     }
                 }
             }
@@ -140,31 +141,34 @@ namespace Day8.Logic
 
         private void DeduceSegmentG() =>
             // 4.1) Tomo al 3 y le saco los segmentos del 7 y del 4, me queda el segmento GG
-            _segments['g'] = _numbers[3].Except(_numbers[7]).Except(_numbers[4]).Single();
+            _segments['g'] = _numbers[3] & ~_numbers[7] & ~_numbers[4];
 
         private void DeduceSegmentE() =>
             // 4.2) Tomo al 2, le resto el 3 y obtengo el segmento EE
-            _segments['e'] = _numbers[2].Except(_numbers[3]).Single();
+            _segments['e'] = _numbers[2] & ~_numbers[3];
 
         private void DeduceSegmentB() =>
-            _segments['b'] = _numbers[5].Except(_numbers[3]).Single();
+            _segments['b'] = _numbers[5] & ~_numbers[3];
 
         private void DeduceSegmentC() =>
-            _segments['c'] = _numbers[4].Except(_numbers[5]).Single();
+            _segments['c'] = _numbers[4] & ~_numbers[5];
 
         private void DeduceSegmentD() =>
-            _segments['d'] = _numbers[4].Except(_numbers[1]).Except(new List<char>() { _segments['b'] }).Single();
+            _segments['d'] = _numbers[4] & ~_numbers[1] & ~_segments['b'];
 
         private void DeduceSegmentF() =>
-            _segments['f'] = _numbers[1].Except(new List<char>() { _segments['c']}).Single();
+            _segments['f'] = _numbers[1] & ~_segments['c'];
 
         private void DeduceNumber0() =>
-            _numbers[0] = string.Concat(new List<char> { _segments['a'], _segments['b'], _segments['c'], _segments['e'], _segments['f'], _segments['g']}.OrderBy(p => p));
+            _numbers[0] = _segments['a'] | _segments['b'] | _segments['c'] | _segments['e'] | _segments['f'] | _segments['g'];
 
         private void DeduceNumber6() =>
-            _numbers[6] = string.Concat(new List<char> { _segments['a'], _segments['b'], _segments['d'], _segments['e'], _segments['f'], _segments['g'] }.OrderBy(p => p));
+            _numbers[6] = _segments['a'] | _segments['b'] | _segments['d'] | _segments['e'] | _segments['f'] | _segments['g'];
 
         private void DeduceNumber9() =>
-            _numbers[9] = string.Concat(new List<char> { _segments['a'], _segments['b'], _segments['c'], _segments['d'], _segments['f'], _segments['g'] }.OrderBy(p => p));
+            _numbers[9] = _segments['a'] | _segments['b'] | _segments['c'] | _segments['d'] | _segments['f'] | _segments['g'];
+
+        public List<char> Wiring =>
+            _segments.Select(p => (char)('a' + System.Numerics.BitOperations.Log2((uint)p.Value))).ToList();
     }
 }
