09c4461c66bab9ff48159d3464546b2c1252ab24
Roberto <rpgrca@gmail.com>
2021-12-09 08:26:55 -0300

    add test, parse input correctly

--- AdventOfCode2021/Day9/Day9.Logic/HeightMap.cs
+++ AdventOfCode2021/Day9/Day9.Logic/HeightMap.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using System.Linq;
+using System;
+using System.Collections.Generic;
 
 namespace Day9.Logic
 {
     public class HeightMap
     {
         private readonly string _data;
+        private readonly List<int[]> _map;
+
+        public int Width => _map[0].Length;
+        public int Height => _map.Count;
 
         public HeightMap(string data)
         {
@@ -14,6 +20,17 @@ namespace Day9.Logic
             }
 
             _data = data;
+            _map = new List<int[]>();
+
+            Parse();
+        }
+
+        private void Parse()
+        {
+            foreach (var row in _data.Split("\n").ToList())
+            {
+                _map.Add(row.Select(p => p - '0').ToArray());
+            }
         }
     }
 }
--- AdventOfCode2021/Day9/Day9.UnitTests/HeightMapMust.cs
+++ AdventOfCode2021/Day9/Day9.UnitTests/HeightMapMust.cs
@@ -1,6 +1,7 @@
 using System;
 using Xunit;
 using Day9.Logic;
+using static Day9.UnitTests.Constant;
 
 namespace Day9.UnitTests
 {
@@ -14,6 +15,16 @@ namespace Day9.UnitTests
         {
             var exception = Assert.Throws<ArgumentException>(() => new HeightMap(invalidData));
             Assert.Equal("Invalid data", exception.Message);
+        }
+
+        [Theory]
+        [InlineData("3335\n3335\n3335", 4, 3)]
+        [InlineData(SAMPLE_HEIGHTMAP, 10, 5)]
+        public void Test1(string map, int expectedWidth, int expectedHeight)
+        {
+            var sut = new HeightMap(map);
+            Assert.Equal(expectedWidth, sut.Width);
+            Assert.Equal(expectedHeight, sut.Height);
         }
     }
 }
