4e6eef67d7058f3f0d9da8e81bd8b7416240a01c
Roberto <rpgrca@gmail.com>
2021-12-09 09:51:23 -0300

    refactoring

--- AdventOfCode2021/Day9/Day9.Logic/HeightMap.cs
+++ AdventOfCode2021/Day9/Day9.Logic/HeightMap.cs
@@ -8,13 +8,13 @@ namespace Day9.Logic
     {
         private readonly string _data;
         private readonly List<int[]> _map;
+        private readonly List<int> _basins;
         private readonly List<(int X, int Y)> _lowPoints;
 
         public int Width => _map[0].Length;
         public int Height => _map.Count;
 
         public int RiskLevel { get; private set; }
-        public List<int> Basins { get; private set; }
 
         public HeightMap(string data)
         {
@@ -26,24 +26,24 @@ namespace Day9.Logic
             _data = data;
             _map = new List<int[]>();
             _lowPoints = new List<(int X, int Y)>();
-            Basins = new List<int>();
+            _basins = new List<int>();
 
             Parse();
+            CalculateLowPoints();
             CalculateRiskLevel();
             CalculateBasinSizes();
         }
 
         private void Parse()
         {
-            foreach (var row in _data.Split("\n").ToList())
+            foreach (var row in _data.Split("\n"))
             {
                 _map.Add(row.Select(p => p - '0').ToArray());
             }
         }
 
-        private void CalculateRiskLevel()
+        private void CalculateLowPoints()
         {
-            RiskLevel = 0;
             for (var y = 0; y < Height; y++)
             {
                 for (var x = 0; x < Width; x++)
@@ -51,11 +51,13 @@ namespace Day9.Logic
                     if (IsLowPoint(x, y))
                     {
                         _lowPoints.Add((x, y));
-                        RiskLevel += _map[y][x] + 1;
                     }
                 }
             }
         }
+
+        private void CalculateRiskLevel() =>
+            RiskLevel = _lowPoints.Select(p => _map[p.Y][p.X] + 1).Sum();
 
         private bool IsLowPoint(int x, int y)
         {
@@ -67,46 +69,28 @@ namespace Day9.Logic
                 (x + 1, y)
             };
 
-            var isLowPoint = true;
-            foreach (var adjacentLocation in adjacentLocations)
+            foreach (var (X, Y) in adjacentLocations)
             {
-                if (adjacentLocation.X < 0 || adjacentLocation.X >= Width || adjacentLocation.Y < 0 || adjacentLocation.Y >= Height)
-                {
-                    continue;
-                }
+                if (OutOfBounds(X, Y)) continue;
 
-                if (_map[y][x] >= _map[adjacentLocation.Y][adjacentLocation.X])
+                if (_map[y][x] >= _map[Y][X])
                 {
-                    isLowPoint = false;
-                    break;
+                    return false;
                 }
             }
 
-            return isLowPoint;
+            return true;
         }
 
-        private void CalculateBasinSizes()
-        {
-            foreach (var lowPoint in _lowPoints)
-            {
-                var basinSize = CalculateBasinSize(lowPoint.X, lowPoint.Y);
-                Basins.Add(basinSize);
-            }
-        }
+        private bool OutOfBounds(int x, int y) =>
+            x < 0 || x >= Width || y < 0 || y >= Height;
+
+        private void CalculateBasinSizes() =>
+            _lowPoints.ForEach(p => _basins.Add(CalculateBasinSize(p.X, p.Y)));
 
         private int CalculateBasinSize(int x, int y)
         {
-            if (x < 0 || x >= Width || y < 0 || y >= Height)
-            {
-                return 0;
-            }
-
-            if (_map[y][x] == 9)
-            {
-                return 0;
-            }
-
-            if (_map[y][x] == -1)
+            if (OutOfBounds(x, y) || _map[y][x] == 9 || _map[y][x] == -1)
             {
                 return 0;
             }
@@ -115,6 +99,11 @@ namespace Day9.Logic
             return 1 + CalculateBasinSize(x - 1, y) + CalculateBasinSize(x + 1, y) + CalculateBasinSize(x, y - 1) + CalculateBasinSize(x, y + 1);
         }
 
-        public int GetBasinMultiplication() => Basins.OrderByDescending(p => p).Take(3).Aggregate(1, (t, i) => t *= i);
+        public int GetBasinSizeFor(int index) => _basins[index];
+
+        public int GetBasinMultiplication() => _basins
+            .OrderByDescending(p => p)
+            .Take(3)
+            .Aggregate(1, (t, i) => t *= i);
     }
 }
--- AdventOfCode2021/Day9/Day9.UnitTests/HeightMapMust.cs
+++ AdventOfCode2021/Day9/Day9.UnitTests/HeightMapMust.cs
@@ -44,27 +44,25 @@ namespace Day9.UnitTests
             Assert.Equal(480, sut.RiskLevel);
         }
 
-        [Theory]
-        [InlineData("99999\n93339\n93239\n93339\n99999", 9)]
-        public void Test2(string map, int expectedBasinSize)
+        [Fact]
+        public void CalculateBasinSizeCorrectly_WhenThereIsOnlyOne()
         {
-            var sut = new HeightMap(map);
-            Assert.Equal(expectedBasinSize, sut.Basins[0]);
+            var sut = new HeightMap("99999\n93339\n93239\n93339\n99999");
+            Assert.Equal(9, sut.GetBasinSizeFor(0));
         }
 
         [Fact]
-        public void Test3()
+        public void CalculateBasinSizesCorrectly_WhenUsingSampleMap()
         {
             var sut = new HeightMap(SAMPLE_HEIGHTMAP);
-            Assert.Collection(sut.Basins,
-                p1 => Assert.Equal(3, p1),
-                p2 => Assert.Equal(9, p2),
-                p3 => Assert.Equal(14, p3),
-                p4 => Assert.Equal(9, p4));
+            Assert.Equal(3, sut.GetBasinSizeFor(0));
+            Assert.Equal(9, sut.GetBasinSizeFor(1));
+            Assert.Equal(14, sut.GetBasinSizeFor(2));
+            Assert.Equal(9, sut.GetBasinSizeFor(3));
         }
 
         [Fact]
-        public void CalculateBasinMultiplicationCorrectly()
+        public void CalculateBasinMultiplicationCorrectly_WhenUsingSampleMap()
         {
             var sut = new HeightMap(SAMPLE_HEIGHTMAP);
             Assert.Equal(1134, sut.GetBasinMultiplication());
