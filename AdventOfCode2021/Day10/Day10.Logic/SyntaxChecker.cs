03110e5f7f78062971eacf54647aeb22ffb4d824
Roberto <rpgrca@gmail.com>
2021-12-10 07:09:23 -0300

    add tests, check for invalid lines in input

--- AdventOfCode2021/Day10/Day10.Logic/SyntaxChecker.cs
+++ AdventOfCode2021/Day10/Day10.Logic/SyntaxChecker.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Day10.Logic
 {
     public class SyntaxChecker
     {
         private readonly string _code;
+        private readonly List<string> _validLines;
+        private readonly List<string> _invalidLines;
 
         public SyntaxChecker(string code)
         {
@@ -13,7 +16,80 @@ namespace Day10.Logic
                 throw new ArgumentException("Invalid code");
             }
 
+            _validLines = new List<string>();
+            _invalidLines = new List<string>();
             _code = code;
+
+            Parse();
         }
+
+        private void Parse()
+        {
+            List<char> stack = new();
+            var line = string.Empty;
+            var isInvalid = false;
+
+            foreach (var character in _code)
+            {
+                line += character;
+
+                switch (character)
+                {
+                    case '(':
+                    case '{':
+                    case '[':
+                    case '<':
+                        stack.Insert(0, character);
+                        break;
+
+                    case ')':
+                        if (stack[0] != '(')
+                        {
+                            isInvalid = true;
+                        }
+                        stack.RemoveAt(0);
+                        break;
+
+                    case ']':
+                        if (stack[0] != '[')
+                        {
+                            isInvalid = true;
+                        }
+                        stack.RemoveAt(0);
+                        break;
+
+                    case '}':
+                        if (stack[0] != '{')
+                        {
+                            isInvalid = true;
+                        }
+                        stack.RemoveAt(0);
+                        break;
+
+                    case '>':
+                        if (stack[0] != '<')
+                        {
+                            isInvalid = true;
+                        }
+                        stack.RemoveAt(0);
+                        break;
+                }
+
+                if (stack.Count == 0)
+                {
+                    if (isInvalid)
+                    {
+                        _invalidLines.Add(line);
+                        isInvalid = false;
+                    }
+                    else
+                    {
+                        _validLines.Add(line);
+                    }
+                }
+            }
+        }
+
+        public List<string> GetSyntaxErrors() => _invalidLines;
     }
 }
--- AdventOfCode2021/Day10/Day10.UnitTests/SyntaxCheckerMust.cs
+++ AdventOfCode2021/Day10/Day10.UnitTests/SyntaxCheckerMust.cs
@@ -10,10 +10,36 @@ namespace Day10.UnitTests
         [InlineData(null)]
         [InlineData("")]
         [InlineData("  ")]
-        public void Test1(string invalidCode)
+        public void ThrowException_WhenInvalidCodeIsUsed(string invalidCode)
         {
             var exception = Assert.Throws<ArgumentException>(() => new SyntaxChecker(invalidCode));
             Assert.Equal("Invalid code", exception.Message);
+        }
+
+        [Theory]
+        [InlineData("()")]
+        [InlineData("[]")]
+        [InlineData("([])")]
+        [InlineData("{()()()}")]
+        [InlineData("<([{}])>")]
+        [InlineData("[<>({}){}[([])<>]]")]
+        [InlineData("(((((((((())))))))))")]
+        public void Test1(string input)
+        {
+            var sut = new SyntaxChecker(input);
+            Assert.Empty(sut.GetSyntaxErrors());
+        }
+
+        [Theory]
+        [InlineData("(]")]
+        [InlineData("{()()()>")]
+        [InlineData("(((()))}")]
+        [InlineData("<([]){()}[{}])")]
+        public void Test2(string input)
+        {
+            var sut = new SyntaxChecker(input);
+            Assert.Collection(sut.GetSyntaxErrors(),
+                p1 => Assert.Equal(input, p1));
         }
     }
 }
