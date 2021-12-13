aee47a3ba4b0c02cfa12686059298e2d230e826d
Roberto <rpgrca@gmail.com>
2021-12-13 07:49:32 -0300

    add test, validate input

--- AdventOfCode2021/Day13/Day13.Logic/Origami.cs
+++ AdventOfCode2021/Day13/Day13.Logic/Origami.cs
@@ -4,5 +4,16 @@ namespace Day13.Logic
 {
     public class Origami
     {
+        private readonly string _instructions;
+
+        public Origami(string instructions)
+        {
+            if (string.IsNullOrWhiteSpace(instructions))
+            {
+                throw new ArgumentException("Invalid instructions");
+            }
+
+            _instructions = instructions;
+        }
     }
 }
--- AdventOfCode2021/Day13/Day13.UnitTests/OrigamiMust.cs
+++ AdventOfCode2021/Day13/Day13.UnitTests/OrigamiMust.cs
@@ -6,5 +6,14 @@ namespace Day13.UnitTests
 {
     public class OrigamiMust
     {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void ThrowException_WhenInitializedWithInvalidInstructions(string invalidInstructions)
+        {
+            var exception = Assert.Throws<ArgumentException>(() => new Origami(invalidInstructions));
+            Assert.Equal("Invalid instructions", exception.Message);
+        }
     }
 }
