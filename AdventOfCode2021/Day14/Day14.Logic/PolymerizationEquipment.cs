327cc926c399866a1412c6bed829bbf3ee750ad4
Roberto <rpgrca@gmail.com>
2021-12-14 11:27:10 -0300

    solve second puzzle

--- AdventOfCode2021/Day14/Day14.Logic/PolymerizationEquipment.cs
+++ AdventOfCode2021/Day14/Day14.Logic/PolymerizationEquipment.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Text;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,7 +8,9 @@ namespace Day14.Logic
     public class PolymerizationEquipment
     {
         private readonly string _input;
-        private readonly Dictionary<string, string> _rules;
+        private readonly Dictionary<string, char> _rules;
+        private readonly Dictionary<string, Dictionary<char, long>> _count;
+        private long _substraction;
 
         public string PolymerTemplate { get; private set; }
 
@@ -19,7 +22,8 @@ namespace Day14.Logic
             }
 
             _input = input;
-            _rules = new Dictionary<string, string>();
+            _rules = new Dictionary<string, char>();
+            _count = new Dictionary<string, Dictionary<char, long>>();
 
             Parse();
         }
@@ -28,7 +32,7 @@ namespace Day14.Logic
         {
             var input = _input.Split("\n");
             PolymerTemplate = input[0];
-            input[2..].Select(p => p.Split(" -> ")).ToList().ForEach(p => _rules.Add(p[0], p[1]));
+            input[2..].Select(p => p.Split(" -> ")).ToList().ForEach(p => _rules.Add(p[0], p[1][0]));
         }
 
         public int GetPairInsertionRulesCount() => _rules.Keys.Count;
@@ -39,26 +43,89 @@ namespace Day14.Logic
             {
                 Step();
             }
+
+            var groups = PolymerTemplate.GroupBy(p => p);
+            var values = groups.Select(p => new { Key = p.Key, Count = p.Count() }).ToList();
+            _substraction = groups.Max(p => p.Count()) - groups.Min(p => p.Count());
+
+//            Step2(count);
         }
 
         private void Step()
         {
-            var newPolymer = PolymerTemplate[0].ToString();
+            var stringBuilder = new StringBuilder(PolymerTemplate[0].ToString());
 
             for (var index = 0; index < PolymerTemplate.Length - 1; index++)
             {
-                newPolymer += _rules[PolymerTemplate[index..(index + 2)]] + PolymerTemplate[index + 1];
+                stringBuilder
+                    .Append(_rules[PolymerTemplate[index..(index + 2)]])
+                    .Append(PolymerTemplate[index + 1]);
             }
 
-            PolymerTemplate = newPolymer;
+            PolymerTemplate = stringBuilder.ToString();
         }
 
-        public int CountElementInTemplate(string value) => PolymerTemplate.Count(p => p == value[0]);
-
-        public int GetSubstraction()
+        public void Step2(int count)
         {
-            var groups = PolymerTemplate.GroupBy(p => p);
-            return groups.Max(p => p.Count()) - groups.Min(p => p.Count());
+            var counter = new Dictionary<char, long>();
+            var cache = new Dictionary<string, Dictionary<char, long>>();
+
+            for (var index = 0; index < PolymerTemplate.Length - 1; index++)
+            {
+                CountValues(count + 1, PolymerTemplate[index], PolymerTemplate[index + 1], counter, cache);
+            }
+
+            foreach (var value in PolymerTemplate)
+            {
+                counter[value]++;
+            }
+
+            _substraction = counter.Max(p => p.Value) - counter.Min(p => p.Value);
         }
+
+        private void CountValues(int count, char first, char second, Dictionary<char, long> counter, Dictionary<string, Dictionary<char, long>> cache)
+        {
+            count--;
+            if (count == 0)
+            {
+                return;
+            }
+
+            var newCharacter = _rules[$"{first}{second}"];
+            counter.TryAdd(newCharacter, 0);
+            counter[newCharacter]++;
+
+            var key = $"{first}{newCharacter}[{count}]";
+            if (!cache.ContainsKey(key))
+            {
+                var newCounter = new Dictionary<char, long>();
+                CountValues(count, first, newCharacter, newCounter, cache);
+                cache.Add(key, newCounter);
+            }
+
+            foreach (var (k, v) in cache[key])
+            {
+                counter.TryAdd(k, 0);
+                counter[k] += v;
+            }
+
+            key = $"{newCharacter}{second}[{count}]";
+            if (!cache.ContainsKey(key))
+            {
+                var newCounter = new Dictionary<char, long>();
+                CountValues(count, newCharacter, second, newCounter, cache);
+                cache.Add(key, newCounter);
+            }
+
+            foreach (var (k, v) in cache[key])
+            {
+                counter.TryAdd(k, 0);
+                counter[k] += v;
+            }
+        }
+
+        public long CountElementInTemplate(string value) => PolymerTemplate.Count(p => p == value[0]);
+
+        public long GetSubstraction() => _substraction;
     }
 }
