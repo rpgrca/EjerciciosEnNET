75b1d32a548ecf94ce61a7a4822daf22ff4410aa
Roberto <rpgrca@gmail.com>
2021-12-15 11:35:59 -0300

    solve second puzzle

--- AdventOfCode2021/Day15/Day15.Logic/CaveMap.cs
+++ AdventOfCode2021/Day15/Day15.Logic/CaveMap.cs
@@ -173,4 +173,173 @@ namespace Day15.Logic
             return _minimumRisk;
         }
     }
+
+    public class FullCaveMap
+    {
+        private readonly string _input;
+        private List<int> _minimumRiskPath;
+        private (int RiskLevel, int TotalRiskSoFar)[][] _map;
+        private int _minimumRisk;
+
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        public FullCaveMap(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                throw new ArgumentException("Invalid map");
+            }
+
+            _input = input;
+            _minimumRiskPath = new List<int>();
+
+            Parse();
+        }
+
+        private void Parse()
+        {
+            var lines = _input.Split("\n");
+            var index = 0;
+
+            _map = new (int, int)[lines.Length * 5][];
+            foreach (var line in lines)
+            {
+                var convertedLine = line.Select(p => (p - '0', int.MaxValue)).ToList();
+                convertedLine.AddRange(line.Select(p => (p - '0' + 1, int.MaxValue)));
+                convertedLine.AddRange(line.Select(p => (p - '0' + 2, int.MaxValue)));
+                convertedLine.AddRange(line.Select(p => (p - '0' + 3, int.MaxValue)));
+                convertedLine.AddRange(line.Select(p => (p - '0' + 4, int.MaxValue)));
+
+                _map[index++] = convertedLine.ToArray();
+            }
+
+            for (var y = 0; y < lines.Length; y++)
+            {
+                _map[index++] = _map[y].Select(p => (p.RiskLevel + 1, p.TotalRiskSoFar)).ToArray();
+            }
+
+            for (var y = 0; y < lines.Length; y++)
+            {
+                _map[index++] = _map[y].Select(p => (p.RiskLevel + 2, p.TotalRiskSoFar)).ToArray();
+            }
+
+            for (var y = 0; y < lines.Length; y++)
+            {
+                _map[index++] = _map[y].Select(p => (p.RiskLevel + 3, p.TotalRiskSoFar)).ToArray();
+            }
+
+            for (var y = 0; y < lines.Length; y++)
+            {
+                _map[index++] = _map[y].Select(p => (p.RiskLevel + 4, p.TotalRiskSoFar)).ToArray();
+            }
+
+            Height = _map.Length;
+            Width = _map[0].Length;
+
+            for (var y = 0; y < Height; y++)
+            {
+                for (var x = 0; x < Width; x++)
+                {
+                    if (_map[y][x].RiskLevel > 9)
+                    {
+                        _map[y][x].RiskLevel -= 9;
+                    }
+                }
+            }
+        }
+
+        public List<int> GetPath()
+        {
+            CalculateBorderPathRisk();
+            CalculateLowestRiskPath2();
+
+            return _minimumRiskPath;
+        }
+
+        private void CalculateBorderPathRisk()
+        {
+            foreach (var (RiskLevel, _) in _map[0][1..])
+            {
+                _minimumRisk += RiskLevel;
+            }
+
+            for (var index = 1; index < Height; index++)
+            {
+                _minimumRisk += _map[index][Width - 1].RiskLevel;
+            }
+        }
+
+        private void CalculateLowestRiskPath2()
+        {
+            CalculateLowestRiskPath2(0, 0, 0, true);
+        }
+
+        private void CalculateLowestRiskPath2(int x, int y, int accumulatedRiskLevel, bool firstTime)
+        {
+            if (! firstTime)
+            {
+                accumulatedRiskLevel += _map[y][x].RiskLevel;
+            }
+
+            if (accumulatedRiskLevel >= _minimumRisk)
+            {
+                return;
+            }
+
+            if (_map[y][x].TotalRiskSoFar <= accumulatedRiskLevel)
+            {
+                return;
+            }
+
+            _map[y][x].TotalRiskSoFar = accumulatedRiskLevel;
+
+            if (x == Width - 1 && y == Height - 1)
+            {
+                var sum = accumulatedRiskLevel;
+                if (sum < _minimumRisk)
+                {
+                    _minimumRisk = sum;
+                }
+
+                return;
+            }
+
+            var sortedList = new SortedList<int, (int X, int Y)>();
+            if (y > 0) sortedList.Add(_map[y - 1][x].RiskLevel << 16 | (y - 1) << 8 | x, (x, y - 1));
+            if (y + 1 < Height) sortedList.Add(_map[y + 1][x].RiskLevel << 16 | (y + 1) << 8 | x, (x, y + 1));
+            if (x > 0) sortedList.Add(_map[y][x - 1].RiskLevel << 16 | y << 8 | (x - 1), (x - 1, y));
+            if (x + 1 < Width) sortedList.Add(_map[y][x + 1].RiskLevel << 16 | y << 8 | (x + 1), (x + 1, y));
+
+ /*           if (y > 0) sortedList.Add(_map[y - 1][x].RiskLevel << 14 | (y - 1) << 7 | x, (x, y - 1));
+            if (y + 1 < Height) sortedList.Add(_map[y + 1][x].RiskLevel << 14 | (y + 1) << 7 | x, (x, y + 1));
+            if (x > 0) sortedList.Add(_map[y][x - 1].RiskLevel << 14 | y << 7 | (x - 1), (x - 1, y));
+            if (x + 1 < Width) sortedList.Add(_map[y][x + 1].RiskLevel << 14 | y << 7 | (x + 1), (x + 1, y));*/
+            foreach (var coordinates in sortedList)
+            {
+                CalculateLowestRiskPath2(coordinates.Value.X, coordinates.Value.Y, accumulatedRiskLevel, false);
+            }
+        }
+
+        public int GetPathLevel()
+        {
+            return _minimumRisk;
+        }
+
+        public string GetHorizontalLine(int index)
+        {
+            return _map[index].Aggregate(string.Empty, (t, i) => t += i.RiskLevel);
+        }
+
+        public string GetVerticalLine(int index)
+        {
+            var result = string.Empty;
+            for (var y = 0; y < Height; y++)
+            {
+                result += _map[y][index].RiskLevel;
+            }
+
+            return result;
+        }
+    }
 }
--- AdventOfCode2021/Day15/Day15.UnitTests/CaveMapMust.cs
+++ AdventOfCode2021/Day15/Day15.UnitTests/CaveMapMust.cs
@@ -80,5 +80,44 @@ namespace Day15.UnitTests
             Assert.True(sut.GetPathLevel() < 829);
             Assert.Equal(824, sut.GetPathLevel());
         }
+
+        [Fact]
+        public void CalculateFullMapCorrectly()
+        {
+            var sut = new FullCaveMap(SAMPLE_MAP);
+            Assert.Equal(50, sut.Width);
+        }
+
+        [Fact]
+        public void VerifyHorizontalCorrectness()
+        {
+            var sut = new FullCaveMap(SAMPLE_MAP);
+            Assert.Equal("11637517422274862853338597396444961841755517295286", sut.GetHorizontalLine(0));
+            Assert.Equal("67554889357866599146897761125791887223681299833479", sut.GetHorizontalLine(49));
+        }
+
+        [Fact]
+        public void VerifyVerticalCorrectness()
+        {
+            var sut = new FullCaveMap(SAMPLE_MAP);
+            Assert.Equal("11237113122234822423334593353444561446455567255756", sut.GetVerticalLine(0));
+            Assert.Equal("66345254557745636566885674767799678587881178969899", sut.GetVerticalLine(49));
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            var sut = new FullCaveMap(SAMPLE_MAP);
+            sut.GetPath();
+            Assert.Equal(315, sut.GetPathLevel());
+        }
+
+        [Fact]
+        public void SolveSecondPuzzle()
+        {
+            var sut = new FullCaveMap(REAL_MAP);
+            sut.GetPath();
+            Assert.Equal(3063, sut.GetPathLevel());
+        }
     }
 }
