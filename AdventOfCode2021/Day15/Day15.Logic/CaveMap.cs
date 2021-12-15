6066e26b5355476027c34da61e573271ac0179d9
Roberto <rpgrca@gmail.com>
2021-12-15 06:59:25 -0300

    add test, parse map correctly

--- AdventOfCode2021/Day15/Day15.Logic/CaveMap.cs
+++ AdventOfCode2021/Day15/Day15.Logic/CaveMap.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Linq;
 
 namespace Day15.Logic
 {
     public class CaveMap
     {
-        public CaveMap(string map)
+        private readonly string _input;
+        private int[][] _map;
+
+        public int Width { get; set; }
+
+        public CaveMap(string input)
         {
-            throw new ArgumentException("Invalid map");
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                throw new ArgumentException("Invalid map");
+            }
+
+            _input = input;
+
+            Parse();
+        }
+
+        private void Parse()
+        {
+            var lines = _input.Split("\n");
+            var index = 0;
+
+            _map = new int[lines.Length][];
+            foreach (var line in lines)
+            {
+                _map[index++] = line.Select(p => p - '0').ToArray();
+            }
+
+            Width = _map.Length;
         }
     }
 }
--- AdventOfCode2021/Day15/Day15.UnitTests/CaveMapMust.cs
+++ AdventOfCode2021/Day15/Day15.UnitTests/CaveMapMust.cs
@@ -1,6 +1,7 @@
 using System;
 using Xunit;
 using Day15.Logic;
+using static Day15.UnitTests.Constants;
 
 namespace Day15.UnitTests
 {
@@ -14,6 +15,15 @@ namespace Day15.UnitTests
         {
             var exception = Assert.Throws<ArgumentException>(() => new CaveMap(map));
             Assert.Equal("Invalid map", exception.Message);
+        }
+
+        [Theory]
+        [InlineData(SAMPLE_MAP, 10)]
+        [InlineData(REAL_MAP, 100)]
+        public void ParseMapCorrectly(string map, int expectedWidth)
+        {
+            var sut = new CaveMap(map);
+            Assert.Equal(expectedWidth, sut.Width);
         }
     }
 }
