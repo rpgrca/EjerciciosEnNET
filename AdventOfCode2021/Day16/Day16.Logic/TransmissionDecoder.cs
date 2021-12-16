dd58c40ce945c80a08378e1ee31138808c949567
Roberto <rpgrca@gmail.com>
2021-12-16 08:01:19 -0300

    add test, load a packet

--- AdventOfCode2021/Day16/Day16.Logic/TransmissionDecoder.cs
+++ AdventOfCode2021/Day16/Day16.Logic/TransmissionDecoder.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using System.Linq;
+using System;
+using System.Collections.Generic;
 
 namespace Day16.Logic
 {
     public class TransmissionDecoder
     {
         private readonly string _transmission;
+        private string _bits;
+
+        public List<Packet> Packets { get; }
 
         public TransmissionDecoder(string transmission)
         {
@@ -14,6 +19,44 @@ namespace Day16.Logic
             }
 
             _transmission = transmission;
+            Packets = new List<Packet>();
+
+            Parse();
         }
+
+        private void Parse()
+        {
+            _bits = _transmission.Select(p => p switch {
+                '0' => "0000",
+                '1' => "0001",
+                '2' => "0010",
+                '3' => "0011",
+                '4' => "0100",
+                '5' => "0101",
+                '6' => "0110",
+                '7' => "0111",
+                '8' => "1000",
+                '9' => "1001",
+                'A' => "1010",
+                'B' => "1011",
+                'C' => "1100",
+                'D' => "1101",
+                'E' => "1110",
+                _ => "1111"
+            }).Aggregate(string.Empty, (t, i) => t += i);
+
+            var packet = new Packet
+            {
+                Version = _bits[0..3],
+                TypeId = _bits[3..6]
+            };
+            Packets.Add(packet);
+        }
+    }
+
+    public class Packet
+    {
+        public string Version { get; set; }
+        public string TypeId { get; set; }
     }
 }
--- AdventOfCode2021/Day16/Day16.UnitTests/TransmissionDecoderMust.cs
+++ AdventOfCode2021/Day16/Day16.UnitTests/TransmissionDecoderMust.cs
@@ -15,5 +15,29 @@ namespace Day16.UnitTests
             var exception = Assert.Throws<ArgumentException>(() => new TransmissionDecoder(invalidTransmission));
             Assert.Equal("Invalid transmission", exception.Message);
         }
+
+        [Theory]
+        [InlineData("D2FE28", "110", "100")]
+        [InlineData("38006F45291200", "001", "110")]
+        [InlineData("EE00D40C823060", "111", "011")]
+        public void Test1(string transmission, string expectedVersion, string expectedTypeId)
+        {
+            var sut = new TransmissionDecoder(transmission);
+            Assert.Collection(sut.Packets,
+                p1 => {
+                    Assert.Equal(expectedVersion, p1.Version);
+                    Assert.Equal(expectedTypeId, p1.TypeId);
+                });
+        }
+
+/*
+                    Assert.Collection(p1.Literal,
+                        p11 => Assert.Equal("10111", p11),
+                        p12 => Assert.Equal("11110", p12),
+                        p13 => Assert.Equal("11110", p13),
+                        p14 => Assert.Equal("00101", p14),
+                        p15 => Assert.Equal("000", p15));
+
+*/
     }
 }
