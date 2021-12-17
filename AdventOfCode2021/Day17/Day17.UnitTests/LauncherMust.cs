ce26e083657ecc09014b5cef3fff98886e3cd5e7
Roberto <rpgrca@gmail.com>
2021-12-17 09:04:54 -0300

    add test cases

--- AdventOfCode2021/Day17/Day17.UnitTests/LauncherMust.cs
+++ AdventOfCode2021/Day17/Day17.UnitTests/LauncherMust.cs
@@ -46,6 +46,23 @@ namespace Day17.UnitTests
         [InlineData(7, 2, 5, 25, 0, 2, -3)]
         [InlineData(7, 2, 6, 27, -3, 1, -4)]
         [InlineData(7, 2, 7, 28, -7, 0, -5)]
+        [InlineData(6, 3, 1, 6, 3, 5, 2)]
+        [InlineData(6, 3, 2, 11, 5, 4, 1)]
+        [InlineData(6, 3, 3, 15, 6, 3, 0)]
+        [InlineData(6, 3, 4, 18, 6, 2, -1)]
+        [InlineData(6, 3, 5, 20, 5, 1, -2)]
+        [InlineData(6, 3, 6, 21, 3, 0, -3)]
+        [InlineData(6, 3, 7, 21, 0, 0, -4)]
+        [InlineData(6, 3, 8, 21, -4, 0, -5)]
+        [InlineData(6, 3, 9, 21, -9, 0, -6)]
+        [InlineData(9, 0, 1, 9, 0, 8, -1)]
+        [InlineData(9, 0, 2, 17, -1, 7, -2)]
+        [InlineData(9, 0, 3, 24, -3, 6, -3)]
+        [InlineData(9, 0, 4, 30, -6, 5, -4)]
+        [InlineData(17, -4, 1, 17, -4, 16, -5)]
+        [InlineData(17, -4, 2, 33, -9, 15, -6)]
+        [InlineData(17, -4, 3, 48, -15, 14, -7)]
+        [InlineData(17, -4, 4, 62, -22, 13, -8)]
         public void ExecuteStepCorrectly(int initialX, int initialY, int steps, int expectedPositionX, int expectedPositionY,
             int expectedVelocityX, int expectedVelocityY)
         {
@@ -57,12 +74,15 @@ namespace Day17.UnitTests
         }
 
         [Theory]
-        [InlineData(7, 2)]
-        public void FindWhetherProbeReachesTargetArea(int initialX, int initialY)
+        [InlineData(7, 2, 7)]
+        [InlineData(6, 3, 9)]
+        [InlineData(9, 0, 4)]
+        [InlineData(17, -4, -1)]
+        public void FindWhetherProbeReachesTargetArea(int initialX, int initialY, int expectedSteps)
         {
             var sut = new Launcher(SAMPLE_TARGET_AREA);
             sut.InitialVelocity(initialX, initialY);
-            Assert.Equal(7, sut.CountStepsUntilHittingTargetArea());
+            Assert.Equal(expectedSteps, sut.CountStepsUntilHittingTargetArea());
         }
     }
 }
