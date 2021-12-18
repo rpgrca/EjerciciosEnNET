55d01b2323a4a0fd363780b96bbbbc04eb24cfc5
Roberto <rpgrca@gmail.com>
2021-12-18 09:17:18 -0300

    add test, validate input

--- AdventOfCode2021/Day18/Day18.Logic/SnailFishNumberCalculator.cs
+++ AdventOfCode2021/Day18/Day18.Logic/SnailFishNumberCalculator.cs
@@ -4,5 +4,16 @@ namespace Day18.Logic
 {
     public class SnailFishNumberCalculator
     {
+        private readonly string _homework;
+
+        public SnailFishNumberCalculator(string homework)
+        {
+            if (string.IsNullOrWhiteSpace(homework))
+            {
+                throw new ArgumentException("Invalid homework");
+            }
+
+            _homework = homework;
+        }
     }
 }
--- AdventOfCode2021/Day18/Day18.UniTests/SnailFishNumberCalculatorMust.cs
+++ AdventOfCode2021/Day18/Day18.UniTests/SnailFishNumberCalculatorMust.cs
@@ -1,14 +1,19 @@
 using System;
+using Day18.Logic;
 using Xunit;
 
 namespace Day18.UniTests
 {
     public class SnailFishNumberCalculatorMust
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void ThrowException_WhenInitializedWithInvalidHomework(string invalidHomework)
         {
-
+            var exception = Assert.Throws<ArgumentException>(() => new SnailFishNumberCalculator(invalidHomework));
+            Assert.Equal("Invalid homework", exception.Message);
         }
     }
 }
