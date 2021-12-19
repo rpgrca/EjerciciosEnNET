ef5aa77ffe918faa35551e36100af14480c96cd1
Roberto <rpgrca@gmail.com>
2021-12-19 08:35:32 -0300

    add test, parse scanner data correctly

--- AdventOfCode2021/Day19/Day19.Logic/Scanner.cs
+++ AdventOfCode2021/Day19/Day19.Logic/Scanner.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Day19.Logic
 {
     public class Scanner
     {
         private readonly string _data;
+
+        public int Id { get; private set; }
+        public List<(int X, int Y, int Z)> Beacons { get; }
 
         public Scanner(string data)
         {
@@ -14,6 +19,21 @@ namespace Day19.Logic
             }
 
             _data = data;
+            Beacons = new List<(int X, int Y, int Z)>();
+
+            Parse();
+        }
+
+        private void Parse()
+        {
+            var lines = _data.Split("\n");
+            Id = int.Parse(lines[0].Replace("---", string.Empty).Replace("scanner", string.Empty));
+
+            foreach (var line in lines[1..])
+            {
+                var coordinates = line.Split(",").Select(p => int.Parse(p)).ToArray();
+                Beacons.Add((coordinates[0], coordinates[1], coordinates[2]));
+            }
         }
     }
 }
--- AdventOfCode2021/Day19/Day19.UnitTests/ScannerMust.cs
+++ AdventOfCode2021/Day19/Day19.UnitTests/ScannerMust.cs
@@ -15,5 +15,47 @@ namespace Day19.UnitTests
             var exception = Assert.Throws<ArgumentException>(() => new Scanner(invalidData));
             Assert.Equal("Invalid data", exception.Message);
         }
+
+        [Theory]
+        [InlineData(@"--- scanner 0 ---
+-1,-1,1
+-2,-2,2
+-3,-3,3
+-2,-3,1
+5,6,-4
+8,0,7")]
+        [InlineData(@"--- scanner 0 ---
+1,-1,1
+2,-2,2
+3,-3,3
+2,-1,3
+-5,4,-6
+-8,-7,0")]
+        [InlineData(@"--- scanner 0 ---
+-1,-1,-1
+-2,-2,-2
+-3,-3,-3
+-1,-3,-2
+4,6,5
+-7,0,8")]
+        [InlineData(@"--- scanner 0 ---
+1,1,-1
+2,2,-2
+3,3,-3
+1,3,-2
+-4,-6,5
+7,0,8")]
+        [InlineData(@"--- scanner 0 ---
+1,1,1
+2,2,2
+3,3,3
+3,1,2
+-6,-4,-5
+0,7,-8")]
+        public void ParseDataCorrectly(string data)
+        {
+            var sut = new Scanner(data);
+            Assert.Equal(6, sut.Beacons.Count);
+        }
     }
 }
