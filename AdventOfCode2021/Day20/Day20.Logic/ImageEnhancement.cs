222e2e81a3e7ba6c131302874f5c34afb7bf8fd6
Roberto <rpgrca@gmail.com>
2021-12-20 09:23:22 -0300

    add test, parse input correctly

--- AdventOfCode2021/Day20/Day20.Logic/ImageEnhancement.cs
+++ AdventOfCode2021/Day20/Day20.Logic/ImageEnhancement.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Day20.Logic
 {
     public class ImageEnhancement
     {
         private readonly string _input;
+        private readonly List<string> _image;
+
+        public int ImageWidth { get; private set; }
+        public int ImageHeight { get; private set; }
+        public string Algorithm { get; private set; }
 
         public ImageEnhancement(string input)
         {
@@ -14,6 +20,20 @@ namespace Day20.Logic
             }
 
             _input = input;
+            _image = new List<string>();
+
+            Parse();
+        }
+
+        private void Parse()
+        {
+            var lines = _input.Split("\n");
+            Algorithm = lines[0];
+
+            _image.AddRange(lines[2..]);
+
+            ImageWidth = _image[0].Length;
+            ImageHeight = _image.Count;
         }
     }
 }
--- AdventOfCode2021/Day20/Day20.UnitTests/ImageEnhancementMust.cs
+++ AdventOfCode2021/Day20/Day20.UnitTests/ImageEnhancementMust.cs
@@ -1,6 +1,7 @@
 using System;
-using Day20.Logic;
 using Xunit;
+using Day20.Logic;
+using static Day20.UnitTests.Constants;
 
 namespace Day20.UnitTests
 {
@@ -10,10 +11,21 @@ namespace Day20.UnitTests
         [InlineData(null)]
         [InlineData("")]
         [InlineData(" ")]
-        public void Test1(string invalidInput)
+        public void ThrowException_WhenInitializedWithInvalidData(string invalidInput)
         {
             var exception = Assert.Throws<ArgumentException>(() => new ImageEnhancement(invalidInput));
             Assert.Equal("Invalid input", exception.Message);
+        }
+
+        [Theory]
+        [InlineData(SAMPLE_INPUT, 5, 5)]
+        [InlineData(REAL_INPUT, 100, 100)]
+        public void BeInitializedCorrectly(string input, int expectedWidth, int expectedHeight)
+        {
+            var sut = new ImageEnhancement(input);
+            Assert.Equal(512, sut.Algorithm.Length);
+            Assert.Equal(expectedWidth, sut.ImageWidth);
+            Assert.Equal(expectedHeight, sut.ImageHeight);
         }
     }
 }
