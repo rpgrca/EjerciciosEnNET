36d92d3cca93561abd8898066dfb0ecb1659cbfb
Roberto <rpgrca@gmail.com>
2021-12-22 08:17:00 -0300

    add test, support on status

--- AdventOfCode2021/Day22/Day22.Logic/ReactorCore.cs
+++ AdventOfCode2021/Day22/Day22.Logic/ReactorCore.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Day22.Logic
 {
     public class ReactorCore
     {
         private readonly string _steps;
+        private readonly Dictionary<(int X, int Y, int Z), int> _turnedOn;
 
         public ReactorCore(string steps)
         {
@@ -14,6 +17,39 @@ namespace Day22.Logic
             }
 
             _steps = steps;
+            _turnedOn = new Dictionary<(int X, int Y, int Z), int>();
+
+            Parse();
         }
+
+        private void Parse()
+        {
+            foreach (var line in _steps.Split("\n"))
+            {
+                var location = line.Split(" ");
+                if (location[0] == "on")
+                {
+                    TurnOn(location[1]);
+                }
+            }
+        }
+
+        private void TurnOn(string area)
+        {
+            var axis = area.Split(",").Select(p => p.Split("=")).Select(p => p[1]).Select(p => p.Split("..")).ToArray();
+            for (var x = int.Parse(axis[0][0]); x <= int.Parse(axis[0][1]); x++)
+            {
+                for (var y = int.Parse(axis[1][0]); y <= int.Parse(axis[1][1]); y++)
+                {
+                    for (var z = int.Parse(axis[2][0]); z <= int.Parse(axis[2][1]); z++)
+                    {
+                        _turnedOn.TryAdd((x, y, z), 0);
+                        _turnedOn[(x, y, z)]++;
+                    }
+                }
+            }
+        }
+
+        public int GetTurnedOnCubesCount() => _turnedOn.Count;
     }
 }
--- AdventOfCode2021/Day22/Day22.UnitTests/ReactorCoreMust.cs
+++ AdventOfCode2021/Day22/Day22.UnitTests/ReactorCoreMust.cs
@@ -15,5 +15,15 @@ namespace Day22.UnitTests
             var exception = Assert.Throws<ArgumentException>(() => new ReactorCore(invalidSteps));
             Assert.Equal("Invalid steps", exception.Message);
         }
+
+        [Theory]
+        [InlineData("on x=10..12,y=10..12,z=10..12", 27)]
+        [InlineData(@"on x=10..12,y=10..12,z=10..12
+on x=11..13,y=11..13,z=11..13", 46)]
+        public void BeInitializedCorrectly(string steps, int expectedCount)
+        {
+            var sut = new ReactorCore(steps);
+            Assert.Equal(expectedCount, sut.GetTurnedOnCubesCount());
+        }
     }
 }
