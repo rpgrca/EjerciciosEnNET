dc0f9891ecb40592da264f01718876db09b39669
Roberto <rpgrca@gmail.com>
2021-12-24 14:59:59 -0300

    add test, implemented inp

--- AdventOfCode2021/Day24/Day24.Logic/ArithmeticLogicUnit.cs
+++ AdventOfCode2021/Day24/Day24.Logic/ArithmeticLogicUnit.cs
@@ -6,7 +6,14 @@ namespace Day24.Logic
     public class ArithmeticLogicUnit
     {
         private readonly string _instructions;
+
+        private readonly Dictionary<string, Action<int, int>> _operations;
+        private readonly List<Action<ArithmeticLogicUnit>> _opcodes;
+        private readonly List<int> _stack;
+        public int W { get; set; }
         public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
 
         public ArithmeticLogicUnit(string instructions)
         {
@@ -16,11 +23,64 @@ namespace Day24.Logic
             }
 
             _instructions = instructions;
+            _stack = new List<int>();
+            _operations = new Dictionary<string, Action<int, int>>
+            {
+                { "mul", (a, b) => a *= b }
+            };
+
+            _opcodes = new List<Action<ArithmeticLogicUnit>>();
+            Parse();
         }
 
-        public void Input(int value)
+        public void Run()
         {
-            X = -value;
+            foreach (var opcode in _opcodes)
+            {
+                opcode.Invoke(this);
+            }
+        }
+
+        private void Parse()
+        {
+            foreach (var line in _instructions.Split("\n"))
+            {
+                var operands = line.Split(" ");
+                switch (operands[0])
+                {
+                    case "inp":
+                        switch (operands[1])
+                        {
+                            case "w":
+                                _opcodes.Add(a => a.W = Pop());
+                                break;
+
+                            case "x":
+                                _opcodes.Add(a => a.X = Pop());
+                                break;
+
+                            case "y":
+                                _opcodes.Add(a => a.Y = Pop());
+                                break;
+
+                            default:
+                                _opcodes.Add(a => a.Z = Pop());
+                                break;
+                        }
+                        break;
+                }
+            }
+        }
+
+        public void Input(int value) => Push(value);
+
+        private void Push(int value) => _stack.Insert(0, value);
+
+        private int Pop()
+        {
+            var result = _stack[0];
+            _stack.RemoveAt(0);
+            return result;
         }
     }
 }
--- AdventOfCode2021/Day24/Day24.UnitTests/ArithmeticLogicUnitMust.cs
+++ AdventOfCode2021/Day24/Day24.UnitTests/ArithmeticLogicUnitMust.cs
@@ -17,12 +17,61 @@ namespace Day24.UnitTests
         }
 
         [Fact]
-        public void NegateInputNumberStoredInX()
+        public void MoveInputValueToRegisterW()
+        {
+            var sut = new ArithmeticLogicUnit("inp w");
+            sut.Input(7);
+            sut.Run();
+            Assert.Equal(7, sut.W);
+        }
+
+        [Fact]
+        public void MoveInputValueToRegisterX()
+        {
+            var sut = new ArithmeticLogicUnit("inp x");
+            sut.Input(5);
+            sut.Run();
+            Assert.Equal(5, sut.X);
+        }
+
+        [Fact]
+        public void MoveInputValueToRegisterY()
+        {
+            var sut = new ArithmeticLogicUnit("inp y");
+            sut.Input(9);
+            sut.Run();
+            Assert.Equal(9, sut.Y);
+        }
+
+        [Fact]
+        public void MoveInputValueToRegisterZ()
+        {
+            var sut = new ArithmeticLogicUnit("inp z");
+            sut.Input(3);
+            sut.Run();
+            Assert.Equal(3, sut.Z);
+        }
+
+/*
+        [Theory]
+        [InlineData(4, -4)]
+        [InlineData(7, -7)]
+        public void NegateInputNumberStoredInX(int input, int expectedX)
         {
             var sut = new ArithmeticLogicUnit(@"inp x
 mul x -1");
-            sut.Input(4);
-            Assert.Equal(-4, sut.X);
+            sut.Input(input);
+            Assert.Equal(expectedX, sut.X);
         }
+
+        [Theory]
+        [InlineData(4, 8)]
+        public void DuplicateNumberStoredInX(int input, int expectedX)
+        {
+            var sut = new ArithmeticLogicUnit(@"inp x
+mul x 2");
+            sut.Input(input);
+            Assert.Equal(expectedX, sut.X);
+        }*/
     }
 }
