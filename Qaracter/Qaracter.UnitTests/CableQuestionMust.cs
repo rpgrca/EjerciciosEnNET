0bfa7a0260ec7eef2297c02047c97ef26a76e6c4
Roberto <rpgrca@gmail.com>
2022-02-19 22:58:59 -0300

    merge both tests

--- Qaracter/Qaracter.UnitTests/CableQuestionMust.cs
+++ Qaracter/Qaracter.UnitTests/CableQuestionMust.cs
@@ -6,16 +6,10 @@ namespace Qaracter.UnitTests
     public class CableQuestionMust
     {
         [Fact]
-        public void InitializePoleHeightCorrectly()
+        public void BeInitializedCorrectly()
         {
             var sut = new CableQuestion(50, 80);
             Assert.Equal(50, sut.PoleHeight);
-        }
-
-        [Fact]
-        public void InitializeCableLengthCorrectly()
-        {
-            var sut = new CableQuestion(50, 80);
             Assert.Equal(80, sut.CableLength);
         }
     }
