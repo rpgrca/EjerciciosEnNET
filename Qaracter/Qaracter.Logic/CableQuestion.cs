f2aaa422db4f9d0cb3fe0e052fc6b62e3a729f11
Roberto <rpgrca@gmail.com>
2022-04-02 14:58:31 -0300

    add tests

--- Qaracter/Qaracter.Logic/CableQuestion.cs
+++ Qaracter/Qaracter.Logic/CableQuestion.cs
@@ -7,12 +7,20 @@ namespace Qaracter.Logic
         public int PoleHeight { get; }
         public int CableLength { get; }
         public int HeightAtCenter { get; }
+        public double PoleDistance { get; set; }
 
         public CableQuestion(int poleHeight, int cableLength, int heightAtCenter)
         {
             PoleHeight = poleHeight;
             CableLength = cableLength;
             HeightAtCenter = heightAtCenter;
+
+            CalculateDistanceBetweenPoles();
+        }
+
+        private void CalculateDistanceBetweenPoles()
+        {
+            
         }
     }
 }
--- Qaracter/Qaracter.UnitTests/CableQuestionMust.cs
+++ Qaracter/Qaracter.UnitTests/CableQuestionMust.cs
@@ -13,5 +13,12 @@ namespace Qaracter.UnitTests
             Assert.Equal(80, sut.CableLength);
             Assert.Equal(10, sut.HeightAtCenter);
         }
+
+        [Fact]
+        public void SolvePuzzle()
+        {
+            var sut = new CableQuestion(50, 80, 10);
+            Assert.Equal(0, sut.PoleDistance);
+        }
     }
 }
