db1cb1525f6853fdf9774609cff9cc9219677bdb
Roberto <rpgrca@gmail.com>
2022-04-07 08:09:09 -0300

    add extra tests

--- Codewars/8kyu/LarioAndMuigiPipeProblem/LarioAndMuigiPipeProblem.UnitTests/LarioWorld.cs
+++ Codewars/8kyu/LarioAndMuigiPipeProblem/LarioAndMuigiPipeProblem.UnitTests/LarioWorld.cs
@@ -19,8 +19,32 @@ public class LarioWorld
     }
 
     [Test]
-    public void FixThosePipes_t3()
-    {
+      public void FixThosePipes_t3()
+      {
         Assert.AreEqual(new List<int>{6,7,8,9}, Fixer.PipeFix(new List<int>{6,9}));
+      }
+
+    [Test]
+    public void SecretNegativeWorld()
+    {
+        Assert.AreEqual(new List<int>{-1,0,1,2,3,4}, Fixer.PipeFix(new List<int>{-1,4}));
+    }
+
+    [Test]
+    public void LengthOfOne()
+    {
+        Assert.AreEqual(new List<int>{2}, Fixer.PipeFix(new List<int>{2}));
+    }
+
+    [Test]
+    public static void RandomTest([Random(0,49,10)]int min, [Random(50,100,10)]int max)
+    {
+        List<int> pipes = new List<int>{ min, max };
+        Assert.AreEqual(Solution(pipes), Fixer.PipeFix(pipes), string.Format("Should work for {0}", string.Join(", ", pipes)));
+    }
+
+    private static List<int> Solution(List<int> pipes)
+    {
+        return Enumerable.Range(pipes.Min(), pipes.Max()+1 - pipes.Min()).ToList();
     }
 }
