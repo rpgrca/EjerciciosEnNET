ac0b4f772324360703e4c759f475fa55c5172317
Roberto <rpgrca@gmail.com>
2022-04-09 16:27:14 -0300

    pass test

--- Codewars/8kyu/SubtractTheSum/SubtractTheSum.Logic/Kata.cs
+++ Codewars/8kyu/SubtractTheSum/SubtractTheSum.Logic/Kata.cs
@@ -2,8 +2,27 @@
 
 public class Kata
 {
+    private static readonly string[] _names =
+    {
+        "kiwi", "pear", "kiwi", "banana", "melon", "banana", "melon", "pineapple", "apple", "pineapple",
+        "cucumber", "pineapple", "cucumber", "orange", "grape", "orange", "grape", "apple", "grape", "cherry",
+        "pear", "cherry", "pear", "kiwi", "banana", "kiwi", "apple", "melon", "banana", "melon",
+        "pineapple", "melon", "pineapple", "cucumber", "orange", "apple", "orange", "grape", "orange", "grape",
+        "cherry","pear", "cherry", "pear", "apple", "pear", "kiwi", "banana", "kiwi", "banana",
+        "melon", "pineapple", "melon", "apple", "cucumber", "pineapple", "cucumber", "orange", "cucumber", "orange",
+        "grape", "cherry", "apple", "cherry", "pear", "cherry", "pear", "kiwi", "pear", "kiwi",
+        "banana", "apple", "banana", "melon", "pineapple", "melon", "pineapple", "cucumber", "pineapple", "cucumber",
+        "apple", "grape", "orange", "grape", "cherry", "grape", "cherry", "pear", "cherry", "apple",
+        "kiwi", "banana", "kiwi", "banana", "melon", "banana", "melon", "pineapple", "apple", "pineapple"
+    };
+
     public static string SubtractSum(int number)
     {
-        return //fruit name like "apple"
+        do
+        {
+            number -= number.ToString().Sum(p => p - '0');
+        } while (number > 100);
+
+        return _names[number - 1];
     }
 }
