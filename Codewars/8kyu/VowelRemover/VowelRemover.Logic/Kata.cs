001a452268deb581034272bc62e5511746136962
Roberto <rpgrca@gmail.com>
2022-04-08 23:14:16 -0300

    solve problem

---
+++ Codewars/8kyu/VowelRemover/VowelRemover.Logic/Kata.cs
@@ -0,0 +1,7 @@
+﻿namespace VowelRemover.Logic;
+
+public class Kata
+{
+    public static string Shortcut(string input) =>
+      string.Join("", input.Where(c => c is not ('a' or 'e' or 'i' or 'o' or 'u')));
+}
