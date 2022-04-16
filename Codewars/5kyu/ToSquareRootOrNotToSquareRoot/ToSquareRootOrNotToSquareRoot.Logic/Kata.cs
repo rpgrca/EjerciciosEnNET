2489d34382c74333b3bf9075b50d72e5e8e18989
Roberto <rpgrca@gmail.com>
2022-04-15 22:50:37 -0300

    slightly optimized solution

--- Codewars/5kyu/ToSquareRootOrNotToSquareRoot/ToSquareRootOrNotToSquareRoot.Logic/Kata.cs
+++ Codewars/5kyu/ToSquareRootOrNotToSquareRoot/ToSquareRootOrNotToSquareRoot.Logic/Kata.cs
@@ -7,9 +7,8 @@ public class Kata
     public static int[] SquareOrSquareRoot(int[] array)
     {
         return array.Select(p => {
-          var root = (int)Math.Sqrt(p);
-          var power = (int)Math.Pow(root, 2);
-          return p == power ? root : (int)Math.Pow(p, 2);
+          var root = Math.Sqrt(p);
+          return root == (int)root ? (int)root : (int)Math.Pow(p, 2);
         }).ToArray();
     }
 }
