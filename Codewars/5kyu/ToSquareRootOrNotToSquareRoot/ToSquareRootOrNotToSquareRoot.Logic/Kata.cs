52697894a7a52aca1f995ec0fe4bd9155c0a109a
Roberto <rpgrca@gmail.com>
2022-04-15 22:48:34 -0300

    solve first test

--- Codewars/5kyu/ToSquareRootOrNotToSquareRoot/ToSquareRootOrNotToSquareRoot.Logic/Kata.cs
+++ Codewars/5kyu/ToSquareRootOrNotToSquareRoot/ToSquareRootOrNotToSquareRoot.Logic/Kata.cs
@@ -1,9 +1,15 @@
-﻿namespace ToSquareRootOrNotToSquareRoot.Logic;
+﻿using System.Linq;
+
+namespace ToSquareRootOrNotToSquareRoot.Logic;
 
 public class Kata
 {
-  public static int[] SquareOrSquareRoot(int[] array)
-  {
-    return array;
-  }
+    public static int[] SquareOrSquareRoot(int[] array)
+    {
+        return array.Select(p => {
+          var root = (int)Math.Sqrt(p);
+          var power = (int)Math.Pow(root, 2);
+          return p == power ? root : (int)Math.Pow(p, 2);
+        }).ToArray();
+    }
 }
