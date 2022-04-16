2ccdea831b5f8d8dd0b87d0ef2f938a607953577
Roberto <rpgrca@gmail.com>
2022-04-16 15:22:20 -0300

    convert to expression

--- Codewars/7kyu/AlphabetSymmetry/AlphabetSymmetry.Logic/Kata.cs
+++ Codewars/7kyu/AlphabetSymmetry/AlphabetSymmetry.Logic/Kata.cs
@@ -5,10 +5,8 @@ using System.Collections.Generic;
 
 public static class Kata
 {
-    public static List<int> Solve(List<string> arr)
-    {
-        return arr.ConvertAll(p => p
+    public static List<int> Solve(List<string> arr) =>
+        arr.ConvertAll(p => p
             .Select((x, i) => (x.ToString().ToLower()[0], i))
             .Count(t => t.Item1 - 'a' == t.i));
-    }
 }
