58621f508e5d401f7a841fa77a4ce359bda36430
Roberto <rpgrca@gmail.com>
2022-04-16 15:21:49 -0300

    add extra tests

--- Codewars/7kyu/AlphabetSymmetry/AlphabetSymmetry.UnitTests/SolutionTest.cs
+++ Codewars/7kyu/AlphabetSymmetry/AlphabetSymmetry.UnitTests/SolutionTest.cs
@@ -3,7 +3,14 @@ namespace Solution;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using AlphabetSymmetry.Logic;
+
+public static class Solution
+{
+    public static List<int> Solve(List<string> arr) =>
+        arr.Select(v => v.Where((c, i) => Char.ToLower(c) - 97 == i).Count()).ToList();
+}
 
 [TestFixture]
 public class SolutionTest
@@ -15,5 +22,19 @@ public class SolutionTest
         Assert.That(Kata.Solve(new List<string> {"abide", "ABc", "xyz"}), Is.EqualTo(new List<int> {4, 3, 0}));
         Assert.That(Kata.Solve(new List<string> {"IAMDEFANDJKL", "thedefgh", "xyzDEFghijabc"}), Is.EqualTo(new List<int> {6, 5, 7}));
         Assert.That(Kata.Solve(new List<string> {"encode", "abc", "xyzD", "ABmD"}), Is.EqualTo(new List<int> {1, 3, 1, 3}));
+    }
+
+    private static Random rnd = new Random();
+    public static string randStr(int min, int max) =>
+        String.Concat(new char[rnd.Next(min, max)].Select(_ => (char)rnd.Next(97, 123)).Select(c => rnd.Next(0, 2) == 0 ? Char.ToLower(c) : Char.ToUpper(c)).OrderBy(Char.ToLower));
+
+    [Test]
+    public void RandomTest()
+    {
+        for (int i = 0; i < 100; ++i)
+        {
+            List<string> arr = new string[rnd.Next(3, 20)].Select(_ => randStr(3, 20)).ToList();
+            Assert.That(Kata.Solve(arr), Is.EqualTo(Solution.Solve(arr)));
+        }
     }
 }
