ab79dddc5e0fab844e73caebd7e07a69672add1e
Roberto <rpgrca@gmail.com>
2022-04-15 21:37:06 -0300

    initial commit

---
+++ Codewars/7kyu/StringEndsWith/StringEndsWith.Logic/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace StringEndsWith.Logic;
+public class Class1
+{
+
+}
---
+++ Codewars/7kyu/StringEndsWith/StringEndsWith.UnitTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace StringEndsWith.UnitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
