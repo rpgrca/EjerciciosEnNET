35f7ba8a79c1a354a79d3ca6f13de1fbf074c6a1
Roberto <rpgrca@gmail.com>
2022-04-16 20:59:34 -0300

    add extra tests

--- Codewars/7kyu/FindTheCapitals/FindTheCapitals.UnitTests/Tests.cs
+++ Codewars/7kyu/FindTheCapitals/FindTheCapitals.UnitTests/Tests.cs
@@ -7,6 +7,18 @@ public class Tests
     [Test]
     public void CodEWaRs()
     {
-        Assert.AreEqual(Kata.Capitals("CodEWaRs"), new int[]{0,3,4,6});
+        Assert.AreEqual(new int[]{0,3,4,6}, Kata.Capitals("CodEWaRs"));
+    }
+
+    [Test]
+    public void NoneOrEmpty(){
+        Assert.AreEqual(new int[]{}, Kata.Capitals("none"));
+        Assert.AreEqual(new int[]{}, Kata.Capitals(""));
+    }
+
+    [Test]
+    public void TestOther(){
+        Assert.AreEqual(new int[]{0,1,2,3,4,5}, Kata.Capitals("ALLALL"));
+        Assert.AreEqual(new int[]{0}, Kata.Capitals("Kata"));
     }
 }
