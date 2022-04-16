ee174cccf1412c536a83fbb69149a2919b44bbdb
Roberto <rpgrca@gmail.com>
2022-04-16 11:04:18 -0300

    project creator helper

---
+++ Codewars/creator.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+LEVEL=$1
+NAME=$2
+KYUDIR=${LEVEL}kyu
+BASEDIR=${KYUDIR}/$NAME
+TESTDIR=$NAME.UnitTests
+LOGICDIR=$NAME.Logic
+
+if [ ! -d $KYUDIR ]; then
+    echo "Creating directory $KYUDIR"
+	mkdir $KYUDIR
+fi
+
+if [ -d $BASEDIR ]; then
+	echo "Kata with the same name in same level already exists! Aborting..."
+	exit 1
+fi
+
+mkdir $BASEDIR
+cd $BASEDIR
+dotnet new nunit -o $TESTDIR
+dotnet new classlib -o $LOGICDIR
+
+cd $TESTDIR
+dotnet add reference ../$LOGICDIR/$LOGICDIR.csproj
+
+cd ..
+dotnet new sln
+dotnet sln $NAME.sln add **/*.csproj
+dotnet build
+
+cd ../..
+dotnet sln Codewars.sln add $BASEDIR/**/*.csproj
+
+cd $BASEDIR
+code -r .
