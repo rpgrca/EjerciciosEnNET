949df3842d6fdfbe510a99aa7467130ea0d946b2
Roberto <rpgrca@gmail.com>
2022-04-16 20:57:40 -0300

    solve test

--- Codewars/7kyu/FindTheCapitals/FindTheCapitals.Logic/Kata.cs
+++ Codewars/7kyu/FindTheCapitals/FindTheCapitals.Logic/Kata.cs
@@ -4,8 +4,6 @@ namespace FindTheCapitals.Logic;
 
 public static class Kata
 {
-    public static int[] Capitals(string word)
-    {
-        //Write your code here
-    }
+    public static int[] Capitals(string word) =>
+        word.Select((p, i) => (p, i)).Where(x => char.IsUpper(x.p)).Select(x => x.i).ToArray();
 }
