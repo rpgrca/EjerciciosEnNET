965a9a996711b53d10b130c650d4acbffc1e3bef
Roberto <rpgrca@gmail.com>
2022-04-16 11:24:56 -0300

    slightly optimized solution

--- Codewars/7kyu/YouGotChange/YouGotChange.Logic/Kata.cs
+++ Codewars/7kyu/YouGotChange/YouGotChange.Logic/Kata.cs
@@ -4,18 +4,13 @@ public class Kata
 {
     public static int[] GiveChange(int amount)
     {
+        var bills = new int[] { 100, 50, 20, 10, 5, 1 };
         var change = new int[] { 0, 0, 0, 0, 0, 0 };
-        change[5] = amount / 100;
-        amount -= 100 * change[5];
-        change[4] = amount / 50;
-        amount -= 50 * change[4];
-        change[3] = amount / 20;
-        amount -= 20 * change[3];
-        change[2] = amount / 10;
-        amount -= 10 * change[2];
-        change[1] = amount / 5;
-        amount -= 5 * change[1];
-        change[0] = amount;
+
+        for (var position = 0; position < bills.Length; position++)
+        {
+            amount -= bills[position] * (change[bills.Length - 1 - position] = amount / bills[position]);
+        }
 
         return change;
     }
