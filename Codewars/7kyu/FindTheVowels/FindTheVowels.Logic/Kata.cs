6114ddb67143574a7acb60669240e61de1ac01ba
Roberto <rpgrca@gmail.com>
2022-04-15 23:22:15 -0300

    pass test

--- Codewars/7kyu/FindTheVowels/FindTheVowels.Logic/Kata.cs
+++ Codewars/7kyu/FindTheVowels/FindTheVowels.Logic/Kata.cs
@@ -1,9 +1,13 @@
-﻿namespace FindTheVowels.Logic;
+﻿using System.Linq;
+
+namespace FindTheVowels.Logic;
 
 public class Kata
 {
-  public static int[] VowelIndices(string word)
-  {
-    // your code here
-  }
+    public static int[] VowelIndices(string word) =>
+        word
+            .Select((p, i) => new { p, i})
+            .Where(a => a.p is 'a' or 'e' or 'i' or 'o' or 'u' or 'y')
+            .Select(a => a.i + 1)
+            .ToArray();
 }
