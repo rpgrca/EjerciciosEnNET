2f421fe6c142573b58e25ed118ed7f0607c7e77e
Roberto <rpgrca@gmail.com>
2022-04-17 20:00:54 -0300

    refactor, simplify

--- Codewars/6kyu/GiveMeAdiamond/GiveMeAdiamond.Logic/DiamondMaker.cs
+++ Codewars/6kyu/GiveMeAdiamond/GiveMeAdiamond.Logic/DiamondMaker.cs
@@ -2,23 +2,10 @@ namespace GiveMeAdiamond.Logic;
 
 public class DiamondMaker : IDiamondMaker
 {
-    private readonly int _width;
+    public string? Diamond { get; }
 
-    public string? Diamond { get; private set; }
-
-    internal DiamondMaker(int width)
-    {
-        _width = width;
-
-        Build(1, i => i <= _width, 2);
-        Build(_width - 2, i => 1 <= i, -2);
-    }
-
-    private void Build(int from, Func<int, bool> condition, int step)
-    {
-        for (var index = from; condition(index); index += step)
-        {
-            Diamond += new string(' ', (_width - index) / 2) + new string('*', index) + "\n";
-        }
-    }
+    internal DiamondMaker(int width) =>
+        Diamond = Enumerable.Range(1, (width * 2) - 1)
+            .Where(i => i % 2 != 0)
+            .Aggregate(string.Empty, (t, i) => t + new string(' ', Math.Abs(width - i) / 2) + new string('*', width - Math.Abs(width - i)) + "\n");
 }
