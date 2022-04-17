e3e28d15fd8cb8e4f05028edcd0732afce8d9bb2
Roberto <rpgrca@gmail.com>
2022-04-16 23:48:44 -0300

    pass test

--- Codewars/7kyu/InsertDashes/InsertDashes.Logic/Kata.cs
+++ Codewars/7kyu/InsertDashes/InsertDashes.Logic/Kata.cs
@@ -4,6 +4,20 @@ public class Kata
 {
     public static string InsertDash(int num)
     {
-        // code me
+        var previous = 0;
+        var result = string.Empty;
+        foreach (var character in num.ToString())
+        {
+            var digit = character - '0';
+            if (((previous & 1) == 1) && ((digit & 1) == 1))
+            {
+                result += "-";
+            }
+
+            previous = digit;
+            result += character;
+        }
+
+        return result;
     }
 }
