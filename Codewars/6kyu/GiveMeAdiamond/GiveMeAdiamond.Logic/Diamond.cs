1634778564680a235c3cc426a29b9d96450a9513
Roberto <rpgrca@gmail.com>
2022-04-17 17:02:07 -0300

    pass test

--- Codewars/6kyu/GiveMeAdiamond/GiveMeAdiamond.Logic/Diamond.cs
+++ Codewars/6kyu/GiveMeAdiamond/GiveMeAdiamond.Logic/Diamond.cs
@@ -4,9 +4,21 @@ namespace GiveMeAdiamond.Logic;
 
 public class Diamond
 {
-	public static string? Print(int n)
-	{
-		if (n < 0 || n % 2 == 0) return null;
-		return string.Empty;
-	}
+    public static string? Print(int n)
+    {
+        if (n < 0 || n % 2 == 0) return null;
+        var result = string.Empty;
+
+        for (var index = 1; index <= n; index += 2)
+        {
+            result += new string(' ', (n - index) / 2) + new string('*', index) + "\n";
+        }
+
+        for (var index = n - 2; index >= 1; index -= 2)
+        {
+            result += new string(' ', (n - index) / 2) + new string('*', index) + "\n";
+        }
+
+        return result;
+    }
 }
