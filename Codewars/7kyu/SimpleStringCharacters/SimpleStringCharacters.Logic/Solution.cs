0e3483f87849a06d3fc12a070bcb647b3cc00d75
Roberto <rpgrca@gmail.com>
2022-04-18 20:38:50 -0300

    pass test

--- Codewars/7kyu/SimpleStringCharacters/SimpleStringCharacters.Logic/Solution.cs
+++ Codewars/7kyu/SimpleStringCharacters/SimpleStringCharacters.Logic/Solution.cs
@@ -4,8 +4,33 @@ using System;
 
 public class Solution
 {
-    public static int [] solve(String s)
+    public static int [] Solve(string s)
     {
-        //..
+        var uppercase = 0;
+        var lowercase = 0;
+        var numbers = 0;
+        var special = 0;
+
+        foreach (var c in s)
+        {
+            if (char.IsUpper(c))
+            {
+                uppercase++;
+            }
+            else if (char.IsLower(c))
+            {
+                lowercase++;
+            }
+            else if (char.IsDigit(c))
+            {
+                numbers++;
+            }
+            else
+            {
+                special++;
+            }
+        }
+
+        return new int[] { uppercase, lowercase, numbers, special };
     }
 }
--- Codewars/7kyu/SimpleStringCharacters/SimpleStringCharacters.UnitTests/SolutionTest.cs
+++ Codewars/7kyu/SimpleStringCharacters/SimpleStringCharacters.UnitTests/SolutionTest.cs
@@ -8,9 +8,9 @@ public class SolutionTest
     [Test]
     public void ExampleTests()
     {
-        Assert.AreEqual(new int[] {1,18,3,2}, Solution.solve("Codewars@codewars123.com"));
-        Assert.AreEqual(new int[] {7,6,3,2},  Solution.solve("bgA5<1d-tOwUZTS8yQ"));
-        Assert.AreEqual(new int[] {9,9,6,9}, Solution.solve("P*K4%>mQUDaG$h=cx2?.Czt7!Zn16p@5H"));
-        Assert.AreEqual(new int[] {15,8,6,9},  Solution.solve("RYT'>s&gO-.CM9AKeH?,5317tWGpS<*x2ukXZD"));
+        Assert.AreEqual(new int[] {1,18,3,2}, Solution.Solve("Codewars@codewars123.com"));
+        Assert.AreEqual(new int[] {7,6,3,2},  Solution.Solve("bgA5<1d-tOwUZTS8yQ"));
+        Assert.AreEqual(new int[] {9,9,6,9}, Solution.Solve("P*K4%>mQUDaG$h=cx2?.Czt7!Zn16p@5H"));
+        Assert.AreEqual(new int[] {15,8,6,9},  Solution.Solve("RYT'>s&gO-.CM9AKeH?,5317tWGpS<*x2ukXZD"));
     }
 }
