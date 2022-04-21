1dc7eccc6416361c6042ec6ff8f4e17fcedd9682
Roberto <rpgrca@gmail.com>
2022-04-20 22:23:08 -0300

    solve tests

--- Codewars/7kyu/NarcissisticNumbers/NarcissisticNumbers.Logic/Kata.cs
+++ Codewars/7kyu/NarcissisticNumbers/NarcissisticNumbers.Logic/Kata.cs
@@ -1,9 +1,13 @@
-﻿namespace NarcissisticNumbers.Logic;
+﻿using System.Linq;
+
+namespace NarcissisticNumbers.Logic;
 
 public class Kata
 {
     public static bool IsNarcissistic(long n)
     {
-        return false;
+        var digits = n.ToString().Select(p => p - '0').ToArray();
+        var power = digits.Length;
+        return digits.Aggregate(0L, (t, i) => t + (long)Math.Pow(i, power)) == n;
     }
 }
