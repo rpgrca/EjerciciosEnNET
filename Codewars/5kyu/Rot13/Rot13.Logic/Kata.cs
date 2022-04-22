99d4b5bb5105a9563618015aa64440a4ab07a22d
Roberto <rpgrca@gmail.com>
2022-04-22 01:13:24 -0300

    better solution

--- Codewars/5kyu/Rot13/Rot13.Logic/Kata.cs
+++ Codewars/5kyu/Rot13/Rot13.Logic/Kata.cs
@@ -4,6 +4,25 @@ using System;
 
 public class Kata
 {
-    public static string Rot13(string input) =>
-        input.Aggregate(string.Empty, (t, i) => t += char.IsLetter(i) ? (char)((((i & 31) + 13) % 26) + (i & 96)) : i);
+    public static string Rot13(string input)
+    {
+        var encrypted = string.Empty;
+        foreach (var c in input)
+        {
+            if (char.IsLower(c))
+            {
+                encrypted += (char)((((c - 'a') + 13) % 26) + 'a');
+            }
+            else if (char.IsUpper(c))
+            {
+                encrypted += (char)((((c - 'A') + 13) % 26) + 'A');
+            }
+            else
+            {
+                encrypted += c;
+            }
+        }
+
+        return encrypted;
+    }
 }
