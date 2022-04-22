8b88f5df523b569b6f00a7c65218b380030f2a04
Roberto <rpgrca@gmail.com>
2022-04-22 00:20:37 -0300

    pass test

--- Codewars/5kyu/Rot13/Rot13.Logic/Kata.cs
+++ Codewars/5kyu/Rot13/Rot13.Logic/Kata.cs
@@ -4,8 +4,6 @@ using System;
 
 public class Kata
 {
-    public static string Rot13(string input)
-    {
-        return "Code Here";
-    }
+    public static string Rot13(string input) =>
+        input.Aggregate(string.Empty, (t, i) => t += char.IsLetter(i) ? (char)((((i & 31) + 13) % 26) + (i & 96)) : i);
 }
