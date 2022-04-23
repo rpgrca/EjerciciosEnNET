a6da1ba61fc06f7635e071164a1a32ac0a90384d
Roberto <rpgrca@gmail.com>
2022-04-23 20:51:00 -0300

    add tests

--- Codewars/8kyu/ColorGhost/ColorGhost.UnitTests/SolutionTest.cs
+++ Codewars/8kyu/ColorGhost/ColorGhost.UnitTests/SolutionTest.cs
@@ -1,17 +1,53 @@
-namespace Solution {
-    using NUnit.Framework;
-    using System;
-    using ColorGhost.Logic;
+namespace Solution;
 
-    // TODO: Replace examples and use TDD by writing your own tests
+using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using ColorGhost.Logic;
 
-    [TestFixture]
-    public class SolutionTest
+[TestFixture]
+public class GhostTests
+{
+    private Dictionary<string, string> GhostColors = new Dictionary<string, string>();
+
+    public GhostTests()
     {
-        [Test]
-        public void MyTest()
+        for(int i = 0; i < 10000; i++)
         {
-            Assert.AreEqual("expected", "actual");
+            string c = new Ghost().GetColor();
+            if(!GhostColors.ContainsKey(c))
+            {
+                GhostColors.Add(c, "");
+            }
         }
+    }
+
+    private bool GhostColor(Dictionary<string, string> arr, string c)
+    {
+        return arr.ContainsKey(c);
+    }
+
+    [Test]
+    public void ShouldSometimesMakeWhiteGhosts()
+    {
+        Assert.AreEqual(true, GhostColor(GhostColors, "white"));
+    }
+
+    [Test]
+    public void ShouldSometimesMakeYellowGhosts()
+    {
+        Assert.AreEqual(true, GhostColor(GhostColors, "yellow"));
+    }
+
+    [Test]
+    public void ShouldSometimesMakePurpleGhosts()
+    {
+        Assert.AreEqual(true, GhostColor(GhostColors, "purple"));
+    }
+
+    [Test]
+    public void ShouldSometimesMakeRedGhosts()
+    {
+        Assert.AreEqual(true, GhostColor(GhostColors, "red"));
     }
 }
