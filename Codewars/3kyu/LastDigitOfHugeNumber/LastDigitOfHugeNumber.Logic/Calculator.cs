5fe702521bbc7106bc2e37d568a43f717f69d845
Roberto <rpgrca@gmail.com>
2022-04-23 00:38:55 -0300

    halfway

--- Codewars/3kyu/LastDigitOfHugeNumber/LastDigitOfHugeNumber.Logic/Calculator.cs
+++ Codewars/3kyu/LastDigitOfHugeNumber/LastDigitOfHugeNumber.Logic/Calculator.cs
@@ -2,10 +2,51 @@
 
 using System;
 
-public class Calculator {
-    public static int LastDigit(int[] array)
+public class Calculator
+{
+    public static int LastDigit(int[] array, int recursionLevel = 0)
     {
-        // Write your code here
-        return 0;
+        if (array.Length == 0 || array.Length == 1) return 1;
+
+        if (array.Length > 2)
+        {
+            var exponent = LastDigit(array[1..], recursionLevel + 1);
+
+            var originalValue = array[0];
+            var power = exponent;
+            var value = originalValue;
+
+            var binary = Convert.ToString(power, 2);
+            foreach (var binaryDigit in binary[1..])
+            {
+                value = (value * value) % 100;
+                if (binaryDigit == '1')
+                {
+                    value = (value * originalValue) % 100;
+                }
+            }
+
+            return value % (recursionLevel == 0? 10 : 100);
+        }
+        else
+        {
+            if (array[0] == 0 && array[1] == 0) return 1;
+
+            var originalValue = array[0];
+            var power = array[1];
+            var value = originalValue;
+
+            var binary = Convert.ToString(power, 2);
+            foreach (var binaryDigit in binary[1..])
+            {
+                value = (value * value) % 100;
+                if (binaryDigit == '1')
+                {
+                    value = (value * originalValue) % 100;
+                }
+            }
+
+            return value % 100;
+        }
     }
 }
--- Codewars/3kyu/LastDigitOfHugeNumber/LastDigitOfHugeNumber.UnitTests/SolutionTest.cs
+++ Codewars/3kyu/LastDigitOfHugeNumber/LastDigitOfHugeNumber.UnitTests/SolutionTest.cs
@@ -16,19 +16,13 @@ namespace Solution
     [TestFixture]
     public class SolutionTest {
         [Test]
-        public void SampleTest() {
+        public void SampleTest()
+        {
             Random rnd = new Random();
             int rand1 = rnd.Next(0,100);
             int rand2 = rnd.Next(0,10);
 
             LDCase[] allCases = new LDCase[] {
-                new LDCase(new int[0],           1),
-                new LDCase(new int[] {0,0},      1),
-                new LDCase(new int[] {0,0,0},    0),
-                new LDCase(new int[] {1,2},      1),
-                new LDCase(new int[] {3,4,5},    1),
-                new LDCase(new int[] {4,3,6},    4),
-                new LDCase(new int[] {7,6,21},   1),
                 new LDCase(new int[] {12,30,21}, 6),
                 new LDCase(new int[] {2,2,2,0},  4),
                 new LDCase(new int[] {937640,767456,981242}, 0),
@@ -38,9 +32,42 @@ namespace Solution
                 new LDCase(new int[] {rand1}, rand1%10),
                 new LDCase(new int[] {rand1,rand2}, (int)Math.Pow(rand1%10,rand2)%10)
             };
+
             for(int i=0; i<allCases.Length;i++) {
                 Assert.AreEqual(allCases[i].expect, Calculator.LastDigit(allCases[i].test));
             }
         }
+
+        [Test]
+        public void Test1() =>
+            Assert.AreEqual(1, Calculator.LastDigit(Array.Empty<int>()));
+
+        [Test]
+        public void Test2() =>
+            Assert.AreEqual(1, Calculator.LastDigit(new int[] { 0, 0 }));
+
+        [Test]
+        public void Test3() =>
+            Assert.AreEqual(0, Calculator.LastDigit(new int[] { 0, 0, 0 }));
+
+        [Test]
+        public void Test4() =>
+            Assert.AreEqual(1, Calculator.LastDigit(new int[] { 1, 2 }));
+
+        [Test]
+        public void Test5() =>
+            Assert.AreEqual(1, Calculator.LastDigit(new int[] { 3, 4, 5 }));
+
+        [Test]
+        public void Test6() =>
+            Assert.AreEqual(4, Calculator.LastDigit(new int[] { 4, 3, 6 }));
+
+        [Test]
+        public void Test7() =>
+            Assert.AreEqual(1, Calculator.LastDigit(new int[] { 7, 6, 21 }));
+
+        [Test]
+        public void Test8() =>
+            Assert.AreEqual(6, Calculator.LastDigit(new int[] {12,30,21}));
     }
 }
