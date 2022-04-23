e62f34b08591a8350df181fa257d13372e079801
Roberto <rpgrca@gmail.com>
2022-04-23 20:07:56 -0300

    clean up

--- Codewars/3kyu/LastDigitOfHugeNumber/LastDigitOfHugeNumber.Logic/Calculator.cs
+++ Codewars/3kyu/LastDigitOfHugeNumber/LastDigitOfHugeNumber.Logic/Calculator.cs
@@ -10,35 +10,23 @@ public class Calculator
         if (array.Length == 0) return 1;
         if (array.Length == 1) return array[0] % 10;
 
-        var result = LastDigitOf(array.Select(p => new BigInteger(p)).ToArray(), 0);
+        var result = LastDigitOf(array.Select(p => new BigInteger(p)).ToArray());
         BigInteger.DivRem(result, 10, out var remainder);
         return (int)remainder;
     }
 
-    private static BigInteger LastDigitOf(BigInteger[] array, int level)
+    private static BigInteger LastDigitOf(BigInteger[] array, int level = 0)
     {
-        BigInteger exponent;
-        if (array.Length > 2)
-        {
-            exponent = LastDigitOf(array[1..], level + 1);
-        }
-        else
-        {
-            exponent = array[1];
-        }
+        var exponent = (array.Length > 2)
+            ? LastDigitOf(array[1..], level + 1)
+            : array[1];
 
         if (array[0] == 0)
         {
-            if (exponent == 0) return 1;
-            else return 0;
+            return (exponent == 0) ? 1 : 0;
         }
 
         var result = BigInteger.ModPow(array[0], exponent, 10000);
-        if (result == 0 && level != 0)
-        {
-            result = 4;
-        }
-
-        return result;
+        return (result == 0 && level != 0) ? 4 : result;
     }
 }
