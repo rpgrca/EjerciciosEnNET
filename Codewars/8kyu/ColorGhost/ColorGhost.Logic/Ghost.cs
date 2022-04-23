00404c93fb794eceef41ecd11857364764057e04
Roberto <rpgrca@gmail.com>
2022-04-23 20:48:41 -0300

    new kata

---
+++ Codewars/8kyu/ColorGhost/ColorGhost.Logic/Ghost.cs
@@ -0,0 +1,11 @@
+﻿namespace ColorGhost.Logic;
+
+public class Ghost
+{
+    private static readonly string[] Color = { "white", "yellow", "purple", "red" };
+    private readonly string _color;
+
+    public Ghost() => _color = Color[new Random().Next() % Color.Length];
+
+    public string GetColor() => _color;
+}
---
+++ Codewars/8kyu/ColorGhost/ColorGhost.UnitTests/SolutionTest.cs
@@ -0,0 +1,17 @@
+namespace Solution {
+    using NUnit.Framework;
+    using System;
+    using ColorGhost.Logic;
+
+    // TODO: Replace examples and use TDD by writing your own tests
+
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void MyTest()
+        {
+            Assert.AreEqual("expected", "actual");
+        }
+    }
+}
