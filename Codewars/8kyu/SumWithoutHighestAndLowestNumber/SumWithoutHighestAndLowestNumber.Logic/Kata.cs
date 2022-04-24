f308e8c244fce841d88ea5f0c0cdeb6b5b5b81b1
Roberto <rpgrca@gmail.com>
2022-04-24 11:29:13 -0300

    simplify

--- Codewars/8kyu/SumWithoutHighestAndLowestNumber/SumWithoutHighestAndLowestNumber.Logic/Kata.cs
+++ Codewars/8kyu/SumWithoutHighestAndLowestNumber/SumWithoutHighestAndLowestNumber.Logic/Kata.cs
@@ -5,9 +5,6 @@ using System.Linq;
 
 public static class Kata
 {
-    public static int Sum(int[] numbers)
-    {
-        if (numbers is null || numbers.Length < 3) return 0;
-        return numbers.OrderBy(p => p).Skip(1).Take(numbers.Length - 2).Sum();
-    }
+    public static int Sum(int[] numbers) =>
+        numbers?.OrderBy(p => p).Skip(1).Take(numbers.Length - 2).Sum() ?? 0;
 }
