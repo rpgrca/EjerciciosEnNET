109cc2310b04c8b998416df59643486774a67efc
Roberto <rpgrca@gmail.com>
2022-04-24 13:59:02 -0300

    solve puzzle

--- Codewars/8kyu/Grader/Grader.Logic/Kata.cs
+++ Codewars/8kyu/Grader/Grader.Logic/Kata.cs
@@ -2,8 +2,12 @@
 
 public class Kata
 {
-    public static char Grader(double score)
-    {
-        return '';
-    }
+    public static char Grader(double score) =>
+        score switch {
+            >= 0.9 and <= 1 => 'A',
+            >= 0.8 => 'B',
+            >= 0.7 => 'C',
+            >= 0.6 => 'D',
+            _ => 'F'
+        };
 }
