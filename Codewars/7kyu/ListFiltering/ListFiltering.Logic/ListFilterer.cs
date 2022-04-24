81aebeba2f9597e32e36e3fe37847e87ba29bed1
Roberto <rpgrca@gmail.com>
2022-04-24 18:07:37 -0300

    solve puzzle

--- Codewars/7kyu/ListFiltering/ListFiltering.Logic/ListFilterer.cs
+++ Codewars/7kyu/ListFiltering/ListFiltering.Logic/ListFilterer.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 
 public class ListFilterer
 {
-    public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
-    {
-    }
+    public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems) =>
+        listOfItems.Where(p => p is int).Select(p => (int)p);
 }
