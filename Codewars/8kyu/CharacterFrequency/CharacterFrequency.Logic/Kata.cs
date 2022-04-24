6796b37d78db3104f1a11de17964c0da3b531246
Roberto <rpgrca@gmail.com>
2022-04-24 16:40:32 -0300

    solve puzzle

--- Codewars/8kyu/CharacterFrequency/CharacterFrequency.Logic/Kata.cs
+++ Codewars/8kyu/CharacterFrequency/CharacterFrequency.Logic/Kata.cs
@@ -4,8 +4,6 @@ using System.Collections.Generic;
 
 public class Kata
 {
-    public static Dictionary<char, int> CharFreq(string message)
-    {
-        return new Dictionary<char, int>();
-    }
+    public static Dictionary<char, int> CharFreq(string message) =>
+        message.GroupBy(t => t).ToDictionary(g => g.Key, g => g.Count());
 }
