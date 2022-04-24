e95ffff40197c4008a043ce6d9efaf56ba367058
Roberto <rpgrca@gmail.com>
2022-04-24 11:58:19 -0300

    add extra tests

--- Codewars/8kyu/WelcomeToTheCity/WelcomeToTheCity.Logic/Kata.cs
+++ Codewars/8kyu/WelcomeToTheCity/WelcomeToTheCity.Logic/Kata.cs
@@ -2,8 +2,6 @@
 
 public class Kata
 {
-    public static string SayHello(string[] name, string city, string state)
-    {
-        return $"Hello, {string.Join(" ", name)}! Welcome to {city}, {state}!";
-    }
+    public static string SayHello(string[] name, string city, string state) =>
+        $"Hello, {string.Join(" ", name)}! Welcome to {city}, {state}!";
 }
--- Codewars/8kyu/WelcomeToTheCity/WelcomeToTheCity.UnitTests/SolutionTest.cs
+++ Codewars/8kyu/WelcomeToTheCity/WelcomeToTheCity.UnitTests/SolutionTest.cs
@@ -1,15 +1,61 @@
 namespace Solution {
     using NUnit.Framework;
     using System;
+    using System.Linq;
     using WelcomeToTheCity.Logic;
 
     [TestFixture]
     public class SolutionTest
     {
-        [Test]
-        public void SampleTest()
+        private static Random rnd = new Random();
+
+        private static object[] fixedTestCases = new object[]
         {
-            Assert.AreEqual("Hello, John Smith! Welcome to Phoenix, Arizona!", Kata.SayHello(new string[] {"John", "Smith"}, "Phoenix", "Arizona"));
+            new object[] {new string[] {"John", "Smith"}, "Phoenix", "Arizona", "Hello, John Smith! Welcome to Phoenix, Arizona!"},
+            new object[] {new string[] {"Franklin", "Delano", "Roosevelt"}, "Chicago", "Illinois", "Hello, Franklin Delano Roosevelt! Welcome to Chicago, Illinois!"},
+            new object[] {new string[] {"Wallace", "Russel", "Osbourne"}, "Albany", "New York", "Hello, Wallace Russel Osbourne! Welcome to Albany, New York!"},
+        }.OrderBy(x => rnd.Next()).ToArray();
+
+        [Test, TestCaseSource("fixedTestCases")]
+        public void FixedTest(string[] name, string city, string state, string expected)
+        {
+            Assert.AreEqual(expected, Kata.SayHello(name, city, state));
+        }
+
+        private static string solution(string[] name, string city, string state) => $"Hello, {String.Join(" ", name)}! Welcome to {city}, {state}!";
+
+        private static string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        [Test, Description("Random Tests")]
+        public void RandomTest()
+        {
+            for (int i = 0; i < 100; ++i)
+            {
+                string[] name = new string[rnd.Next(1, 6)];
+                for (int j = 0; j < name.Length; ++j)
+                {
+                    int l = rnd.Next(3, 12);
+                    string n = "";
+                    for (int k = 0; k < l; ++k)
+                    {
+                        n += chars[rnd.Next(0, chars.Length)];
+                    }
+                    name[j] = n;
+                }
+
+                string city = "";
+                string state = "";
+                int length = rnd.Next(3, 12);
+                for (int j = 0; j < length; ++j)
+                {
+                    city += chars[rnd.Next(0, chars.Length)];
+                    state += chars[rnd.Next(0, chars.Length)];
+                }
+
+                string expected = solution(name, city, state);
+                string actual = Kata.SayHello(name, city, state);
+                Assert.AreEqual(expected, actual);
+            }
         }
     }
 }
