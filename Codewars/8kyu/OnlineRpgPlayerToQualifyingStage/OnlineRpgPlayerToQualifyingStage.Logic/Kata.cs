924fe7e8ec8dbc0bf2d89f2efbb44d537a0803b1
Roberto <rpgrca@gmail.com>
2022-04-23 22:40:14 -0300

    solve kata

--- Codewars/8kyu/OnlineRpgPlayerToQualifyingStage/OnlineRpgPlayerToQualifyingStage.Logic/Kata.cs
+++ Codewars/8kyu/OnlineRpgPlayerToQualifyingStage/OnlineRpgPlayerToQualifyingStage.Logic/Kata.cs
@@ -3,8 +3,8 @@
 using System;
 
 public class Kata{
-    public static Object PlayerRankUp(int points)
-    {
-
-    }
+    public static Object PlayerRankUp(int points) =>
+        points >= 100
+            ? "Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up."
+            : (Object)false;
 }
