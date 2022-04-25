64a54fa2d169f73f3a7e96cc4b052793d84d0d30
Roberto <rpgrca@gmail.com>
2022-04-24 22:45:46 -0300

    solve kata

--- Codewars/7kyu/FindTheNthDigitOfNumber/FindTheNthDigitOfNumber.Logic/Kata.cs
+++ Codewars/7kyu/FindTheNthDigitOfNumber/FindTheNthDigitOfNumber.Logic/Kata.cs
@@ -4,5 +4,7 @@ public class Kata
 {
     public static int FindDigit(int num, int nth)
     {
+        if (nth <= 0) return -1;
+        return Math.Abs(num) / (int)Math.Pow(10, nth - 1) % 10;
     }
 }
