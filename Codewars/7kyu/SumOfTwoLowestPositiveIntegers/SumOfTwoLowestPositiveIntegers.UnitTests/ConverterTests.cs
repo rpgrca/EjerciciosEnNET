24a5a58b31c11908294964928364e6fcc629b4a4
Roberto <rpgrca@gmail.com>
2022-04-24 23:29:23 -0300

    add extra tests

--- Codewars/7kyu/SumOfTwoLowestPositiveIntegers/SumOfTwoLowestPositiveIntegers.UnitTests/ConverterTests.cs
+++ Codewars/7kyu/SumOfTwoLowestPositiveIntegers/SumOfTwoLowestPositiveIntegers.UnitTests/ConverterTests.cs
@@ -7,23 +7,66 @@ using SumOfTwoLowestPositiveIntegers.Logic;
 public class ConverterTests
 {
     [Test]
-	public void Test1()
-	{
-    int[] numbers = {5, 8, 12, 19, 22};
-		Assert.AreEqual(13, Kata.sumTwoSmallestNumbers(numbers));		
-	}
-  
-  [Test]
-	public void Test2()
-	{
-    int[] numbers = {19, 5, 42, 2, 77};
-		Assert.AreEqual(7, Kata.sumTwoSmallestNumbers(numbers));		
-	}
-  
-  [Test]
-	public void Test3()
-	{
-    int[] numbers = {10, 343445353, 3453445, 2147483647};
-		Assert.AreEqual(3453455, Kata.sumTwoSmallestNumbers(numbers));		
-	}
+    public void Test1()
+    {
+        int[] numbers = {5, 8, 12, 19, 22};
+        Assert.AreEqual(13, Kata.sumTwoSmallestNumbers(numbers));
+    }
+
+    [Test]
+    public void Test2()
+    {
+        int[] numbers = {15, 28, 4, 2, 43};
+        Assert.AreEqual(6, Kata.sumTwoSmallestNumbers(numbers));
+    }
+
+    [Test]
+    public void Test3()
+    {
+        int[] numbers = {3, 87, 45, 12, 7};
+        Assert.AreEqual(10, Kata.sumTwoSmallestNumbers(numbers));
+    }
+
+    [Test]
+    public void Test4()
+    {
+        int[] numbers = {23, 71, 33, 82, 1};
+        Assert.AreEqual(24, Kata.sumTwoSmallestNumbers(numbers));
+    }
+
+    [Test]
+    public void Test5()
+    {
+        int[] numbers = {52, 76, 14, 12, 4};
+        Assert.AreEqual(16, Kata.sumTwoSmallestNumbers(numbers));
+    }
+
+    [Test]
+    public void Test6()
+    {
+        int[] numbers = {1000, 2, 3, 6};
+        Assert.AreEqual(5, Kata.sumTwoSmallestNumbers(numbers));
+    }
+
+    [Test]
+    public void TestRandomNumbers()
+    {
+        Random rnd = new Random();
+        for (int run = 0; run < 100; ++run)
+        {
+            var numbers = new int[rnd.Next(4, 100)];
+            int min1 = Int32.MaxValue;
+            int min2 = Int32.MaxValue;
+            for(int i = 0; i < numbers.Length; ++i)
+            {
+                int n = rnd.Next(1, 1000000);
+                if(n < min1) { min2 = min1; min1 = n; }
+                else if(n < min2) min2 = n;
+                numbers[i] = n;
+            }
+            int expected = min1 + min2;
+            int actual = Kata.sumTwoSmallestNumbers(numbers);
+            Assert.AreEqual(expected, actual, "Lowest numbers are " + min1 + " and " + min2);
+        }
+    }
 }
