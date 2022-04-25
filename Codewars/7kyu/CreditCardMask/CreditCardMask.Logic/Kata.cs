615ce4649c9ae2fe269b1ca0547d11c2669a804a
Roberto <rpgrca@gmail.com>
2022-04-24 23:11:42 -0300

    solve kata, add extra tests

--- Codewars/7kyu/CreditCardMask/CreditCardMask.Logic/Kata.cs
+++ Codewars/7kyu/CreditCardMask/CreditCardMask.Logic/Kata.cs
@@ -1,9 +1,13 @@
 ﻿namespace CreditCardMask.Logic;
 
+using System.Text.RegularExpressions;
+
 public static class Kata
 {
     // return masked string
     public static string Maskify(string cc)
     {
+        if (cc.Length < 4) return cc;
+        return $"{new string('#', cc.Length - 4)}{cc[^4..]}";
     }
 }
--- Codewars/7kyu/CreditCardMask/CreditCardMask.UnitTests/Tests.cs
+++ Codewars/7kyu/CreditCardMask/CreditCardMask.UnitTests/Tests.cs
@@ -1,16 +1,36 @@
-namespace CreditCardMask.UnitTests;
-
+using System;
 using NUnit.Framework;
 using CreditCardMask.Logic;
 
 [TestFixture]
 public class Tests
 {
-  [Test]
-  public void ExamplesTests()
-  {
-    Assert.AreEqual("############5616", Kata.Maskify("4556364607935616"));
-    Assert.AreEqual("1", Kata.Maskify("1"));
-    Assert.AreEqual("#1111", Kata.Maskify("11111"));
-  }
+    [Test]
+    public void ExamplesTests()
+    {
+        Assert.AreEqual("############5616", Kata.Maskify("4556364607935616"));
+        Assert.AreEqual("1", Kata.Maskify("1"));
+        Assert.AreEqual("#1111", Kata.Maskify("11111"));
+    }
+
+    [Test]
+    public void RandomTests()
+    {
+        Func<string, string> solution = (cc) =>
+        {
+            return cc.Length <= 4 ? cc : new String('#', cc.Length - 4) + cc.Substring(cc.Length - 4);
+        };
+        Random rand = new Random();
+        Func<string> randomToken = () =>
+        {
+            return rand.Next(1000, 9999).ToString();
+        };
+
+        for (int i = 0; i < 100; i++)
+        {
+            string t = randomToken() + randomToken() + randomToken() + randomToken();
+            t = t.Substring(0, 1 + (rand.Next(1, 15) % t.Length));
+            Assert.AreEqual(solution(t), Kata.Maskify(t));
+        }
+    }
 }
