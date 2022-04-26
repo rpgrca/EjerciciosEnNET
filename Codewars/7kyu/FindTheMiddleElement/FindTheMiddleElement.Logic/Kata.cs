e718d520df2c8dc8c7815c4d628d3f07e6156d2e
Roberto <rpgrca@gmail.com>
2022-04-25 23:30:58 -0300

    solve kata

--- Codewars/7kyu/FindTheMiddleElement/FindTheMiddleElement.Logic/Kata.cs
+++ Codewars/7kyu/FindTheMiddleElement/FindTheMiddleElement.Logic/Kata.cs
@@ -2,8 +2,6 @@
 
 public class Kata
 {
-    public static int Gimme(double[] inputArray)
-    {
-        // Implement this function
-    }
+    public static int Gimme(double[] inputArray) =>
+        inputArray.Select((p, i) => (p, i)).OrderBy(p => p.p).Skip(1).First().i;
 }
