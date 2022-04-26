6484b06a6bb3cc0dc8a4e70ee78265fd7ed5e219
Roberto <rpgrca@gmail.com>
2022-04-26 00:21:37 -0300

    solve kata

--- Codewars/7kyu/JadenCasingStrings/JadenCasingStrings.Logic/JadenCase.cs
+++ Codewars/7kyu/JadenCasingStrings/JadenCasingStrings.Logic/JadenCase.cs
@@ -1,11 +1,10 @@
 ﻿namespace JadenCasingStrings.Logic;
 
 using System;
+using System.Globalization;
 
 public static class JadenCase
 {
-    public static string ToJadenCase(this string phrase)
-    {
-        return String.Empty;
-    }
+    public static string ToJadenCase(this string phrase) =>
+        string.Join(" ", phrase.Split(" ").Select(p => CultureInfo.CurrentCulture.TextInfo.ToTitleCase(p)));
 }
