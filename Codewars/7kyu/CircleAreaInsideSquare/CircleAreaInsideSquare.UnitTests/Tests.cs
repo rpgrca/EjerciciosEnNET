5eb9f5dfa8dc08c9ca49006931b5cf9a2ee7ed1d
Roberto <rpgrca@gmail.com>
2022-04-25 21:16:35 -0300

    add extra tests

--- Codewars/7kyu/CircleAreaInsideSquare/CircleAreaInsideSquare.UnitTests/Tests.cs
+++ Codewars/7kyu/CircleAreaInsideSquare/CircleAreaInsideSquare.UnitTests/Tests.cs
@@ -1,24 +1,34 @@
-namespace Kata {
-    using NUnit.Framework;
-    using System;
-    using CircleAreaInsideSquare.Logic;
+namespace Kata;
 
-    [TestFixture]
-    public class Tests
+using NUnit.Framework;
+using System;
+
+[TestFixture]
+public class Tests
+{
+    [Test]
+    public void Test9()
     {
-        [Test]
-        public void Test9()
-        {
-            var result = CircleAreaInsideSquare.Logic.Convert.SquareAreaToCircle(9);
-            Assert.AreEqual(result.GetType(), typeof(double),"should a be double");
-            Assert.AreEqual(Math.Round(result,8), Math.Round(7.0685834705770345d,8));
-        }
+        var result = CircleAreaInsideSquare.Logic.Convert.SquareAreaToCircle(9);
+        Assert.AreEqual(result.GetType(), typeof(double),"should a be double");
+        Assert.AreEqual(Math.Round(result,8), Math.Round(7.0685834705770345d,8));
+    }
 
-        [Test]
-        public void Test20()
-        {
-            var result = CircleAreaInsideSquare.Logic.Convert.SquareAreaToCircle(20);
-            Assert.AreEqual(Math.Round(result,8), Math.Round(15.707963267948969d,8));
+    [Test]
+    public void Test20()
+    {
+        var result = CircleAreaInsideSquare.Logic.Convert.SquareAreaToCircle(20);
+        Assert.AreEqual(Math.Round(result,8), Math.Round(15.707963267948969d,8));
+    }
+
+    [Test]
+    public void RandomTest()
+    {
+        var sqr = new Func<double, double>((size) => Math.Pow(Math.Sqrt(size) / 2,2) * Math.PI);
+        var r = new Random();
+        for(var i = 0; i < 10; i++){
+            var input = r.NextDouble();
+            Assert.AreEqual(Math.Round(CircleAreaInsideSquare.Logic.Convert.SquareAreaToCircle(input),8), Math.Round(sqr(input),8));
         }
     }
 }
