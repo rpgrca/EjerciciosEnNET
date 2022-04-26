c20d37640016d5b0b94cfec19ba119f0ec249ff1
Roberto <rpgrca@gmail.com>
2022-04-26 00:27:40 -0300

    simplify

--- Codewars/7kyu/JadenCasingStrings/JadenCasingStrings.Logic/JadenCase.cs
+++ Codewars/7kyu/JadenCasingStrings/JadenCasingStrings.Logic/JadenCase.cs
@@ -6,5 +6,5 @@ using System.Globalization;
 public static class JadenCase
 {
     public static string ToJadenCase(this string phrase) =>
-        string.Join(" ", phrase.Split(" ").Select(p => CultureInfo.CurrentCulture.TextInfo.ToTitleCase(p)));
+        CultureInfo.CurrentCulture.TextInfo.ToTitleCase(phrase);
 }
