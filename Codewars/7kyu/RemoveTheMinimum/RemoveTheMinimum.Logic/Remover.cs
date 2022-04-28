ef6a181dff72dcddd0186cd32c4a76b101497296
Roberto <rpgrca@gmail.com>
2022-04-28 11:35:57 -0300

    solve kata

--- Codewars/7kyu/RemoveTheMinimum/RemoveTheMinimum.Logic/Remover.cs
+++ Codewars/7kyu/RemoveTheMinimum/RemoveTheMinimum.Logic/Remover.cs
@@ -8,6 +8,17 @@ public class Remover
 {
     public static List<int> RemoveSmallest(List<int> numbers)
     {
-        // Good Luck!
+        var minimumValue = int.MaxValue;
+        var minimumIndex = 0;
+        for (var index = 0; index < numbers.Count; index++)
+        {
+            if (numbers[index] < minimumValue)
+            {
+                minimumValue = numbers[index];
+                minimumIndex = index;
+            }
+        }
+
+        return numbers.Select((p, i) => new { p, i}).Where(p => p.i != minimumIndex).Select(p => p.p).ToList();
     }
 }
