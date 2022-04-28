de398ae74c791438a9274a380b8adfbc54b9d389
Roberto <rpgrca@gmail.com>
2022-04-28 00:24:29 -0300

    solve kata

--- Codewars/7kyu/Largest5DigitNumberInSeries/Largest5DigitNumberInSeries.Logic/Kata.cs
+++ Codewars/7kyu/Largest5DigitNumberInSeries/Largest5DigitNumberInSeries.Logic/Kata.cs
@@ -4,8 +4,9 @@ using System;
 
 public class Kata
 {
-    public static int GetNumber(string str)
-    {
-        return 0;
-    }
+    public static int GetNumber(string str) =>
+        str[0..^4].Zip(str[1..^3]).Zip(str[2..^2]).Zip(str[3..^1]).Zip(str[4..])
+            .Select(i => ((i.First.First.First.First - '0') * 10000) + ((i.First.First.First.Second - '0') * 1000) + ((i.First.First.Second - '0') * 100) + ((i.First.Second - '0') * 10) + (i.Second - '0'))
+            .OrderByDescending(p => p)
+            .First();
 }
