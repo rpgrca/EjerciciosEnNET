26fa45db66e0f95b24f63b56ceec1425003e1e8c
Roberto <rpgrca@gmail.com>
2022-04-28 21:55:41 -0300

    pass test

--- Codewars/8kyu/Holiday4SharkPontoon/Holiday4SharkPontoon.Logic/Kata.cs
+++ Codewars/8kyu/Holiday4SharkPontoon/Holiday4SharkPontoon.Logic/Kata.cs
@@ -2,14 +2,8 @@
 
 public class Kata
 {
-    public static string Shark(
-      int pontoonDistance,
-      int sharkDistance,
-      int yourSpeed,
-      int sharkSpeed,
-      bool dolphin)
-    {
-        //your code here
-        return "Alive!";
-    }
+    public static string Shark(int pontoonDistance, int sharkDistance, int yourSpeed, int sharkSpeed, bool dolphin) =>
+        sharkDistance / (float)sharkSpeed < pontoonDistance / (float)yourSpeed / (dolphin? 2 : 1)
+            ? "Shark Bait!"
+            : "Alive!";
 }
