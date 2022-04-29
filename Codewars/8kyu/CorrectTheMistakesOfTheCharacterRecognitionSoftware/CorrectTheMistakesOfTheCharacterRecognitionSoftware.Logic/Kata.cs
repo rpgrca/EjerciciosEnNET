ddb26b1d3e457cb2b0fd02e6c1c39686033b138d
Roberto <rpgrca@gmail.com>
2022-04-28 22:32:13 -0300

    solve test

--- Codewars/8kyu/CorrectTheMistakesOfTheCharacterRecognitionSoftware/CorrectTheMistakesOfTheCharacterRecognitionSoftware.Logic/Kata.cs
+++ Codewars/8kyu/CorrectTheMistakesOfTheCharacterRecognitionSoftware/CorrectTheMistakesOfTheCharacterRecognitionSoftware.Logic/Kata.cs
@@ -2,8 +2,6 @@
 
 public class Kata
 {
-    public static string Correct(string text)
-    {
-        return text;
-    }
+    public static string Correct(string text) =>
+        text.Replace("5", "S").Replace("0", "O").Replace("1", "I");
 }
