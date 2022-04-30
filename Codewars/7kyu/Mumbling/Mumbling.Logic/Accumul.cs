0ffabc373963a62ee4b8be2e6cb32eebb7fa35aa
Roberto <rpgrca@gmail.com>
2022-04-29 23:44:26 -0300

    solve kata

---
+++ Codewars/7kyu/Mumbling/Mumbling.Logic/Accumul.cs
@@ -0,0 +1,9 @@
+﻿namespace Mumbling.Logic;
+
+using System;
+
+public class Accumul
+{
+    public static String Accum(string s) =>
+        string.Join("-", s.Select((p, i) => char.ToUpper(p) + new string(char.ToLower(p), i)));
+}
---
+++ Codewars/7kyu/Mumbling/Mumbling.UnitTests/AccumulTests.cs
@@ -0,0 +1,22 @@
+using System;
+using NUnit.Framework;
+using Mumbling.Logic;
+
+[TestFixture]
+public static class AccumulTests
+{
+    private static void testing(string actual, string expected)
+    {
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public static void test1()
+    {
+        testing(Accumul.Accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+        testing(Accumul.Accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+        testing(Accumul.Accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+        testing(Accumul.Accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+        testing(Accumul.Accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
+    }
+}
