5f888e7b04bee6080031d989919f215bbca33834
Roberto <rpgrca@gmail.com>
2022-11-20 23:32:33 -0300

    add extra tests

--- Codewars/7kyu/NextPalindromicNumber/NextPalindromicNumber.UnitTests/NextPalMust.cs
+++ Codewars/7kyu/NextPalindromicNumber/NextPalindromicNumber.UnitTests/NextPalMust.cs
@@ -16,5 +16,38 @@ public class NextPalMust
             Assert.AreEqual(202, Kata.NextPal(191));
             Assert.AreEqual(2552, Kata.NextPal(2541));
         }
+
+        [Test]
+        public void RandomTests()
+        {
+            var rand = new Random();
+            
+            Func<int,int> myNextPal = delegate (int val)
+            {
+                do
+                {
+                  val++;
+                }
+                while(val.ToString() != string.Concat(val.ToString().Reverse()));
+             
+                return val;
+            };
+            
+            Action<int,int> TestFor = delegate (int rStart, int rEnd)
+            {      
+                for(var i=0;i<40;i++)
+                {
+                  var val = rand.Next(rStart, rEnd);
+                  Console.WriteLine("val = " + val);
+                  Assert.AreEqual(myNextPal(val), Kata.NextPal(val));
+                }
+            };
+            
+            TestFor(100,10001);
+            TestFor(10000,100001);
+            TestFor(100000,1000001);
+            TestFor(1000000,10000001);
+            TestFor(10000000,100000001);
+        }
     }
 }
