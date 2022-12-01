4a1923f5587d358fb596ba47daabbe82a9b04a43
Roberto <rpgrca@gmail.com>
2022-12-01 07:36:30 -0300

    solve first puzzle

---
+++ AdventOfCode/2022/Day1/Day1.Logic/CalorieCounter.cs
@@ -0,0 +1,40 @@
+﻿namespace Day1.Logic;
+public class CalorieCounter
+{
+    private string _input;
+
+    public int MostCaloriesCarriedBySingleElf { get; set; }
+
+    public CalorieCounter(string input)
+    {
+        this._input = input;
+
+        Parse();
+    }
+
+    private void Parse()
+    {
+        var lines = _input.Split("\n");
+        var total = 0;
+        var maximum = 0;
+
+        foreach (var line in lines)
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                if (total > maximum)
+                {
+                    maximum = total;
+                }
+
+                total = 0;
+            }
+            else
+            {
+                total += int.Parse(line);
+            }
+        }
+
+        MostCaloriesCarriedBySingleElf = maximum;
+    }
+}
---
+++ AdventOfCode/2022/Day1/Day1.UnitTests/CalorieCounterMust.cs
@@ -0,0 +1,21 @@
+namespace Day1.UnitTests;
+
+using Day1.Logic;
+using static Constants;
+
+public class CalorieCounterMust
+{
+    [Fact]
+    public void SolveFirstSampleCorrectly()
+    {
+        var sut = new CalorieCounter(SAMPLE_INPUT);
+        Assert.Equal(24000, sut.MostCaloriesCarriedBySingleElf);
+    }
+
+    [Fact]
+    public void SolveFirstPuzzleCorrectly()
+    {
+        var sut = new CalorieCounter(PUZZLE_INPUT);
+        Assert.Equal(69501, sut.MostCaloriesCarriedBySingleElf);
+    }
+}
