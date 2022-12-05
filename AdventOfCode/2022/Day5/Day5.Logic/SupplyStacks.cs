9fbde22cc5c0da38bba0b486d503ba342c5a8fe5
Roberto <rpgrca@gmail.com>
2022-12-05 07:22:16 -0300

    add test, load sample correctly

--- AdventOfCode/2022/Day5/Day5.Logic/SupplyStacks.cs
+++ AdventOfCode/2022/Day5/Day5.Logic/SupplyStacks.cs
@@ -1,5 +1,48 @@
 ﻿namespace Day5.Logic;
 public class SupplyStacks
 {
+    private string _input;
+    private int _stackCount;
 
+    public List<List<char>> Stacks { get; private set; }
+
+    public SupplyStacks(string input, int stackCount)
+    {
+        _input = input;
+        _stackCount = stackCount;
+        Stacks = new List<List<char>>();
+
+        for (var index = 0; index < _stackCount; index++)
+        {
+            Stacks.Add(new List<char>());
+        }
+
+        Parse();
+    }
+
+    private void Parse()
+    {
+        foreach (var line in _input.Split("\n"))
+        {
+            if (line[1] == '1')
+            {
+                break;
+            }
+
+            if (line[1] != ' ')
+            {
+                Stacks[0].Add(line[1]);
+            }
+
+            if (line[5] != ' ')
+            {
+                Stacks[1].Add(line[5]);
+            }
+
+            if (line[9] != ' ')
+            {
+                Stacks[2].Add(line[9]);
+            }
+        }
+    }
 }
--- AdventOfCode/2022/Day5/Day5.UnitTests/SupplyStacksMust.cs
+++ AdventOfCode/2022/Day5/Day5.UnitTests/SupplyStacksMust.cs
@@ -1,10 +1,26 @@
+using Day5.Logic;
+using static Day5.UnitTests.Constants;
+
 namespace Day5.UnitTests;
 
 public class SupplyStacksMust
 {
     [Fact]
-    public void Test1()
+    public void LoadSampleInputCorrectly()
     {
-
+        var sut = new SupplyStacks(SAMPLE_INPUT, 3);
+        Assert.Collection(sut.Stacks,
+            p1 => {
+                Assert.Equal('N', p1[0]);
+                Assert.Equal('Z', p1[1]);
+            },
+            p2 => {
+                Assert.Equal('D', p2[0]);
+                Assert.Equal('C', p2[1]);
+                Assert.Equal('M', p2[2]);
+            },
+            p3 => {
+                Assert.Equal('P', p3[0]);
+            });
     }
 }
