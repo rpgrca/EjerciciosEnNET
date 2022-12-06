a5ba5bf56d5c972e7a0fcf17e514c91687f41542
Roberto <rpgrca@gmail.com>
2022-12-06 07:41:09 -0300

    solve sample

--- AdventOfCode/2022/Day6/Day6.UnitTests/TuningTroubleMust.cs
+++ AdventOfCode/2022/Day6/Day6.UnitTests/TuningTroubleMust.cs
@@ -1,4 +1,5 @@
 using Day6.Logic;
+using static Day6.UnitTests.Constants;
 
 namespace Day6.UnitTests;
 
@@ -15,9 +16,18 @@ public class TuningTroubleMust
     [InlineData("jpqm", 4)]
     [InlineData("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)]
     [InlineData("nppdvjthqldpwncqszvftbrmjlhg", 6)]
+    [InlineData("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)]
+    [InlineData("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
     public void DetectStartOfPacketCorrectly(string input, int expectedLength)
     {
         var sut = new TuningTrouble(input);
         Assert.Equal(expectedLength, sut.ProcessedForStartOfPacket);
+    }
+
+    [Fact]
+    public void SolveFirstSample()
+    {
+        var sut = new TuningTrouble(SAMPLE_INPUT);
+        Assert.Equal(7, sut.ProcessedForStartOfPacket);
     }
 }
