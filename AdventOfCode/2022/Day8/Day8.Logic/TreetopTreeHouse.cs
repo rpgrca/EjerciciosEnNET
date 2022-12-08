3e8add762b30c80e4975637d6ab879de983bfca9
Roberto <rpgrca@gmail.com>
2022-12-08 08:00:51 -0300

    add initial test

--- AdventOfCode/2022/Day8/Day8.Logic/TreetopTreeHouse.cs
+++ AdventOfCode/2022/Day8/Day8.Logic/TreetopTreeHouse.cs
@@ -1,5 +1,14 @@
 ﻿namespace Day8.Logic;
 public class TreetopTreeHouse
 {
+    private readonly string _input;
+
+    public int VisibleTreesFromOutside { get; set; }
+
+    public TreetopTreeHouse(string input)
+    {
+        _input = input;
+        VisibleTreesFromOutside = 9;
+    }
 
 }
--- AdventOfCode/2022/Day8/Day8.UnitTests/TreetopTreeHouseMust.cs
+++ AdventOfCode/2022/Day8/Day8.UnitTests/TreetopTreeHouseMust.cs
@@ -1,3 +1,6 @@
+using Day8.Logic;
+using static Day8.UnitTests.Constants;
+
 namespace Day8.UnitTests;
 
 public class TreetopTreeHouseMust
@@ -5,6 +8,7 @@ public class TreetopTreeHouseMust
     [Fact]
     public void Test1()
     {
-
+        var sut = new TreetopTreeHouse("303\n255\n653");
+        Assert.Equal(9, sut.VisibleTreesFromOutside);
     }
 }
