2cb5910a9398ef134319de5abf1e50ef39bd461a
Roberto <rpgrca@gmail.com>
2022-12-09 07:37:06 -0300

    add tests, movements right

--- AdventOfCode/2022/Day9/Day9.Logic/RopeBridge.cs
+++ AdventOfCode/2022/Day9/Day9.Logic/RopeBridge.cs
@@ -2,5 +2,42 @@
 
 public class RopeBridge
 {
+    private readonly string _input;
+    private (int X, int Y) _head;
+    private (int X, int Y) _tail;
+
+    public int VisitedPositions { get; private set; }
+
+    public RopeBridge(string input)
+    {
+        _input = input;
+        VisitedPositions = 1;
+
+        var lines = _input.Split("\n");
+        foreach (var line in lines)
+        {
+            var splittedLine = line.Split(" ");
+            (string direction, int steps) = (splittedLine[0], int.Parse(splittedLine[1]));
+            switch (direction)
+            {
+                case "R":
+                    for (var step = 0; step < steps; step++)
+                    {
+                        MoveRight();
+                    }
+                    break;
+            }
+        }
+    }
+
+    private void MoveRight()
+    {
+        _head.X++;
+        if (Math.Abs(_head.X - _tail.X) > 1)
+        {
+            _tail.X++;
+            VisitedPositions++;
+        }
+    }
 
 }
--- AdventOfCode/2022/Day9/Day9.UnitTests/RopeBridgeMust.cs
+++ AdventOfCode/2022/Day9/Day9.UnitTests/RopeBridgeMust.cs
@@ -5,9 +5,15 @@ namespace Day9.UnitTests;
 
 public class RopeBridgeMust
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData("R 1", 1)]
+    [InlineData("R 2", 2)]
+    [InlineData("R 1\nR 1", 2)]
+    [InlineData("R 4", 4)]
+    [InlineData("R 1\nL 1", 1)]
+    public void Test1(string input, int expectedVisitedPositions)
     {
-
+        var sut = new RopeBridge(input);
+        Assert.Equal(expectedVisitedPositions, sut.VisitedPositions);
     }
 }
