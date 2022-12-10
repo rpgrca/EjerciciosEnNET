3ce3bac05335476089abe10b3eeac3ae2bbbd6bc
Roberto <rpgrca@gmail.com>
2022-12-10 09:21:25 -0300

    add tests, calculate signal strength

--- AdventOfCode/2022/Day10/Day10.Logic/CathodeRayTube.cs
+++ AdventOfCode/2022/Day10/Day10.Logic/CathodeRayTube.cs
@@ -3,14 +3,19 @@
 public class CathodeRayTube
 {
     private readonly string _input;
+    private readonly int[] _samples;
     private readonly string[] _lines;
+    private readonly int _cycle;
 
     public int X { get; private set; }
+    public int SignalStrength { get; private set; }
 
-    public CathodeRayTube(string input)
+    public CathodeRayTube(string input, int[] samples)
     {
         _input = input;
+        _samples = samples;
         _lines = input.Split("\n");
+        _cycle = 1;
         X = 1;
 
         foreach (var line in _lines)
@@ -19,9 +24,28 @@ public class CathodeRayTube
             switch (instruction[0])
             {
                 case "noop":
+                    if (_samples.Contains(_cycle))
+                    {
+                        SignalStrength += _cycle * X;
+                    }
+
+                    _cycle += 1;
                     break;
 
                 case "addx":
+                    if (_samples.Contains(_cycle))
+                    {
+                        SignalStrength += _cycle * X;
+                    }
+
+                    _cycle += 1;
+
+                    if (_samples.Contains(_cycle))
+                    {
+                        SignalStrength += _cycle * X;
+                    }
+
+                    _cycle += 1;
                     X += int.Parse(instruction[1]);
                     break;
             }
--- AdventOfCode/2022/Day10/Day10.UnitTests/CathodeRayTubeMust.cs
+++ AdventOfCode/2022/Day10/Day10.UnitTests/CathodeRayTubeMust.cs
@@ -10,9 +10,24 @@ public class CathodeRayTubeMust
     [InlineData("noop", 1)]
     [InlineData("addx 1\nnoop", 2)]
     [InlineData("noop\naddx 3\naddx -5", -1)]
-    public void Test1(string input, int expectedValue)
+    public void ExecuteInstructionsCorrectly(string input, int expectedValue)
     {
-        var sut = new CathodeRayTube(input);
+        var sut = new CathodeRayTube(input, Array.Empty<int>());
         Assert.Equal(expectedValue, sut.X);
     }
+
+    [Fact]
+    public void CalculateSignalStrengthCorrectly1()
+    {
+        var sut = new CathodeRayTube("noop\naddx 3\naddx -5", new[] { 1, 4 });
+        Assert.Equal(17, sut.SignalStrength);
+    }
+
+    [Fact]
+    public void CalculateSignalStrengthCorrectly2()
+    {
+        var sut = new CathodeRayTube("noop\naddx 3\naddx -5", new[] { 4, 5 });
+        Assert.Equal(36, sut.SignalStrength);
+    }
+
 }
