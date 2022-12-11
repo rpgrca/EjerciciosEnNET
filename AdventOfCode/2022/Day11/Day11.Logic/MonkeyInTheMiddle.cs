0c97d94de6c3334da48a132ed0634aa424a5f1a2
Roberto <rpgrca@gmail.com>
2022-12-11 09:34:33 -0300

    add test, load sample data correctly (hardcoded)

--- AdventOfCode/2022/Day11/Day11.Logic/MonkeyInTheMiddle.cs
+++ AdventOfCode/2022/Day11/Day11.Logic/MonkeyInTheMiddle.cs
@@ -1,14 +1,47 @@
 ﻿namespace Day11.Logic;
 
-public class MonkeyInTheMiddle
+public class Monkey
 {
-    private string _input;
+    public List<int> Items { get; }
+    private Func<int, int> Operation { get; }
+    private Func<int, bool> Test { get; }
+    private Action<List<Monkey>, int> Success { get; }
+    private Action<List<Monkey>, int> Failure { get; }
 
-    public int MonkeysCount { get; set; } = 1;
-
-    public MonkeyInTheMiddle(string input)
+    public Monkey(Func<int, int> operation, Func<int, bool> test, Action<List<Monkey>, int> success, Action<List<Monkey>, int> failure, params int[] items)
     {
-        _input = input;
+        Items = new List<int>(items);
+        Operation = operation;
+        Test = test;
+        Success = success;
+        Failure = failure;
     }
 
+    public void Give(int i)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class MonkeyInTheMiddle
+{
+    public List<Monkey> Monkeys { get; set; }
+
+    public static MonkeyInTheMiddle CreateForFirstPuzzle(string input)
+    {
+        var monkeys = new List<Monkey>
+        {
+            new(o => o * 19, t => (t % 23) == 0, (m, i) => m[2].Give(i), (m, i) => m[3].Give(i), 79, 98),
+            new(o => o + 6, t => t % 19 == 0, (m, i) => m[2].Give(i), (m, i) => m[0].Give(i), 54, 65, 75, 74),
+            new(o => o * o, t => t % 13 == 0, (m, i) => m[1].Give(i), (m, i) => m[3].Give(i), 79, 60, 97),
+            new(o => o + 3, t => t % 17 == 0, (m, i) => m[0].Give(i), (m, i) => m[1].Give(i), 74)
+        };
+
+        return new MonkeyInTheMiddle(input, monkeys);
+    }
+
+    private MonkeyInTheMiddle(string input, List<Monkey> monkeys)
+    {
+        Monkeys = monkeys;
+    }
 }
--- AdventOfCode/2022/Day11/Day11.UnitTests/MonkeyInTheMiddleMust.cs
+++ AdventOfCode/2022/Day11/Day11.UnitTests/MonkeyInTheMiddleMust.cs
@@ -6,16 +6,35 @@ namespace Day11.UnitTests;
 public class MonkeyInTheMiddleMust
 {
     [Fact]
-    public void Test1()
+    public void LoadSampleMonkeyItemsCorrectly()
     {
-        const string input = @"Monkey 0:
-  Starting items: 79, 98
-  Operation: new = old * 19
-  Test: divisible by 23
-    If true: throw to monkey 2
-    If false: throw to monkey 3";
-
-        var sut = new MonkeyInTheMiddle(input);
-        Assert.Equal(1, sut.MonkeysCount);
+        var sut = MonkeyInTheMiddle.CreateForFirstPuzzle(SAMPLE_INPUT);
+        Assert.Collection(sut.Monkeys,
+            m0 =>
+            {
+                Assert.Collection(m0.Items,
+                    i1 => Assert.Equal(79, i1),
+                    i2 => Assert.Equal(98, i2));
+            },
+            m1 =>
+            {
+                Assert.Collection(m1.Items,
+                    i1 => Assert.Equal(54, i1),
+                    i2 => Assert.Equal(65, i2),
+                    i3 => Assert.Equal(75, i3),
+                    i4 => Assert.Equal(74, i4));
+            },
+            m2 =>
+            {
+                Assert.Collection(m2.Items,
+                    i1 => Assert.Equal(79, i1),
+                    i2 => Assert.Equal(60, i2),
+                    i3 => Assert.Equal(97, i3));
+            },
+            m3 =>
+            {
+                Assert.Collection(m3.Items,
+                    i1 => Assert.Equal(74, i1));
+            });
     }
 }
