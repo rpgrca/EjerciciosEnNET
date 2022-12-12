f93bad45e7d9a3788a0f777443d6002849e6b82c
Roberto <rpgrca@gmail.com>
2022-12-12 07:38:24 -0300

    add tests, find trivial solutions

--- AdventOfCode/2022/Day12/Day12.Logic/HillClimbingAlgorithm.cs
+++ AdventOfCode/2022/Day12/Day12.Logic/HillClimbingAlgorithm.cs
@@ -2,5 +2,46 @@
 
 public class HillClimbingAlgorithm
 {
+    private string _input;
+    private string[] _lines;
+    private readonly int _columns;
+    private readonly int _rows;
+
+    public int FewestStepsToTarget { get; private set; }
+
+    public HillClimbingAlgorithm(string input)
+    {
+        _input = input;
+        _lines = _input.Split("\n");
+        _columns = _lines[0].Length;
+        _rows = _lines.Length;
+        FewestStepsToTarget = int.MaxValue;
+
+        StartAlgorithm();
+    }
+
+    private void StartAlgorithm()
+    {
+        MoveFrom(0, 0, 0);
+    }
+
+    private void MoveFrom(int x, int y, int steps)
+    {
+        if (x >= _columns || y >= _rows)
+        {
+            return;
+        }
+
+        if (_lines[y][x] == 'E')
+        {
+            FewestStepsToTarget = steps;
+            return;
+        }
+        else
+        {
+            MoveFrom(x + 1, y, steps + 1);
+            MoveFrom(x, y + 1, steps + 1);
+        }
+    }
 
 }
--- AdventOfCode/2022/Day12/Day12.UnitTests/HillClimbingAlgorithmMust.cs
+++ AdventOfCode/2022/Day12/Day12.UnitTests/HillClimbingAlgorithmMust.cs
@@ -5,8 +5,21 @@ namespace Day12.UnitTests;
 
 public class HillClimbingAlgorithmMust
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData("SE", 1)]
+    [InlineData("SaE", 2)]
+    [InlineData("SabE", 3)]
+    public void FindSteps_WhenMapIsHorizontal(string input, int expectedValue)
     {
+        var sut = new HillClimbingAlgorithm(input);
+        Assert.Equal(expectedValue, sut.FewestStepsToTarget);
+    }
+
+    [Theory]
+    [InlineData("S\nE", 1)]
+    public void FindSteps_WhenMapIsVertical(string input, int expectedValue)
+    {
+        var sut = new HillClimbingAlgorithm(input);
+        Assert.Equal(expectedValue, sut.FewestStepsToTarget);
     }
 }
