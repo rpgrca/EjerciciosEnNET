1c6a78c6c096eed3246c6c77cfd58ce32ec1e1b9
Roberto <rpgrca@gmail.com>
2022-12-13 07:24:03 -0300

    solve trivial integer examples

--- AdventOfCode/2022/Day13/Day13.Logic/DistressSignalDecoder.cs
+++ AdventOfCode/2022/Day13/Day13.Logic/DistressSignalDecoder.cs
@@ -1,5 +1,67 @@
 ﻿namespace Day13.Logic;
 public class DistressSignalDecoder
 {
+    public int SumOfCorrectIndexes { get; set; }
 
+    public DistressSignalDecoder(string input)
+    {
+        SumOfCorrectIndexes = 0;
+
+        var lines = input.Split("\n");
+        var sectionsAbove = lines[0][1..^1].Split(",").Select(p => int.Parse(p)).ToList();
+        var sectionsBelow = lines[1][1..^1].Split(",").Select(p => int.Parse(p)).ToList();
+
+        if (sectionsAbove.Count == sectionsBelow.Count)
+        {
+            for (var index = 0; index < sectionsAbove.Count; index++)
+            {
+                if (sectionsAbove[index] < sectionsBelow[index])
+                {
+                    SumOfCorrectIndexes += 1;
+                    return;
+                }
+                else if (sectionsAbove[index] > sectionsBelow[index])
+                {
+                    return;
+                }
+            }
+
+            // equals?
+        }
+        else if (sectionsAbove.Count < sectionsBelow.Count)
+        {
+            for (var index = 0; index < sectionsAbove.Count; index++)
+            {
+                if (sectionsAbove[index] < sectionsBelow[index])
+                {
+                    SumOfCorrectIndexes += 1;
+                    return;
+                }
+                else if (sectionsAbove[index] > sectionsBelow[index])
+                {
+                    return;
+                }
+            }
+
+            SumOfCorrectIndexes += 1;
+            return;
+        }
+        else
+        {
+            for (var index = 0; index < sectionsBelow.Count; index++)
+            {
+                if (sectionsAbove[index] < sectionsBelow[index])
+                {
+                    SumOfCorrectIndexes += 1;
+                    return;
+                }
+                else if (sectionsAbove[index] > sectionsBelow[index])
+                {
+                    return;
+                }
+            }
+
+            return;
+        }
+    }
 }
--- AdventOfCode/2022/Day13/Day13.UnitTests/DistressSignalDecoderMust.cs
+++ AdventOfCode/2022/Day13/Day13.UnitTests/DistressSignalDecoderMust.cs
@@ -1,10 +1,16 @@
+using Day13.Logic;
+using static Day13.UnitTests.Constants;
+
 namespace Day13.UnitTests;
 
 public class DistressSignalDecoderMust
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData("[1,1,1]\n[1,1,1,1]", 1)]
+    [InlineData("[7,7,7,7]\n[7,7,7]", 0)]
+    public void Test1(string input, int expectedSum)
     {
-
+        var sut = new DistressSignalDecoder(input);
+        Assert.Equal(expectedSum, sut.SumOfCorrectIndexes);
     }
 }
