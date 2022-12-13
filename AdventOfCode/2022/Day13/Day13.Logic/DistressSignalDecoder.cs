c423c289faeda37ed356ed04a4dac3bd8efe85dc
Roberto <rpgrca@gmail.com>
2022-12-13 07:31:47 -0300

    add tests for numbers combination

--- AdventOfCode/2022/Day13/Day13.Logic/DistressSignalDecoder.cs
+++ AdventOfCode/2022/Day13/Day13.Logic/DistressSignalDecoder.cs
@@ -8,8 +8,41 @@ public class DistressSignalDecoder
         SumOfCorrectIndexes = 0;
 
         var lines = input.Split("\n");
-        var sectionsAbove = lines[0][1..^1].Split(",").Select(p => int.Parse(p)).ToList();
-        var sectionsBelow = lines[1][1..^1].Split(",").Select(p => int.Parse(p)).ToList();
+        List<int> sectionsAbove;
+        var tokensAbove = lines[0][1..^1].Split(",").ToList();
+        if (tokensAbove.Count == 1)
+        {
+            if (string.IsNullOrWhiteSpace(tokensAbove[0]))
+            {
+                sectionsAbove = new List<int>();
+            }
+            else
+            {
+                sectionsAbove = tokensAbove.Select(int.Parse).ToList();
+            }
+        }
+        else
+        {
+            sectionsAbove = tokensAbove.Select(p => int.Parse(p)).ToList();
+        }
+        
+        List<int> sectionsBelow;
+        var tokensBelow = lines[1][1..^1].Split(",").ToList();
+        if (tokensBelow.Count == 1)
+        {
+            if (string.IsNullOrWhiteSpace(tokensBelow[0]))
+            {
+                sectionsBelow = new List<int>();
+            }
+            else
+            {
+                sectionsBelow = tokensBelow.Select(int.Parse).ToList();
+            }
+        }
+        else
+        {
+            sectionsBelow = tokensBelow.Select(int.Parse).ToList();
+        }
 
         if (sectionsAbove.Count == sectionsBelow.Count)
         {
--- AdventOfCode/2022/Day13/Day13.UnitTests/DistressSignalDecoderMust.cs
+++ AdventOfCode/2022/Day13/Day13.UnitTests/DistressSignalDecoderMust.cs
@@ -8,6 +8,9 @@ public class DistressSignalDecoderMust
     [Theory]
     [InlineData("[1,1,1]\n[1,1,1,1]", 1)]
     [InlineData("[7,7,7,7]\n[7,7,7]", 0)]
+    [InlineData("[1,1,3,1,1]\n[1,1,5,1,1]", 1)]
+    [InlineData("[]\n[3]", 1)]
+    [InlineData("[3]\n[]", 0)]
     public void Test1(string input, int expectedSum)
     {
         var sut = new DistressSignalDecoder(input);
