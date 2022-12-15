c7442c1cf1d1f35b1874955bec046fa46084211a
Roberto <rpgrca@gmail.com>
2022-12-14 22:36:52 -0300

    update script to create constant and switch to lf

--- AdventOfCode/setup.sh
+++ AdventOfCode/setup.sh
@@ -6,9 +6,13 @@ if [ $# -lt 2 ]; then
 	exit 1
 fi
 
+# Build directory structure
+mkdir -p $YEAR
 cd $YEAR
-mkdir $DAY
+mkdir -p $DAY
 cd $DAY
+
+# Create solution and projects
 dotnet new classlib -o $DAY.Logic
 dotnet new xunit -o $DAY.UnitTests
 cd $DAY.UnitTests
@@ -17,9 +21,28 @@ cd ..
 dotnet new sln
 dotnet sln $DAY.sln add **/*.csproj
 cd ..
-pwd
 dotnet sln AdventOfCode$YEAR.sln add $DAY/**/*.csproj
 cd $DAY
+
+# Create Constants.cs
+cat <<EOT > $DAY.UnitTests/Constants.cs
+namespace Day$DAY.UnitTests;
+
+public static class Constants
+{
+    public const string SAMPLE_INPUT = @"";
+
+    public const string PUZZLE_INPUT = @"";
+}
+EOT
+
+# Convert all files to LF
+dos2unix $DAY.UnitTests/$DAY.UnitTests.csproj
+dos2unix $DAY.UnitTests/UnitTest1.cs
+dos2unix $DAY.Logic/$DAY.Logic.csproj
+dos2unix $DAY.Logic/Class1.cs
+
+# Open Visual Studio Code
 code -r .
 
 exit 0
