b0a200ab3a85acef87051875fe6a727f40aa7831
Roberto <rpgrca@gmail.com>
2022-12-16 19:39:14 -0300

    clean up tests

--- AdventOfCode/2022/Day16/Day16.UnitTests/PressureReleaseValveMust.cs
+++ AdventOfCode/2022/Day16/Day16.UnitTests/PressureReleaseValveMust.cs
@@ -18,18 +18,12 @@ public class PressureReleaseValveMust
      *  +---+---+---+
      */
     [Theory]
-    [InlineData("Valve AA has flow rate=0; tunnels lead to valves BB\nValve BB has flow rate=0; tunnels lead to valves AA", 0, 0)]
-    [InlineData("Valve AA has flow rate=0; tunnels lead to valves BB\nValve BB has flow rate=13; tunnels lead to valves AA", 13, 28 * 13)]
-    public void LoadInputCorrectly(string input, int expectedFlowRate, int expectedPressureRelease)
+    [InlineData("Valve AA has flow rate=0; tunnels lead to valves BB\nValve BB has flow rate=0; tunnels lead to valves AA", 0)]
+    [InlineData("Valve AA has flow rate=0; tunnels lead to valves BB\nValve BB has flow rate=13; tunnels lead to valves AA", 28 * 13)]
+    public void LoadInputCorrectly(string input, int expectedPressureRelease)
     {
-        var sut = new PressureReleaseValve(input, new int[][] {
-            new[] { 0, 1 },
-            new[] { 1, 0 }
-        },
-        new[] { "AA", "BB" },
-        new[] { 0, 13 });
-
-        //Assert.Equal(expectedFlowRate, sut.FlowRate);
+        var graphParser = new GraphParser(input);
+        var sut = new PressureReleaseValve(input, graphParser.Graph, graphParser.Names, graphParser.Flows);
         Assert.Equal(expectedPressureRelease, sut.ReleasedPressure);
     }
 
@@ -90,16 +84,8 @@ Valve DD has flow rate=20; tunnels lead to valves CC, AA";
          *
          * Total: 24 * 55 + 2 * 42 + 2 * 20 = 1444
          */
-        var sut = new PressureReleaseValve(input, new int[][]
-        {
-            new[] { 0, 1, 2, 1 },
-            new[] { 1, 0, 1, 2 },
-            new[] { 2, 1, 0, 1 },
-            new[] { 1, 2, 1, 0 }
-        },
-        new[] { "AA", "BB", "CC", "DD" },
-        new[] { 0, 13, 22, 20 });
-        //Assert.Equal(55, sut.FlowRate);
+        var graphParser = new GraphParser(input);
+        var sut = new PressureReleaseValve(input, graphParser.Graph, graphParser.Names, graphParser.Flows);
         Assert.Equal(1444, sut.ReleasedPressure);
     }
 
@@ -164,39 +150,9 @@ Valve DD has flow rate=20; tunnels lead to valves CC, AA";
          *
          * Total: 23 * 35 + 33 * 2 + 20 * 3 = 931
          */
-        var sut = new PressureReleaseValve(input, new int[][]
-        {
-            new[] { 0, 1, 2, 1 },
-            new[] { 1, 0, 1, 2 },
-            new[] { 2, 1, 0, 1 },
-            new[] { 1, 2, 1, 0 }
-        },
-        new[] { "AA", "BB", "CC", "DD" },
-        new[] { 0, 13, 2, 20 });
-
-        //Assert.Equal(35, sut.FlowRate);
+        var graphParser = new GraphParser(input);
+        var sut = new PressureReleaseValve(input, graphParser.Graph, graphParser.Names, graphParser.Flows);
         Assert.Equal(931, sut.ReleasedPressure);
-    }
-
-    [Fact]
-    public void SolveFirstSample()
-    {
-        var sut = new PressureReleaseValve(SAMPLE_INPUT, new int[][]
-        {
-            new[] { 0, 1, 2, 1, 2, 3, 4, 5, 1, 2 },
-            new[] { 1, 0, 1, 2, 3, 4, 5, 6, 2, 3 },
-            new[] { 2, 1, 0, 1, 2, 3, 4, 5, 3, 4 },
-            new[] { 1, 2, 1, 0, 1, 2, 3, 4, 2, 3 },
-            new[] { 2, 3, 2, 1, 0, 1, 2, 3, 3, 4 },
-            new[] { 3, 4, 3, 2, 1, 0, 1, 2, 4, 5 },
-            new[] { 4, 5, 4, 3, 2, 1, 0, 1, 5, 6 },
-            new[] { 5, 6, 5, 4, 3, 2, 1, 0, 6, 7 },
-            new[] { 1, 2, 3, 2, 3, 4, 5, 6, 0, 1 },
-            new[] { 2, 3, 4, 3, 4, 5, 6, 7, 1, 0 }
-        },
-        new[] { "AA", "BB", "CC", "DD", "EE", "FF", "GG", "HH", "II", "JJ" },
-        new[] { 0, 13, 2, 20, 3, 0, 0, 22, 0, 21 });
-        Assert.Equal(1651, sut.ReleasedPressure);
     }
 
     [Fact]
@@ -212,8 +168,6 @@ Valve DD has flow rate=20; tunnels lead to valves CC, AA";
     {
         var graphParser = new GraphParser(PUZZLE_INPUT);
         var sut = new PressureReleaseValve(PUZZLE_INPUT, graphParser.Graph, graphParser.Names, graphParser.Flows);
-        //new[] { "AA", "BF", "BL", "BN", "BT", "BU", "CA", "CP", "CX", "EH", "ES", "EU", "EZ", "FC", "FS", "FU", "GS", "HV", "HX", "IF", "IL", "IR", "JG", "JJ", "JL", "JT", "JU", "KE", "KS", "MA", "MO", "NH", "NL", "NN", "NT", "OJ", "OY", "PA", "PH", "PJ", "PT", "QD", "QG", "RL", "RM", "RN", "SQ", "SV", "TG", "TO", "TX", "UV", "VJ", "XQ", "YB", "YI", "YQ", "YW", "ZH" },
-        //new[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 22, 0, 14, 0, 0, 0, 25, 0, 13, 0, 0, 0, 9, 0, 21, 0, 0, 0, 0, 0, 23, 0, 0, 11, 0, 20, 0, 18, 0, 5, 0, 0, 0, 0, 16, 0, 3, 0, 19, 0, 0, 0, 0, 0, 0, 0 });
         Assert.Equal(1716, sut.ReleasedPressure);
     }
 }
