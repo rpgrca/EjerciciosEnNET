3700fb4b6fc18a558face94582d453b598da16b9
Roberto <rpgrca@gmail.com>
2022-12-16 07:16:25 -0300

    add test, open valve

--- AdventOfCode/2022/Day16/Day16.Logic/PressureReleaseValve.cs
+++ AdventOfCode/2022/Day16/Day16.Logic/PressureReleaseValve.cs
@@ -2,14 +2,37 @@ namespace Day16.Logic;
 
 public class PressureReleaseValve
 {
-    private string _input;
+    private readonly string _input;
+    private readonly string[] _lines;
+    private int _timeLeft;
 
     public int FlowRate { get; set; }
-    public int PressureRelease { get; set; }
+    public int ReleasedPressure { get; set; }
 
     public PressureReleaseValve(string input)
     {
         _input = input;
-    }
+        _lines = input.Split("\n");
+        _timeLeft = 30;
 
+        string valve = string.Empty;
+        int flowRate = 0;
+        string other = string.Empty;
+        foreach (var line in _lines)
+        {
+            var sections = line.Split(";");
+//Valve AA has flow rate=0; tunnels lead to valves BB
+            valve = new string(line.AsSpan()[6..8]);
+            flowRate = int.Parse(sections[0].Split("=")[1]);
+            other = sections[1][24..];
+        }
+
+        _timeLeft--;
+        FlowRate = flowRate;
+        _timeLeft--;
+        while (_timeLeft-- > 0)
+        {
+            ReleasedPressure += FlowRate; 
+        }
+    }
 }
--- AdventOfCode/2022/Day16/Day16.UnitTests/PressureReleaseValveMust.cs
+++ AdventOfCode/2022/Day16/Day16.UnitTests/PressureReleaseValveMust.cs
@@ -7,10 +7,11 @@ public class PressureReleaseValveMust
 {
     [Theory]
     [InlineData("Valve AA has flow rate=0; tunnels lead to valves BB\nValve BB has flow rate=0; tunnels lead to valves AA", 0, 0)]
+    [InlineData("Valve AA has flow rate=0; tunnels lead to valves BB\nValve BB has flow rate=13; tunnels lead to valves AA", 13, 364)]
     public void Test1(string input, int expectedFlowRate, int expectedPressureRelease)
     {
         var sut = new PressureReleaseValve(input);
         Assert.Equal(expectedFlowRate, sut.FlowRate);
-        Assert.Equal(expectedPressureRelease, sut.PressureRelease);
+        Assert.Equal(expectedPressureRelease, sut.ReleasedPressure);
     }
 }
