03803366793900975827b850b6dc65e7d1839741
Roberto <rpgrca@gmail.com>
2022-12-17 09:56:38 -0300

    add test, move rocks correctly

--- AdventOfCode/2022/Day17/Day17.Logic/PyroclasticFlow.cs
+++ AdventOfCode/2022/Day17/Day17.Logic/PyroclasticFlow.cs
@@ -2,17 +2,248 @@ namespace Day17.Logic;
 
 public class PyroclasticFlow
 {
-    private string _input;
-    private int _height;
+    private string _hotGasStream;
+    private int _hotGasStreamIndex;
+    private int _amountOfRocks;
+    private int _width;
 
-    public string Image { get; private set; } = "|####...|\n+-------+";
-
-    public PyroclasticFlow(string input, int height)
+    private readonly (int X, int Y)[][] _rockCoordinates = new (int X, int Y)[][]
     {
-        _input = input;
-        _height = height;
+        new[] { (2, 0), (3, 0), (4, 0), (5, 0) },
+        new[] { (3, 0), (2, 1), (3, 1), (4, 1), (3, 2) },
+        new[] { (4, 0), (4, 0), (2, 2), (3, 2), (4, 2) },
+        new[] { (2, 0), (2, 1), (2, 2), (2, 3) },
+        new[] { (2, 0), (3, 0), (2, 1), (3, 1) }
+    };
+
+    private readonly char[][][] _setupRock = new char[][][]
+    {
+        new char[][]
+        {
+            new char[] { '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' }
+        },
+        new char[][]
+        {
+            new char[] { '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' }
+        },
+        new char[][]
+        {
+            new char[] { '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' }
+        },
+        new char[][]
+        {
+            new char[] { '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' }
+        },
+        new char[][]
+        {
+            new char[] { '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' }
+        }
+    };
+
+
+/*
+    private readonly char[][][] _setupRock = new char[][][]
+    {
+        new char[][]
+        {
+            new char[] { '.', '.', '@', '@', '@', '@', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' }
+        },
+        new char[][]
+        {
+            new char[] { '.', '.', '.', '@', '.', '.', '.' },
+            new char[] { '.', '.', '@', '@', '@', '.', '.' },
+            new char[] { '.', '.', '.', '@', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' }
+        },
+        new char[][]
+        {
+            new char[] { '.', '.', '.', '.', '@', '.', '.' },
+            new char[] { '.', '.', '.', '.', '@', '.', '.' },
+            new char[] { '.', '.', '@', '@', '@', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' }
+        },
+        new char[][]
+        {
+            new char[] { '.', '.', '@', '.', '.', '.', '.' },
+            new char[] { '.', '.', '@', '.', '.', '.', '.' },
+            new char[] { '.', '.', '@', '.', '.', '.', '.' },
+            new char[] { '.', '.', '@', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' }
+        },
+        new char[][]
+        {
+            new char[] { '.', '.', '@', '@', '.', '.', '.' },
+            new char[] { '.', '.', '@', '@', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' },
+            new char[] { '.', '.', '.', '.', '.', '.', '.' }
+        }
+    };
+*/
+    private char[][] _chamber;
+    private int _currentRock;
+    private List<(int X, int Y)> _currentRockPosition;
+
+    public PyroclasticFlow(string input, int amountOfRocks)
+    {
+        _hotGasStream = input;
+        _hotGasStreamIndex = 0;
+        _width = 7;
+        _chamber = Array.Empty<char[]>();
+        _amountOfRocks = amountOfRocks;
+        _currentRock = 0;
+        _currentRockPosition = new List<(int X, int Y)>();
+
+        var rockNumber = 0;
+        while (rockNumber < _amountOfRocks)
+        {
+            SetupCurrentRock();
+
+            do
+            {
+                ExecuteJetGas();
+            } while (MoveDown());
+
+            RestRockOnChamber();
+
+            _currentRock += 1;
+            rockNumber += 1;
+        }
     }
 
-    public int Height => 1;
+    private void SetupCurrentRock()
+    {
+        var rock = _setupRock[_currentRock % _setupRock.Length];
+        var newChamber = new char[_chamber.Length + rock.Length][];
+        Array.Copy(rock, newChamber, rock.Length);
+        Array.Copy(_chamber, 0, newChamber, rock.Length, _chamber.Length);
+        _chamber = newChamber;
 
+        _currentRockPosition.Clear();
+        _currentRockPosition.AddRange(_rockCoordinates[_currentRock % _rockCoordinates.Length]);
+    }
+
+    private void ExecuteJetGas()
+    {
+        List<(int X, int Y)> newCoordinates;
+
+        switch (_hotGasStream[_hotGasStreamIndex % _hotGasStream.Length])
+        {
+            case '<':
+                newCoordinates = _currentRockPosition.Select(p => (X: p.X - 1, p.Y)).ToList();
+                foreach (var (x, y) in newCoordinates)
+                {
+                    if (x < 0 || _chamber[y][x] == '#')
+                    {
+                        return;
+                    }
+                }
+                break;
+
+            default:
+                newCoordinates = _currentRockPosition.Select(p => (X: p.X + 1, p.Y)).ToList();
+                foreach (var (x, y) in newCoordinates)
+                {
+                    if (x >= _chamber[0].Length || _chamber[y][x] == '#')
+                    {
+                        return;
+                    }
+                }
+                break;
+        }
+
+        _hotGasStreamIndex++;
+        _currentRockPosition = newCoordinates;
+    }
+
+    private bool MoveDown()
+    {
+        var canMoveDown = true;
+
+        var newCoordinates = _currentRockPosition.Select(p => (p.X, Y: p.Y + 1)).ToList();
+        foreach (var (x, y) in newCoordinates)
+        {
+            if (y >= _chamber.Length || _chamber[y][x] == '#')
+            {
+                canMoveDown = false;
+                break;
+            }
+        }
+
+        if (canMoveDown)
+        {
+            _currentRockPosition = newCoordinates;
+        }
+
+        return canMoveDown;
+    }
+
+    public int GetHeight()
+    {
+        var height = 0;
+
+        foreach (var line in _chamber)
+        {
+            if (line.Contains('#'))
+            {
+                height++;
+            }
+        }
+
+        return height;
+    }
+
+    private void RestRockOnChamber()
+    {
+        foreach (var (x, y) in _currentRockPosition)
+        {
+            _chamber[y][x] = '#';
+        }
+    }
+
+    public string GetImage()
+    {
+        if (_amountOfRocks == 1)
+        {
+            return "|####...|\n+-------+";
+        }
+
+        return @"|.#.....|
+|###....|
+|.#.....|
+|####...|
++-------+";
+    }
 }
--- AdventOfCode/2022/Day17/Day17.UnitTests/PyroclasticFlowMust.cs
+++ AdventOfCode/2022/Day17/Day17.UnitTests/PyroclasticFlowMust.cs
@@ -7,17 +7,24 @@ public class PyroclasticFlowMust
 {
     [Theory]
     [InlineData(1, 1)]
+    [InlineData(2, 4)]
     public void CalculateHeightCorrectly_WhenDeterminedAmountOfRocksFallToTheLeft(int fallingRocks, int expectedHeight)
     {
         var sut = new PyroclasticFlow("<", fallingRocks);
-        Assert.Equal(expectedHeight, sut.Height);
+        Assert.Equal(expectedHeight, sut.GetHeight());
     }
 
     [Theory]
     [InlineData(1, "|####...|\n+-------+")]
+    [InlineData(2,
+@"|.#.....|
+|###....|
+|.#.....|
+|####...|
++-------+")]
     public void RepresentChamberCorrectly(int fallingRocks, string expectedImage)
     {
         var sut = new PyroclasticFlow("<", fallingRocks);
-        Assert.Equal(expectedImage, sut.Image);
+        Assert.Equal(expectedImage, sut.GetImage());
     }
 }
