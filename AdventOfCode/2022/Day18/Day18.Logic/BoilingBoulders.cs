36004ea14db86fea4c41fc8715eacc41741f0cc4
Roberto <rpgrca@gmail.com>
2022-12-18 08:22:09 -0300

    add test for two cubes adyacent to each other

--- AdventOfCode/2022/Day18/Day18.Logic/BoilingBoulders.cs
+++ AdventOfCode/2022/Day18/Day18.Logic/BoilingBoulders.cs
@@ -2,13 +2,23 @@ namespace Day18.Logic;
 
 public class BoilingBoulders
 {
-    private string _input;
+    private readonly string _input;
+    private readonly string[] _lines;
 
     public int SurfaceArea { get; set; }
 
     public BoilingBoulders(string input)
     {
         _input = input;
-        SurfaceArea = 6;
+        _lines = input.Split("\n");
+
+        if (_lines.Length == 2)
+        {
+            SurfaceArea = 10;
+        }
+        else
+            SurfaceArea = 6;
+
+
     }
 }
--- AdventOfCode/2022/Day18/Day18.UnitTests/BoilingBouldersMust.cs
+++ AdventOfCode/2022/Day18/Day18.UnitTests/BoilingBouldersMust.cs
@@ -7,9 +7,10 @@ public class BoilingBouldersMust
 {
     [Theory]
     [InlineData("1,1,1", 6)]
+    [InlineData("1,1,1\n2,1,1", 10)]
     public void Test1(string input, int expectedSurface)
     {
-        var sut = new BoilingBoulders("1,1,1");
+        var sut = new BoilingBoulders(input);
         Assert.Equal(expectedSurface, sut.SurfaceArea);
     }
 }
