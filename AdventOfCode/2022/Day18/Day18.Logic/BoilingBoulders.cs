80483b84fa86ab69e1bf7efa8d5985d8c43e39f6
Roberto <rpgrca@gmail.com>
2022-12-18 08:55:14 -0300

    solve first sample

--- AdventOfCode/2022/Day18/Day18.Logic/BoilingBoulders.cs
+++ AdventOfCode/2022/Day18/Day18.Logic/BoilingBoulders.cs
@@ -4,6 +4,7 @@ public class BoilingBoulders
 {
     private readonly string _input;
     private readonly string[] _lines;
+    private readonly List<(int X, int Y, int Z)> _cubes;
 
     public int SurfaceArea { get; set; }
 
@@ -11,14 +12,93 @@ public class BoilingBoulders
     {
         _input = input;
         _lines = input.Split("\n");
+        _cubes = new List<(int X, int Y, int Z)>();
 
-        if (_lines.Length == 2)
+        foreach (var line in _lines)
         {
-            SurfaceArea = 10;
+            var coordinates = line.Split(",");
+            _cubes.Add((int.Parse(coordinates[0]), int.Parse(coordinates[1]), int.Parse(coordinates[2])));
         }
-        else
-            SurfaceArea = 6;
 
+        _cubes.Sort();
 
+        TraceAway();
+    }
+
+    private void TraceAway()
+    {
+        SurfaceArea = 0;
+
+        foreach (var cube in _cubes)
+        {
+            var blockedAtLeft = false;
+            var blockedAtRight = false;
+            var blockedAtBottom = false;
+            var blockedAtTop = false;
+            var blockedAtFront = false;
+            var blockedAtBack = false;
+            var cubeSurfaceArea = 6;
+
+            if (_cubes.Any(c => c.X < cube.X && c.Y == cube.Y && c.Z == cube.Z))
+            {
+                blockedAtLeft = true;
+                if (_cubes.Contains((cube.X - 1, cube.Y, cube.Z)))
+                {
+                    cubeSurfaceArea -= 1;
+                }
+            }
+
+            if (_cubes.Any(c => c.X > cube.X && c.Y == cube.Y && c.Z == cube.Z))
+            {
+                blockedAtRight = true;
+                if (_cubes.Contains((cube.X + 1, cube.Y, cube.Z)))
+                {
+                    cubeSurfaceArea -= 1;
+                }
+            }
+
+            if (_cubes.Any(c => c.Y < cube.Y && c.X == cube.X && c.Z == cube.Z))
+            {
+                blockedAtBottom = true;
+                if (_cubes.Contains((cube.X, cube.Y - 1, cube.Z)))
+                {
+                    cubeSurfaceArea -= 1;
+                }
+            }
+
+            if (_cubes.Any(c => c.Y > cube.Y && c.X == cube.X && c.Z == cube.Z))
+            {
+                blockedAtTop = true;
+                if (_cubes.Contains((cube.X, cube.Y + 1, cube.Z)))
+                {
+                    cubeSurfaceArea -= 1;
+                }
+            }
+
+            if (_cubes.Any(c => c.Z < cube.Z && c.X == cube.X && c.Y == cube.Y))
+            {
+                blockedAtBack = true;
+                if (_cubes.Contains((cube.X, cube.Y, cube.Z - 1)))
+                {
+                    cubeSurfaceArea -= 1;
+                }
+            }
+
+            if (_cubes.Any(c => c.Z > cube.Z && c.X == cube.X && c.Y == cube.Y))
+            {
+                blockedAtFront = true;
+                if (_cubes.Contains((cube.X, cube.Y, cube.Z + 1)))
+                {
+                    cubeSurfaceArea -= 1;
+                }
+            }
+
+            if (blockedAtBack && blockedAtFront && blockedAtTop && blockedAtBottom && blockedAtLeft && blockedAtRight)
+            {
+                continue;
+            }
+
+            SurfaceArea += cubeSurfaceArea;
+        }
     }
 }
--- AdventOfCode/2022/Day18/Day18.UnitTests/BoilingBouldersMust.cs
+++ AdventOfCode/2022/Day18/Day18.UnitTests/BoilingBouldersMust.cs
@@ -8,9 +8,24 @@ public class BoilingBouldersMust
     [Theory]
     [InlineData("1,1,1", 6)]
     [InlineData("1,1,1\n2,1,1", 10)]
+    [InlineData("3,1,1\n1,1,1\n2,1,1", 14)]
     public void Test1(string input, int expectedSurface)
     {
         var sut = new BoilingBoulders(input);
         Assert.Equal(expectedSurface, sut.SurfaceArea);
+    }
+
+    [Fact]
+    public void SolveFirstSample()
+    {
+        var sut = new BoilingBoulders(SAMPLE_INPUT);
+        Assert.Equal(64, sut.SurfaceArea);
+    }
+
+    [Fact(Skip = "still undue")]
+    public void SolveFirstPuzzle()
+    {
+        var sut = new BoilingBoulders(PUZZLE_INPUT);
+        Assert.Equal(-1, sut.SurfaceArea);
     }
 }
