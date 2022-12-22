8002b502b01cba3331d7eedc7af0979a65d17256
Roberto <rpgrca@gmail.com>
2022-12-22 06:59:15 -0300

    new way of calculating

--- AdventOfCode/2022/Day19/Day19.Logic/RobotBlueprint2.cs
+++ AdventOfCode/2022/Day19/Day19.Logic/RobotBlueprint2.cs
@@ -27,20 +27,58 @@ public class RobotBlueprint2
             id = int.Parse(sentences[0][10..]);
             var costs = sentences[1].Split(".");
 
-            oreRobot = new RobotFactory(0, 0, int.Parse(costs[0].Split(" ")[5]), _oreRobot);
-            clayRobot = new RobotFactory(0, 0, int.Parse(costs[1].Split(" ")[5]), _clayRobot);
+            oreRobot = new RobotFactory(0, 0, int.Parse(costs[0].Split(" ")[5]), _oreRobot, 13);
+            clayRobot = new RobotFactory(0, 0, int.Parse(costs[1].Split(" ")[5]), _clayRobot, 7);
 
             oreCost = int.Parse(costs[2].Split(" ")[5]);
             clayCost = int.Parse(costs[2].Split(" ")[8]);
-            obsidianRobot = new RobotFactory(0, clayCost, oreCost, _obsidianRobot);
+            obsidianRobot = new RobotFactory(0, clayCost, oreCost, _obsidianRobot, 3);
 
             oreCost = int.Parse(costs[3].Split(" ")[5]);
             obsidianCost = int.Parse(costs[3].Split(" ")[8]);
-            geodeRobot = new RobotFactory(obsidianCost, 0, oreCost, _geodeRobot);
+            geodeRobot = new RobotFactory(obsidianCost, 0, oreCost, _geodeRobot, 1);
 
             Blueprints.Add(new Blueprint(id, oreRobot, clayRobot, obsidianRobot, geodeRobot));
         }
     }
 
+    public void Run()
+    {
+        var pool = new Pool();
+        var minute = 0;
+        var robotGeneration = new Pool();
+        robotGeneration.Add(0, 0, 0, 1);
 
+        foreach (var blueprint in Blueprints)
+        {
+            var maximumGeode = 0;
+
+            while (minute < 24)
+            {
+                (int, RobotFactory)[] timeForNextRobot =
+                {
+                    ( blueprint.OreRobot.UntilNextAvailable(pool, robotGeneration), blueprint.OreRobot ),
+                    ( blueprint.ClayRobot.UntilNextAvailable(pool, robotGeneration), blueprint.ClayRobot ),
+                    ( blueprint.ObsidianRobot.UntilNextAvailable(pool, robotGeneration), blueprint.ObsidianRobot ),
+                    ( blueprint.GeodeRobot.UntilNextAvailable(pool, robotGeneration), blueprint.GeodeRobot )
+                };
+
+                var nextRobot = timeForNextRobot.MinBy(p => p.Item1 * p.Item2.Priority);
+                var repetitions = nextRobot.Item1;
+                while (repetitions > 0)
+                {
+                    minute += 1;
+                    repetitions -= 1;
+                    pool.Add(robotGeneration);
+                }
+
+                minute += 1;
+                pool.Spend(nextRobot.Item2.ObsidianCost, nextRobot.Item2.ClayCost, nextRobot.Item2.OreCost);
+                pool.Add(robotGeneration);
+                robotGeneration.Add(nextRobot.Item2.Create().Generate());
+            }
+
+            QualityLevel += blueprint.Id * pool.Geode;
+        }
+    }
 }
--- AdventOfCode/2022/Day19/Day19.Logic/RobotFactory.cs
+++ AdventOfCode/2022/Day19/Day19.Logic/RobotFactory.cs
@@ -6,12 +6,16 @@ public class RobotFactory
     public int ObsidianCost { get; }
     public int ClayCost { get; }
     public int OreCost { get; }
+    public (int, int, int) Cost => (OreCost, ClayCost, ObsidianCost);
 
-    public RobotFactory(int obsidianCost, int clayCost, int oreCost, (int, int, int, int) generation)
+    public int Priority { get; internal set; }
+
+    public RobotFactory(int obsidianCost, int clayCost, int oreCost, (int, int, int, int) generation, int priority)
     {
         ObsidianCost = obsidianCost;
         ClayCost = clayCost;
         OreCost = oreCost;
+        Priority = priority;
         _generation = generation;
     }
 
@@ -19,4 +23,42 @@ public class RobotFactory
 
     internal bool CanCreateWith(Pool pool) =>
         pool.Obsidian >= ObsidianCost && pool.Clay >= ClayCost && pool.Ore >= OreCost;
+
+    internal int UntilNextAvailable(Pool pool, Pool robotGeneration)
+    {
+        var neededOre = OreCost - pool.Ore;
+        var neededClay = ClayCost - pool.Clay;
+        var neededObsidian = ObsidianCost - pool.Obsidian;
+
+        if (neededOre <= 0 && neededClay <= 0 && neededObsidian <= 0)
+        {
+            return 1;
+        }
+
+        int[] time = { 1000, 1000, 1000 };
+        if (neededOre >= 0 && robotGeneration.Ore != 0)
+        {
+            time[0] = neededOre / robotGeneration.Ore;
+        }
+
+        if (neededClay <= 0)
+        {
+            time[1] = 0;
+        }
+        else if (neededClay > 0 && robotGeneration.Clay != 0)
+        {
+            time[1] = (int)Math.Ceiling(neededClay / (double)robotGeneration.Clay);
+        }
+
+        if (neededObsidian <= 0)
+        {
+            time[2] = 0;
+        }
+        else if (neededObsidian > 0 && robotGeneration.Obsidian != 0)
+        {
+            time[2] = (int)Math.Ceiling(neededObsidian / (double)robotGeneration.Obsidian);
+        }
+
+        return time.Max();
+    }
 }
