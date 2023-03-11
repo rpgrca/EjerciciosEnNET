72decbbdf86ec94a7374a31237dcc77fe43fb6f7
Roberto <rpgrca@gmail.com>
2023-03-10 22:42:18 -0300

    solve puzzle

--- Codewars/6kyu/ConvertStringToCamelCase/ConvertStringToCamelCase.Logic/Kata.cs
+++ Codewars/6kyu/ConvertStringToCamelCase/ConvertStringToCamelCase.Logic/Kata.cs
@@ -1,11 +1,13 @@
 ﻿namespace ConvertStringToCamelCase.Logic;
 
 using System;
+using System.Text.RegularExpressions;
 
 public class Kata
 {
     public static string ToCamelCase(string str)
     {
-        return str;
+        var values = Regex.Split(str, "[-_]");
+        return string.Concat(values[0], string.Concat(values[1..].Select(System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase)));
     }
 }
