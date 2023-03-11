9d8d51653f05994be67edc7aaaffc6f0c17f75a7
Roberto <rpgrca@gmail.com>
2023-03-10 22:51:42 -0300

    add extra tests

--- Codewars/6kyu/ConvertStringToCamelCase/ConvertStringToCamelCase.UnitTests/KataTest.cs
+++ Codewars/6kyu/ConvertStringToCamelCase/ConvertStringToCamelCase.UnitTests/KataTest.cs
@@ -1,5 +1,8 @@
-using System;
 using ConvertStringToCamelCase.Logic;
+using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 
 namespace ConvertStringToCamelCase.UnitTests;
@@ -7,10 +10,40 @@ namespace ConvertStringToCamelCase.UnitTests;
 [TestFixture]
 public class KataTest
 {
-  [Test]
-  public void KataTests()
-  {
-    Assert.AreEqual("theStealthWarrior", Kata.ToCamelCase("the_stealth_warrior"), "Kata.ToCamelCase('the_stealth_warrior') did not return correct value");
-    Assert.AreEqual("TheStealthWarrior", Kata.ToCamelCase("The-Stealth-Warrior"), "Kata.ToCamelCase('The-Stealth-Warrior') did not return correct value");
-  }
+    public string Solution(string str)
+    {
+        return Regex.Replace(str, @"[_-](\w)", m => m.Groups[1].Value.ToUpper());
+    }
+  
+    [Test]
+    public void KataTests()
+    {
+        Assert.AreEqual("theStealthWarrior", Kata.ToCamelCase("the_stealth_warrior"), "Kata.ToCamelCase('the_stealth_warrior') did not return correct value");
+        Assert.AreEqual("TheStealthWarrior", Kata.ToCamelCase("The-Stealth-Warrior"), "Kata.ToCamelCase('The-Stealth-Warrior') did not return correct value");
+    }
+  
+    [Test]
+    public void SimpleTests()
+    {
+        Assert.AreEqual("", Kata.ToCamelCase(""), "An empty string was provided but not returned");
+        Assert.AreEqual("ABC", Kata.ToCamelCase("A-B-C"), "Kata.ToCamelCase('A-B-C') did not return correct value");
+    }
+  
+    [Test]
+    public void RandomTests()
+    {
+        var random = new Random();
+        string randomStr;
+        for (int i = 0; i < 10; i++)
+        {
+            randomStr = 
+                String.Join("", Enumerable.Range(0, 10).Select(o => (char)random.Next('a', 'z')))
+                + "_"
+                + String.Join("", Enumerable.Range(0, 10).Select(o => (char)random.Next('a', 'z')))
+                + "-"
+                + String.Join("", Enumerable.Range(0, 10).Select(o => (char)random.Next('a', 'z')));
+
+            Assert.AreEqual(Solution(randomStr), Kata.ToCamelCase(randomStr));
+        }
+    }
 }
