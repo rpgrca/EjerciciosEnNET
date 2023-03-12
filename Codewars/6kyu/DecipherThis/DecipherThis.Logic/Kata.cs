2a3566acac1e4ff57f2eef6e2bc1fa42e7f046cd
Roberto <rpgrca@gmail.com>
2023-03-11 21:27:15 -0300

    solve samples

--- Codewars/6kyu/DecipherThis/DecipherThis.Logic/Kata.cs
+++ Codewars/6kyu/DecipherThis/DecipherThis.Logic/Kata.cs
@@ -6,6 +6,22 @@ public class Kata
 {
     public static string DecipherThis(string s)
     {
-        return ""; // Implement me! :)
+        if (string.IsNullOrEmpty(s)) return string.Empty;
+
+        var decipheredText = new List<string>();
+        foreach (var value in s.Split(" "))
+        {
+            var number = string.Concat(value.TakeWhile(char.IsDigit));
+            var decipherWord = value.Replace(number, $"{(char)int.Parse(number)}");
+
+            if (decipherWord.Length > 2)
+            {
+                decipherWord = $"{decipherWord[0]}{decipherWord[^1]}{decipherWord[2..^1]}{decipherWord[1]}";
+            }
+
+            decipheredText.Add(decipherWord);
+        }
+
+        return string.Join(" ", decipheredText);
     }
 }
