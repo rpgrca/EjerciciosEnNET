623617eb79191b34b5634672ee6faebf414257be
Roberto <rpgrca@gmail.com>
2023-03-24 17:00:08 -0300

    make user nullable in spy

--- LegacyAppRefactor/LegacyApp.UnitTests/UserDataAccessSpy.cs
+++ LegacyAppRefactor/LegacyApp.UnitTests/UserDataAccessSpy.cs
@@ -2,7 +2,7 @@ using LegacyApp;
 
 public class UserDataAccessSpy : IUserDataAccess
 {
-    public User AddedUser { get; private set; }
+    public User? AddedUser { get; private set; }
 
     public void AddUser(User user) => AddedUser = user;
 }
