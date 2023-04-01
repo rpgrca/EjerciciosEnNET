9e3d80cdb9011b32a497f22de033dddd6fa21f8f
Roberto <rpgrca@gmail.com>
2023-03-31 22:02:29 -0300

    solve sample tests

--- Codewars/4kyu/MostUsedWordInText/MostUsedWordInText.Logic/TopWords.cs
+++ Codewars/4kyu/MostUsedWordInText/MostUsedWordInText.Logic/TopWords.cs
@@ -7,7 +7,17 @@ public class TopWords
 {
     public static List<string> Top3(string s)
     {
-        // Your code here
-        return null;
+        var words = s
+          .Split(new char[] { '.', ' ', '\n', '\r', ',', '/' });
+        return words
+            .Where(p => !string.IsNullOrWhiteSpace(p))
+            .Where(p => p.Any(p => char.IsAsciiLetterOrDigit(p)))
+            .Select(p => p.ToLower())
+            .GroupBy(p => p, (a, b) => new { Word = a, Count = b.Count() }, StringComparer.CurrentCultureIgnoreCase)
+            .OrderByDescending(p => p.Count)
+            .Take(3)
+            .Select(w => w.Word)
+            .ToList();
     }
+
 }
