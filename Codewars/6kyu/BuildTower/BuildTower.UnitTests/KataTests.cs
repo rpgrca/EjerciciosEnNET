3e3935834a461b61a2186f3dbca151d0326d2095
Roberto <rpgrca@gmail.com>
2023-04-01 20:57:26 -0300

    add extra tests

--- Codewars/6kyu/BuildTower/BuildTower.UnitTests/KataTests.cs
+++ Codewars/6kyu/BuildTower/BuildTower.UnitTests/KataTests.cs
@@ -2,6 +2,7 @@ namespace Solution
 {
   using NUnit.Framework;
   using System;
+  using System.Linq;
   using BuildTower.Logic;
 
   [TestFixture]
@@ -13,6 +14,43 @@ namespace Solution
       Assert.AreEqual(string.Join(",", new [] { "*" }), string.Join(",", Kata.TowerBuilder(1)));
       Assert.AreEqual(string.Join(",", new [] { " * ", "***" }), string.Join(",", Kata.TowerBuilder(2)));
       Assert.AreEqual(string.Join(",", new [] { "  *  ", " *** ", "*****" }), string.Join(",", Kata.TowerBuilder(3)));
+    }
+    
+    [Test]
+    public void RandomTests()
+    {
+      var rand = new Random();
+      
+      Func<int,string[]> myTowerBuilder = delegate (int nFloors)
+      {
+        string[] lines = new string[nFloors];
+        for(var i = 1; i<=nFloors; i++)
+        {
+          lines[i-1] = (new string(' ', nFloors-i) + new string('*', i*2-1) + new string(' ', nFloors-i));
+        }
+        return lines;
+      };
+      
+      var seq = Enumerable.Range(1,100).ToArray();
+      for(int r=0;r<100;r++)
+      {
+        for(int p=0;p<100;p++)
+        {
+          if(rand.Next(0, 2) == 0)
+          {
+            var temp = seq[r];
+            seq[r] = seq[p];
+            seq[p] = temp;
+          }
+        }
+      }
+      
+      for(int r=0;r<100;r++)
+      {
+        var n = seq[r];
+        //Console.WriteLine(n);
+        Assert.AreEqual(string.Join(",", myTowerBuilder(n)), string.Join(",", Kata.TowerBuilder(n)));
+      }
     }
   }
 }
