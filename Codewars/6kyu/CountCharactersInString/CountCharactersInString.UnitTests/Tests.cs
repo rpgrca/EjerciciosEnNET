72d6b862de9d95a058155e3b4f0904efeaca763c
Roberto <rpgrca@gmail.com>
2023-04-02 23:26:04 -0300

    add readme

--- Codewars/6kyu/CountCharactersInString/CountCharactersInString.UnitTests/Tests.cs
+++ Codewars/6kyu/CountCharactersInString/CountCharactersInString.UnitTests/Tests.cs
@@ -24,4 +24,36 @@ public class Tests
     d.Add('b', 2);
     Assert.AreEqual(d, Kata.Count("aabb"));
   }
+  
+  private static Dictionary<char, int> Solution(string str)
+  {
+    Dictionary<char, int> dict = new Dictionary<char, int>();
+    foreach(char c in str)
+    {
+      if(dict.ContainsKey(c))
+        dict[c] = dict[c]+1;
+      else
+        dict.Add(c, 1);
+    }
+    return dict;
+  }
+  
+  private static string GetRandomString(int length)
+  {
+    string alpha = "abcdefghijklmnopqrstuvwxyz";
+    string str = string.Empty;
+    Random r = new Random();
+    for(int i = 0; i < length; i++)
+    {
+      str += alpha[r.Next(26)];
+    }
+    return str;
+  }
+  
+  [Test]
+  public static void RandomTest([Random(5,100,100)]int length)
+  {
+    string str = GetRandomString(length);
+    Assert.AreEqual(Solution(str), Kata.Count(str), string.Format("Should work for {0}", str));
+  }
 }
---
+++ Codewars/6kyu/CountCharactersInString/README.md
@@ -0,0 +1,7 @@
+## Count characters in your string
+
+The main idea is to count all the occurring characters in a string. If you have a string like _aba_, then the result should be _{'a': 2, 'b': 1}_.
+
+What if the string is empty? Then the result should be empty object literal, _{}_.
+
+[Try it yourself!](https://www.codewars.com/kata/52efefcbcdf57161d4000091)
