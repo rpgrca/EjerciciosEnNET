fc5bee7ea314ce050c7313b236773e4edb307b65
Roberto <rpgrca@gmail.com>
2023-04-02 23:26:45 -0300

    simplify

--- Codewars/6kyu/CountCharactersInString/CountCharactersInString.Logic/Kata.cs
+++ Codewars/6kyu/CountCharactersInString/CountCharactersInString.Logic/Kata.cs
@@ -6,5 +6,5 @@ using System.Collections.Generic;
 public class Kata
 {
   public static Dictionary<char, int> Count(string str) =>
-    str.OrderBy(p => p).GroupBy(p => p, (q, r) => new { Key = q, Value = r.Count() }).ToDictionary(k => k.Key, v => v.Value);
+    str.GroupBy(p => p).ToDictionary(k => k.Key, v => v.Count());
 }
