ac6d77ed778e63b11a27063122767844f315a475
Roberto <rpgrca@gmail.com>
2023-04-02 23:18:50 -0300

    solve tests

--- Codewars/6kyu/CountCharactersInString/CountCharactersInString.Logic/Kata.cs
+++ Codewars/6kyu/CountCharactersInString/CountCharactersInString.Logic/Kata.cs
@@ -7,6 +7,12 @@ public class Kata
 {
   public static Dictionary<char, int> Count(string str)
   {
-    
+    var result = new Dictionary<char, int>();
+    if (string.IsNullOrEmpty(str))
+    {
+        return result;
+    }
+
+    return str.OrderBy(p => p).GroupBy(p => p, (q, r) => new { Key = q, Value = r.Count() }).ToDictionary(k => k.Key, v => v.Value);
   }
 }
