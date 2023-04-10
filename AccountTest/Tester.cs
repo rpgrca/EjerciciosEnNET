ba1e5b092e1d185cb1641632da3665ed4b92b20f
Roberto <rpgrca@gmail.com>
2023-04-10 17:47:49 -0300

    add tests to solve problem

--- AccountTest/Tester.cs
+++ AccountTest/Tester.cs
@@ -9,7 +9,67 @@ public class Tester
     [Test]
     public void AccountCannotHaveNegativeOverdraftLimit()
     {
-        Account account = new Account(-20);
+        var account = new Account(-20);
         Assert.AreEqual(0, account.OverdraftLimit, epsilon);
+    }
+
+    [Test]
+    public void DepositMustNotAcceptNegativeNumbers()
+    {
+        var account = new Account(0);
+        account.Deposit(30);
+
+        account.Deposit(-15);
+        Assert.AreEqual(30, account.Balance);
+    }
+
+    [Test]
+    public void WithdrawMustNotAcceptNegativeNumbers()
+    {
+        var account = new Account(0);
+        account.Deposit(30);
+
+        account.Withdraw(-7);
+        Assert.AreEqual(30, account.Balance);
+    }
+
+    [Test]
+    public void DepositMustIncrementCorrectAmount()
+    {
+        var account = new Account(0);
+        account.Deposit(30);
+        Assert.AreEqual(30, account.Balance);
+    }
+
+    [Test]
+    public void WithdrawMustDiscountCorrectAmount()
+    {
+        var account = new Account(0);
+        account.Deposit(30);
+        account.Withdraw(7);
+        Assert.AreEqual(23, account.Balance);
+    }
+
+    [Test]
+    public void NeitherDepositNorWithdrawMustAcceptNegativeNumbers()
+    {
+        var account = new Account(0);
+        Assert.AreEqual(false, account.Deposit(-30));
+        Assert.AreEqual(false, account.Withdraw(-15));
+    }
+
+    [Test]
+    public void BothDepositAndWithdrawMustWorkWithPositiveNumbers()
+    {
+        var account = new Account(0);
+        Assert.AreEqual(true, account.Deposit(30));
+        Assert.AreEqual(true, account.Withdraw(13));
+    }
+
+    [Test]
+    public void CannotWithdrawAboveOverdraft()
+    {
+        var account = new Account(30);
+        Assert.AreEqual(false, account.Withdraw(35));
     }
 }
