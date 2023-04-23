fea9493701e97a1fbc1361d1406b277832af59d2
Roberto <rpgrca@gmail.com>
2023-04-22 21:56:27 -0300

    update solution to fulfill all tests

--- SortedSearch/SortedSearch.cs
+++ SortedSearch/SortedSearch.cs
@@ -6,6 +6,35 @@ public class SortedSearch
 {
     public static int CountNumbers(int[] sortedArray, int lessThan)
     {
-        return sortedArray.TakeWhile(p => p < lessThan).Count();
+        var bottom = 0;
+        var top = sortedArray.Length - 1;
+        int previousTop;
+        int previousBottom;
+        int middle;
+
+        if (top < 100)
+        {
+            return sortedArray.TakeWhile(p => p < lessThan).Count();
+        }
+
+        do
+        {
+            previousBottom = bottom;
+            previousTop = top;
+
+            middle = (top - bottom) / 2;
+            if (sortedArray[bottom + middle] < lessThan)
+            {
+                bottom += middle;
+            }
+
+            if (sortedArray[top - middle] > lessThan)
+            {
+                top -= middle;
+            }
+
+        } while (previousBottom != bottom || previousTop != top);
+
+        return sortedArray[bottom + middle] == lessThan ? (top + bottom) / 2 : top;
     }
 }
--- SortedSearch/SortedSearchMust.cs
+++ SortedSearch/SortedSearchMust.cs
@@ -5,8 +5,38 @@ using SortedSearch.Logic;
 public class SortedSearchMust
 {
     [Fact]
+    public void SolveShortArray_WhenValueIsInArray()
+    {
+        Assert.Equal(0, SortedSearch.CountNumbers(new int[] { 2 }, 2));
+    }
+
+    [Theory]
+    [InlineData(10, 1)]
+    [InlineData(1, 0)]
+    public void SolveShortArray_WhenValueIsNotInArray(int lessThan, int expectedResult)
+    {
+       Assert.Equal(expectedResult, SortedSearch.CountNumbers(new int[] { 2 }, lessThan));
+    }
+
+    [Fact]
     public void SolveExample()
     {
         Assert.Equal(2, SortedSearch.CountNumbers(new int[] { 1, 3, 5, 7 }, 4));
+    }
+
+    [Fact]
+    public void SolveLargeExample()
+    {
+        var array = Enumerable.Range(1, 100000).Select(p => p).ToArray();
+        Assert.Equal(36999, SortedSearch.CountNumbers(array, 37000));
+    }
+
+    [Theory]
+    [InlineData(3, 1)]
+    [InlineData(36001, 18000)]
+    public void SolveLargeExample_WhenValueIsNotInArray(int lessThan, int expectedResult)
+    {
+        var array = Enumerable.Range(1, 100000).Select(p => p * 2).ToArray();
+        Assert.Equal(expectedResult, SortedSearch.CountNumbers(array, lessThan));
     }
 }
