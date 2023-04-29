e1e5b3942415bd41277236e98e91431321aa3642
Roberto <rpgrca@gmail.com>
2023-04-29 19:20:23 -0300

    add initial setup

---
+++ UserInput/TextInput.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class TextInput {}
+
+public class NumericInput {}
+
+public class UserInput
+{
+    public static void Main(string[] args)
+    {
+        //TextInput input = new NumericInput();
+        //input.Add('1');
+        //input.Add('a');
+        //input.Add('0');
+        //Console.WriteLine(input.GetValue());
+    }
+}
