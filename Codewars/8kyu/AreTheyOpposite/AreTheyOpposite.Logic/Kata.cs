da38c6bdc60354dd13ed68f64956bb57a46f5e66
Roberto <rpgrca@gmail.com>
2024-07-09 21:40:20 -0300

    refactor

--- Codewars/8kyu/AreTheyOpposite/AreTheyOpposite.Logic/Kata.cs
+++ Codewars/8kyu/AreTheyOpposite/AreTheyOpposite.Logic/Kata.cs
@@ -7,16 +7,6 @@ public class Kata
     if (s1.Length == 0 || s2.Length == 0) return false;
     if (s1.Length != s2.Length) return false;
 
-    for (var index = 0; index < s1.Length; index++) {
-        if (s1[index] != s2[index]) {
-            if (char.ToUpper(s1[index]) == char.ToUpper(s2[index])) {
-                continue;
-            }
-        }
-
-        return false;
-    }
-    
-    return true;
+    return s1.Zip(s2).All(p => (p.First ^ p.Second) == 32);
   }
 }
