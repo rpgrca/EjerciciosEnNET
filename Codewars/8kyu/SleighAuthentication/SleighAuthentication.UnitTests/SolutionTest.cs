604db529950b6583afe79ab73e7ab2b80e3d92e1
Roberto <rpgrca@gmail.com>
2024-07-12 20:25:05 -0300

    add full tests

--- Codewars/8kyu/SleighAuthentication/SleighAuthentication.UnitTests/SolutionTest.cs
+++ Codewars/8kyu/SleighAuthentication/SleighAuthentication.UnitTests/SolutionTest.cs
@@ -4,20 +4,32 @@ using NUnit.Framework;
 using SleighAuthentication.Logic;
 using System;
 
-[TestFixture]
-public class SolutionTest
-{
+  [TestFixture]
+  public class SolutionTest
+  {
     [Test, Description("The Sleigh must authenticate with correct credentials")]
     public void CorrectTest()
     {
-        Assert.That(Sleigh.Authenticate("Santa Claus", "Ho Ho Ho!"));
+      Assert.That(Sleigh.Authenticate("Santa Claus", "Ho Ho Ho!"));
     }
-
+    
+    private static Random rnd = new Random();
+    
+    private static object[] incorrectTests = new object[]
+    {
+      new object[] {"Santa", "Ho Ho Ho!"},
+      new object[] {"Santa Claus", "Ho Ho Ho"},
+      new object[] {"Santa Claus", "Ho Ho!"},
+      new object[] {"Easter Bunny", "Ho Ho Ho!"},
+      new object[] {"jhoffner", "CodeWars"},
+    }.OrderBy(_ => rnd.Next()).ToArray();
+    
     [Test, Description("The Sleigh must not authenticate with incorrect credentials")]
     public void IncorrectTest()
     {
-        Assert.That(!Sleigh.Authenticate("Santa", "Ho Ho Ho!"));
-        Assert.That(!Sleigh.Authenticate("Santa Claus", "Ho Ho!"));
-        Assert.That(!Sleigh.Authenticate("jhoffner", "CodeWars"));
+      for (int i = 0; i < incorrectTests.Length; ++i)
+      {
+        Assert.That(!Sleigh.Authenticate(((incorrectTests[i] as object[])[0] as string), ((incorrectTests[i] as object[])[1] as string)));
+      }
     }
-}
+  }
