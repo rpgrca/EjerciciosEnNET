955f6ab163c8a895149ca9be898c0a6efb1ed5c4
Roberto <rpgrca@gmail.com>
2024-07-26 17:53:50 -0300

    add solution

--- Codewars/8kyu/Collinearity/Collinearity.Logic/Kata.cs
+++ Codewars/8kyu/Collinearity/Collinearity.Logic/Kata.cs
@@ -4,6 +4,11 @@ public class Kata
 {
     public static bool Collinearity(int x1, int y1, int x2, int y2)
     {
+        if (x1 == 0 && y1 == 0) return true;
+        if (x2 == 0 && y2 == 0) return true;
+        if (x1 == 0 && x2 == 0) return true;
+        if (y1 == 0 && y2 == 0) return true;
+        if (x2 != 0 && y2 != 0 && x1/x2 == y1/y2) return true;
         return false;
     }
 }
