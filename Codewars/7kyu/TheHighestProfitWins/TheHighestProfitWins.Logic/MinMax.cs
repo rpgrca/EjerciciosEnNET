99f636e1c64efb3fce81d7294c03a489a53d5fd8
Roberto <rpgrca@gmail.com>
2024-08-22 18:00:03 -0300

    add solution

--- Codewars/7kyu/TheHighestProfitWins/TheHighestProfitWins.Logic/MinMax.cs
+++ Codewars/7kyu/TheHighestProfitWins/TheHighestProfitWins.Logic/MinMax.cs
@@ -6,6 +6,12 @@ public class MinMax
 {
     public static int[] minMax(int[] lst) 
     {
-        // your code
+        if (lst.Length == 1)
+        {
+            return new[] { lst[0], lst[0] };
+        }
+
+        var sorted = lst.OrderBy(p => p).ToList();
+        return new[] { sorted[0], sorted[^1] };
     }
 }
