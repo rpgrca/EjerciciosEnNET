1f4246e101fa70909c19f6543ed6c006ab5dcd19
Roberto <rpgrca@gmail.com>
2024-08-27 17:26:47 -0300

    initial setup

---
+++ Codewars/7kyu/HelpFarmerCountAnimals/HelpFarmerCountAnimals.Logic/Kata.cs
@@ -0,0 +1,10 @@
+﻿namespace HelpFarmerCountAnimals.Logic;
+
+using System.Collections.Generic;
+
+class Kata{
+    public static Dictionary<string, int> get_animals_count(int legs_number, int heads_number, int horns_number){
+        // TODO: write your solution here
+        return new Dictionary<string, int>();
+    }
+}
---
+++ Codewars/7kyu/HelpFarmerCountAnimals/HelpFarmerCountAnimals.UnitTests/SolutionTest.cs
@@ -0,0 +1,27 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace HelpFarmerCountAnimals.UnitTests;
+
+[TestFixture]
+public class SolutionTest
+{
+[Test]
+public void BasicTests()
+{
+    var expected = new Dictionary<string, int>(){
+    {"rabbits", 3},
+    {"chickens", 5},
+    {"cows", 3}
+    };
+    Assert.AreEqual(expected, Kata.get_animals_count(34, 11, 6));
+    
+    expected = new Dictionary<string, int>(){
+    {"rabbits", 30},
+    {"chickens", 7},
+    {"cows", 5}
+    };
+    Assert.AreEqual(expected, Kata.get_animals_count(154, 42, 10));
+}
+}
