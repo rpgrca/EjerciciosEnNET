b7eb9ad73e847b1c3ec20e09d51531672c21992c
Roberto <rpgrca@gmail.com>
2024-08-27 17:40:50 -0300

    rewrite solution

--- Codewars/7kyu/HelpFarmerCountAnimals/HelpFarmerCountAnimals.Logic/Kata.cs
+++ Codewars/7kyu/HelpFarmerCountAnimals/HelpFarmerCountAnimals.Logic/Kata.cs
@@ -2,9 +2,19 @@
 
 using System.Collections.Generic;
 
-class Kata{
+public class Kata{
     public static Dictionary<string, int> get_animals_count(int legs_number, int heads_number, int horns_number){
-        // TODO: write your solution here
-        return new Dictionary<string, int>();
+        var cows = horns_number / 2;
+        legs_number -= cows * 4;
+        heads_number -= cows;
+
+        var rabbits = legs_number / 2 - heads_number;
+        var chickens = heads_number - rabbits;
+
+        return new Dictionary<string, int> {
+            {"rabbits", rabbits},
+            {"chickens", chickens},
+            {"cows", cows}
+        };
     }
 }
--- Codewars/7kyu/HelpFarmerCountAnimals/HelpFarmerCountAnimals.UnitTests/SolutionTest.cs
+++ Codewars/7kyu/HelpFarmerCountAnimals/HelpFarmerCountAnimals.UnitTests/SolutionTest.cs
@@ -1,3 +1,4 @@
+using HelpFarmerCountAnimals.Logic;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
