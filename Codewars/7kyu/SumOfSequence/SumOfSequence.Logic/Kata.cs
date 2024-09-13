710a58198fa974e94c5b88ebac1d50fbf07726b0
Roberto <rpgrca@gmail.com>
2024-09-13 09:28:42 -0300

    add solution

--- Codewars/7kyu/SumOfSequence/SumOfSequence.Logic/Kata.cs
+++ Codewars/7kyu/SumOfSequence/SumOfSequence.Logic/Kata.cs
@@ -4,6 +4,13 @@ public class Kata
 {
   public static int SequenceSum(int start, int end, int step)
   {
-    return -1;
+    if (start > end) return 0;
+
+    var result = 0;
+    for (var index = start; index <= end; index += step) {
+        result += index;
+    }
+
+    return result;
   }
 }
