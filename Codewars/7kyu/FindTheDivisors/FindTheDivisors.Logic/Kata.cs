a8fba03e09d0fb7da73e793544e3b8c14e926d1e
Roberto <rpgrca@gmail.com>
2024-09-19 20:50:09 -0300

    refactor

--- Codewars/7kyu/FindTheDivisors/FindTheDivisors.Logic/Kata.cs
+++ Codewars/7kyu/FindTheDivisors/FindTheDivisors.Logic/Kata.cs
@@ -5,9 +5,17 @@ public class Kata
 #nullable enable
     public static int[]? Divisors(int n)
     {
+        var divisors = Enumerable
+            .Range(2, n / 2 - 1)
+            .Where(p => n % p == 0)
+            .Select(p => p)
+            .ToArray();
+
+        return divisors.Length == 0 ? null : divisors;
+/*
         var divisors = new List<int>();
         var maximum = n / 2;
-     
+
         for (var i = 2; i <= maximum; i++)
         {
             if (n % i == 0)
@@ -16,7 +24,7 @@ public class Kata
             }
         }
 
-        return divisors.Count == 0 ? null : divisors.ToArray();
+        return divisors.Count == 0 ? null : divisors.ToArray();*/
     }
 #nullable disable
 }
