a178926f019951e56007a915e802ae565ec9679f
Roberto <rpgrca@gmail.com>
2024-09-19 20:43:48 -0300

    add solution

--- Codewars/7kyu/FindTheDivisors/FindTheDivisors.Logic/Kata.cs
+++ Codewars/7kyu/FindTheDivisors/FindTheDivisors.Logic/Kata.cs
@@ -2,8 +2,21 @@
 
 public class Kata
 {
-  public static int[] Divisors(int n)
-  {
-    return new int[0];
-  }
+#nullable enable
+    public static int[]? Divisors(int n)
+    {
+        var divisors = new List<int>();
+        var maximum = n / 2;
+     
+        for (var i = 2; i <= maximum; i++)
+        {
+            if (n % i == 0)
+            {
+                divisors.Add(i);
+            }
+        }
+
+        return divisors.Count == 0 ? null : divisors.ToArray();
+    }
+#nullable disable
 }
