382bc469ed5c5d4171552f65f3acb737edba9a9f
Roberto <rpgrca@gmail.com>
2024-09-25 22:03:18 -0300

    add solution

--- Codewars/6kyu/IpValidation/IpValidation.Logic/Kata.cs
+++ Codewars/6kyu/IpValidation/IpValidation.Logic/Kata.cs
@@ -1,9 +1,19 @@
-﻿namespace IpValidation.Logic;
+﻿using System.Net;
+
+namespace IpValidation.Logic;
 
 public class Kata
 {
-    public static bool IsValidIp(string ipAddres)
+    public static bool IsValidIp(string ipAddress)
     {
+        var octects = ipAddress.Split(".")
+            .Where(p => p.Length > 0)
+            .Where(p => p == p.Trim())
+            .Where(p => p == "0" || !p.StartsWith("0"))
+            .Where(p => int.TryParse(p, out var octect) && octect >= 0 && octect <= 255)
+            .ToList();
+
+        if (octects.Count != 4) return false;
         return true;
     }
 }
