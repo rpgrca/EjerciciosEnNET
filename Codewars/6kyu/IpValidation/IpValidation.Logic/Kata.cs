e320a914bc0a93d44519e2e77c5a7ebaa7a69ff3
Roberto <rpgrca@gmail.com>
2024-09-25 22:07:33 -0300

    refactor to cover quintuple with mistake

--- Codewars/6kyu/IpValidation/IpValidation.Logic/Kata.cs
+++ Codewars/6kyu/IpValidation/IpValidation.Logic/Kata.cs
@@ -6,14 +6,17 @@ public class Kata
 {
     public static bool IsValidIp(string ipAddress)
     {
-        var octects = ipAddress.Split(".")
+        var octects = ipAddress.Split(".");
+        if (octects.Length != 4) return false;
+
+        var sanitizedOctects = octects
             .Where(p => p.Length > 0)
             .Where(p => p == p.Trim())
             .Where(p => p == "0" || !p.StartsWith("0"))
             .Where(p => int.TryParse(p, out var octect) && octect >= 0 && octect <= 255)
             .ToList();
 
-        if (octects.Count != 4) return false;
+        if (sanitizedOctects.Count != 4) return false;
         return true;
     }
 }
