1bc92bd9f90cad516904509369dca48b01eb2829
Roberto <rpgrca@gmail.com>
2025-01-07 15:26:33 -0300

    update legacy app to net9

--- LegacyAppRefactor/LegacyApp/UserCreditService.cs
+++ LegacyAppRefactor/LegacyApp/UserCreditService.cs
@@ -1,36 +1,22 @@
 namespace LegacyApp
 {
-    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
-    [System.ServiceModel.ServiceContractAttribute(ConfigurationName = "LegacyApp.IUserCreditService")]
+    [System.ServiceModel.ServiceContract(ConfigurationName = "LegacyApp.IUserCreditService")]
     public interface IUserCreditService : IDisposable
     {
-        [System.ServiceModel.OperationContractAttribute(Action = "http://totally-real-service.com/IUserCreditService/GetCreditLimit", ReplyAction = "http://totally-real-service.com/IUserCreditService/GetCreditLimitResponse")]
+        [System.ServiceModel.OperationContract(Action = "http://totally-real-service.com/IUserCreditService/GetCreditLimit", ReplyAction = "http://totally-real-service.com/IUserCreditService/GetCreditLimitResponse")]
         int GetCreditLimit(string firstname, string surname, System.DateTime dateOfBirth);
     }
 
-    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
     public interface IUserCreditServiceChannel : IUserCreditService, System.ServiceModel.IClientChannel
     {
     }
 
-    [System.Diagnostics.DebuggerStepThroughAttribute()]
-    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
+    [System.Diagnostics.DebuggerStepThrough()]
     public partial class UserCreditServiceClient : System.ServiceModel.ClientBase<IUserCreditService>, IUserCreditService
     {
         public UserCreditServiceClient()
         {
         }
-
-		public UserCreditServiceClient(string endpointConfigurationName):
-            base(endpointConfigurationName)
-        {
-        }
-
-        public UserCreditServiceClient(string endpointConfigurationName, System.ServiceModel.EndpointAddress remoteAddress):
-			base(endpointConfigurationName, remoteAddress)
-		{
-		}
-
 
 		public UserCreditServiceClient(System.ServiceModel.Channels.Binding binding, System.ServiceModel.EndpointAddress remoteAddress):
 			base(binding, remoteAddress)
